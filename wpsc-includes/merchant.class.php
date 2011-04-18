54a381cc5af62b79dd8bf80cd827cda576bcc3cd
garyc40 <garyc40@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-04-18 06:06:07 +0000

    Fixes issue 534. Select from the correct table in wpsc_merchant::get_authcode(). Props johnbeales.
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/trunk@85 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-includes/merchant.class.php
+++ wpsc-includes/merchant.class.php
@@ -322,7 +322,7 @@ class wpsc_merchant {
 		if($append === false){
 			return $wpdb->update(WPSC_TABLE_PURCHASE_LOGS,array('authcode'=>$authcode), array('id'=>absint($this->purchase_id)),array('%s'), array('%d'));
 		}else{
-			$current_authcode = $wpdb->get_var( "SELECT authcode FROM `" . WPSC_TABLE_CART_CONTENTS . "` WHERE `sessionid` = " . absint( $this->session_id ) . " LIMIT 1" );
+			$current_authcode = $wpdb->get_var( "SELECT authcode FROM `" . WPSC_TABLE_PURCHASE_LOGS . "` WHERE `sessionid` = " . absint( $this->session_id ) . " LIMIT 1" );
 			//this is overwrite
 			$new_authcode = isset($current_authcode) ? $current_authcode.'|' :'';
 			$new_authcode .= $authcode;
