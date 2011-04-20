a6f1b119bc3063d9302db5d09c279c86ce90298d
garyc40 <garyc40@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-04-20 07:53:49 +0000

    wpsc_display_products_page() epic fail
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@142 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-includes/theme.functions.php
+++ wpsc-includes/theme.functions.php
@@ -897,11 +897,7 @@ function wpsc_get_the_new_id($prod_id){
  */
 function wpsc_display_products_page( $query ) {
 	global $wpdb, $wpsc_query,$wp_query;
-	static $count = 0;
-	$count++;
 	remove_filter('the_title','wpsc_the_category_title');
-	if ( $count > 10 )
-		exit( 'fail' );
 
 	// If the data is coming from a shortcode parse the values into the args variable, 
 	// I did it this was to preserve backwards compatibility
