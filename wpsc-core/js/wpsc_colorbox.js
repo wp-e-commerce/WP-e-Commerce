16f0d5b3513766f6453788d6dc0011df97a501e5
garyc40 <garyc40@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-04-25 07:55:04 +0000

    Prevent duplicate image in colorbox gallery
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@178 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-core/js/wpsc_colorbox.js
+++ wpsc-core/js/wpsc_colorbox.js
@@ -1,5 +1,21 @@
 jQuery(document).ready(function(){
 	jQuery('.imagecol').each(function(){
-		jQuery('.thickbox', this).colorbox({maxWidth:'90%', maxHeight:'90%'});
+		var t = jQuery(this).find('.wpcart_gallery .thickbox');
+		t.colorbox({
+			maxWidth :'90%',
+			maxHeight :'90%',
+			returnFocus : false
+		});
+		
+		jQuery(this).children('.thickbox').click(function(e){
+			var that = jQuery(this);
+			e.preventDefault();
+			t.each(function(){
+				if (jQuery(this).attr('href') == that.attr('href')) {
+					jQuery(this).click();
+					return;
+				}
+			});
+		});
 	});
 });
