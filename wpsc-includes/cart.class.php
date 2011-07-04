1cc08f4645d525835a710521a09175eb6df27337
office@zaowebdesign.com <office@zaowebdesign.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-07-04 22:49:16 +0000

    Adding filter to 3.8 branch for cart product title (see trunk notes.)
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@507 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-includes/cart.class.php
+++ wpsc-includes/cart.class.php
@@ -1627,8 +1627,8 @@ class wpsc_cart_item {
       
    	  $price = apply_filters('wpsc_price', $price, $product_id);
       // create the string containing the product name.
-      $product_name = $product->post_title;
-
+      $product_name = apply_filters( 'wpsc_cart_product_title', $product->post_title, $product_id );
+      
       $this->product_name = $product_name;
       $this->priceandstock_id = $priceandstock_id;
       $this->meta = $product_meta;
