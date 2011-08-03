2138f29dc6afed14a8f358d2e1f1fa188d77ec11
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-08-03 07:40:36 +0000

    bump version
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@611 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-core/wpsc-constants.php
+++ wpsc-core/wpsc-constants.php
@@ -26,9 +26,9 @@ function wpsc_core_constants() {
 	if(!defined('WPSC_URL'))
 		define( 'WPSC_URL',       plugins_url( '', __FILE__ ) );
 	// Define Plugin version
-	define( 'WPSC_VERSION', '3.8.5' );
+	define( 'WPSC_VERSION', '3.8.6' );
 	define( 'WPSC_MINOR_VERSION', '405496' );
-	define( 'WPSC_PRESENTABLE_VERSION', '3.8.5' );
+	define( 'WPSC_PRESENTABLE_VERSION', '3.8.6' );
 
 	// Define Debug Variables for developers
 	define( 'WPSC_DEBUG', false );
