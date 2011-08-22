7e088c128c6b35c9d10ee7c54fe21551ff82ece9
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-08-22 05:15:55 +0000

    Fix: Incompatibility with WP 3.3-dev (dashboard.css is merged into wp-admin.css)
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@678 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-admin/admin.php
+++ wpsc-admin/admin.php
@@ -276,7 +276,8 @@ function wpsc_admin_include_css_and_js_refac( $pagehook ) {
 	if ( is_ssl ( ) )
 		$siteurl = str_replace( "http://", "https://", $siteurl );
 
-	wp_admin_css( 'dashboard' );
+	if ( version_compare( '3.3', get_bloginfo( 'version' ), '<' ) )
+		wp_admin_css( 'dashboard' );
 	
 	if($current_screen->id == 'dashboard_page_wpsc-sales-logs'){
 		// jQuery
