65e55c8a66ed672350420c981df17b95a910e264
benjaminhuson@gmail.com <benjaminhuson@gmail.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-09-07 14:57:49 +0000

    Added proper post updated messages - now says "Product updated" instead or "Post updated". Tiny detail, but it was bugging me.
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@712 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-core/wpsc-functions.php
+++ wpsc-core/wpsc-functions.php
@@ -377,6 +377,33 @@ function wpsc_register_post_types() {
 }
 add_action( 'init', 'wpsc_register_post_types', 8 );
 
+/**
+ * Post Updated Messages
+ */
+function wpsc_post_updated_messages( $messages ) {
+	global $post, $post_ID;
+	
+	$messages['wpsc-product'] = array(
+		0  => '', // Unused. Messages start at index 1.
+		1  => sprintf( __( 'Product updated. <a href="%s">View product</a>' ), esc_url( get_permalink( $post_ID ) ) ),
+		2  => __( 'Custom field updated.' ),
+		3  => __( 'Custom field deleted.' ),
+		4  => __( 'Product updated.' ),
+		// translators: %s: date and time of the revision
+		5  => isset( $_GET['revision'] ) ? sprintf( __('Product restored to revision from %s' ), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+		6  => sprintf( __( 'Product published. <a href="%s">View product</a>' ), esc_url( get_permalink( $post_ID ) ) ),
+		7  => __( 'Product saved.' ),
+		8  => sprintf( __( 'Product submitted. <a target="_blank" href="%s">Preview product</a>' ), esc_url( add_query_arg( 'preview', 'true', get_permalink( $post_ID ) ) ) ),
+		9  => sprintf( __( 'Product scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview product</a>' ),
+			// translators: Publish box date format, see http://php.net/date
+			date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( get_permalink( $post_ID ) ) ),
+		10 => sprintf( __( 'Product draft updated. <a target="_blank" href="%s">Preview product</a>' ), esc_url( add_query_arg( 'preview', 'true', get_permalink( $post_ID ) ) ) ),
+	);
+	
+	return $messages;
+}
+add_filter( 'post_updated_messages', 'wpsc_post_updated_messages' );
+
 function wpsc_check_thumbnail_support() {
 	if ( !current_theme_supports( 'post-thumbnails' ) ) {
 		add_theme_support( 'post-thumbnails' );
