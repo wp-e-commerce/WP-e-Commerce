ea4fc4a783394c2e7c8543902d0fbe7bc18a5d20
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-09-08 07:49:22 +0000

    New: Option to set claimed stock clearance interval (Store Settings -> General).
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/trunk@727 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-admin/includes/settings-pages/general.php
+++ wpsc-admin/includes/settings-pages/general.php
@@ -77,6 +77,24 @@ function wpsc_options_general() {
 ?>
 					</td>
 				</tr>
+				<?php 
+					$stock_keeping_time = get_option( 'wpsc_stock_keeping_time', 1 );
+					$stock_keeping_interval = get_option( 'wpsc_stock_keeping_interval', 'day' );
+				?>
+				<tr>
+					<th scope="row">
+						<label for="wpsc-stock-keeping-time"><?php _e( 'Keep stock in cart for' ); ?>:</label>
+					</th>
+					<td>
+						<input type="text" name="wpsc_options[wpsc_stock_keeping_time]" id="wpsc-stock-keeping-time" size="2" value="<?php echo esc_attr( $stock_keeping_time ); ?>" />
+						<select name="wpsc_options[wpsc_stock_keeping_interval]">
+							<option value="hour" <?php selected( 'hour', $stock_keeping_interval ); ?>><?php echo _n( 'hour', 'hours', $stock_keeping_time, 'wpsc' ); ?></option>
+							<option value="day" <?php selected( 'day', $stock_keeping_interval ); ?>><?php echo _n( 'day', 'days', $stock_keeping_time, 'wpsc' ) ?></option>
+							<option value="week" <?php selected( 'week', $stock_keeping_interval ); ?>><?php echo _n( 'week', 'weeks', $stock_keeping_time, 'wpsc' ) ?></option>
+						</select><br />
+						<?php _e( "Set the amount of time items in a customer's cart are reserved. You can also specify decimal amounts such as '0.5 days' or '1.25 weeks'. Note that the minimum interval you can enter is 1 hour, i.e. you can't schedule it to run every 0.5 hour.") ?>
+					</td>
+				</tr>
 			</table>
 
 			<h3 class="form_group"><?php _e( 'Currency Settings', 'wpsc' ); ?>:</h3>
