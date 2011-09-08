fb30c891bd831738af5eb7a339fdfd0f02910c1c
benjaminhuson@gmail.com <benjaminhuson@gmail.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-09-08 14:15:11 +0000

    Added 'wpsc' textdomain to post updated messages so that they can be translated.
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@738 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-core/wpsc-functions.php
+++ wpsc-core/wpsc-functions.php
@@ -385,19 +385,19 @@ function wpsc_post_updated_messages( $messages ) {
 	
 	$messages['wpsc-product'] = array(
 		0  => '', // Unused. Messages start at index 1.
-		1  => sprintf( __( 'Product updated. <a href="%s">View product</a>' ), esc_url( get_permalink( $post_ID ) ) ),
-		2  => __( 'Custom field updated.' ),
-		3  => __( 'Custom field deleted.' ),
-		4  => __( 'Product updated.' ),
+		1  => sprintf( __( 'Product updated. <a href="%s">View product</a>', 'wpsc' ), esc_url( get_permalink( $post_ID ) ) ),
+		2  => __( 'Custom field updated.', 'wpsc' ),
+		3  => __( 'Custom field deleted.', 'wpsc' ),
+		4  => __( 'Product updated.', 'wpsc' ),
 		// translators: %s: date and time of the revision
-		5  => isset( $_GET['revision'] ) ? sprintf( __('Product restored to revision from %s' ), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
-		6  => sprintf( __( 'Product published. <a href="%s">View product</a>' ), esc_url( get_permalink( $post_ID ) ) ),
-		7  => __( 'Product saved.' ),
-		8  => sprintf( __( 'Product submitted. <a target="_blank" href="%s">Preview product</a>' ), esc_url( add_query_arg( 'preview', 'true', get_permalink( $post_ID ) ) ) ),
-		9  => sprintf( __( 'Product scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview product</a>' ),
+		5  => isset( $_GET['revision'] ) ? sprintf( __('Product restored to revision from %s', 'wpsc' ), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+		6  => sprintf( __( 'Product published. <a href="%s">View product</a>', 'wpsc' ), esc_url( get_permalink( $post_ID ) ) ),
+		7  => __( 'Product saved.', 'wpsc' ),
+		8  => sprintf( __( 'Product submitted. <a target="_blank" href="%s">Preview product</a>', 'wpsc' ), esc_url( add_query_arg( 'preview', 'true', get_permalink( $post_ID ) ) ) ),
+		9  => sprintf( __( 'Product scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview product</a>', 'wpsc' ),
 			// translators: Publish box date format, see http://php.net/date
-			date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( get_permalink( $post_ID ) ) ),
-		10 => sprintf( __( 'Product draft updated. <a target="_blank" href="%s">Preview product</a>' ), esc_url( add_query_arg( 'preview', 'true', get_permalink( $post_ID ) ) ) ),
+			date_i18n( __( 'M j, Y @ G:i', 'wpsc' ), strtotime( $post->post_date ) ), esc_url( get_permalink( $post_ID ) ) ),
+		10 => sprintf( __( 'Product draft updated. <a target="_blank" href="%s">Preview product</a>', 'wpsc' ), esc_url( add_query_arg( 'preview', 'true', get_permalink( $post_ID ) ) ) ),
 	);
 	
 	return $messages;
