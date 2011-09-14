f59f63b19b7b3495871370562075f7b59394b849
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-09-14 16:23:51 +0000

    Fix: Security vulnerability in chronopay.
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/trunk@770 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-merchants/chronopay.php
+++ wpsc-merchants/chronopay.php
@@ -15,11 +15,11 @@ function gateway_chronopay($separator, $sessionid)
 	$purchase_log = $wpdb->get_results($purchase_log_sql,ARRAY_A) ;
 
 	$cart_sql = "SELECT * FROM `".WPSC_TABLE_CART_CONTENTS."` WHERE `purchaseid`='".$purchase_log[0]['id']."'";
-	$cart = $wpdb->get_results($cart_sql,ARRAY_A) ; 
-  
+	$cart = $wpdb->get_results($cart_sql,ARRAY_A) ;
+
 	// ChronoPay post variables
 	$chronopay_url = get_option('chronopay_url');
-	
+
 	$data['product_id'] = get_option('chronopay_product_id');
 	$data['product_name'] = get_option('chronopay_product_name');
 	$data['product_price_currency'] = get_option('chronopay_curcode');
@@ -30,31 +30,31 @@ function gateway_chronopay($separator, $sessionid)
 	$data['cs1'] = $sessionid;
 	$data['cs2'] = 'chronopay';
 	$salt = get_option('chronopay_salt');
-	$data['cs3'] = md5($salt . md5($sessionid . $salt));	// placed in here for security so that the return call can be validated as 'real'	
-		
-	// User details   
+	$data['cs3'] = md5($salt . md5($sessionid . $salt));	// placed in here for security so that the return call can be validated as 'real'
+
+	// User details
 	if($_POST['collected_data'][get_option('chronopay_form_first_name')] != '')
-    {   
+    {
     	$data['f_name'] = $_POST['collected_data'][get_option('chronopay_form_first_name')];
     }
 	if($_POST['collected_data'][get_option('chronopay_form_last_name')] != "")
-    {   
+    {
     	$data['s_name'] = $_POST['collected_data'][get_option('chronopay_form_last_name')];
     }
   	if($_POST['collected_data'][get_option('chronopay_form_address')] != '')
-    {   
-    	$data['street'] = str_replace("\n",', ', $_POST['collected_data'][get_option('chronopay_form_address')]); 
+    {
+    	$data['street'] = str_replace("\n",', ', $_POST['collected_data'][get_option('chronopay_form_address')]);
     }
    	if($_POST['collected_data'][get_option('chronopay_form_city')] != '')
     {
-    	$data['city'] = $_POST['collected_data'][get_option('chronopay_form_city')]; 
+    	$data['city'] = $_POST['collected_data'][get_option('chronopay_form_city')];
     }
   	if(preg_match("/^[a-zA-Z]{2}$/",$_SESSION['selected_country']))
-    {   
+    {
     	$data['country'] = $_SESSION['selected_country'];
-    }    
+    }
 
-  	// Change suggested by waxfeet@gmail.com, if email to be sent is not there, dont send an email address        
+  	// Change suggested by waxfeet@gmail.com, if email to be sent is not there, dont send an email address
   	$email_data = $wpdb->get_results("SELECT `id`,`type` FROM `".WPSC_TABLE_CHECKOUT_FORMS."` WHERE `type` IN ('email') AND `active` = '1'",ARRAY_A);
   	foreach((array)$email_data as $email)
     {
@@ -64,30 +64,30 @@ function gateway_chronopay($separator, $sessionid)
     {
     	$data['email'] = $_POST['collected_data'][get_option('email_form_field')];
     }
-	
-	
+
+
 	// Get Currency details abd price
 	$currency_code = $wpdb->get_results("SELECT `code` FROM `".WPSC_TABLE_CURRENCY_LIST."` WHERE `id`='".get_option('currency_type')."' LIMIT 1",ARRAY_A);
 	$local_currency_code = $currency_code[0]['code'];
 	$chronopay_currency_code = get_option('chronopay_curcode');
-  
-	// ChronoPay only processes in the set currency.  This is USD or EUR dependent on what the Chornopay account is set up with.  
+
+	// ChronoPay only processes in the set currency.  This is USD or EUR dependent on what the Chornopay account is set up with.
 	// This must match the ChronoPay settings set up in wordpress.  Convert to the chronopay currency and calculate total.
 	$curr=new CURRENCYCONVERTER();
 	$decimal_places = 2;
 	$total_price = 0;
-  
+
 	$i = 1;
-  
+
 	$all_donations = true;
 	$all_no_shipping = true;
-	
+
 	foreach($cart as $item)
 	{
 		$product_data = $wpdb->get_results("SELECT * FROM `" . $wpdb->posts . "` WHERE `id`='".$item['prodid']."' LIMIT 1",ARRAY_A);
 		$product_data = $product_data[0];
 		$variation_count = count($product_variations);
-    
+
 		$variation_sql = "SELECT * FROM `".WPSC_TABLE_CART_ITEM_VARIATIONS."` WHERE `cart_id`='".$item['id']."'";
 		$variation_data = $wpdb->get_results($variation_sql,ARRAY_A);
 		$variation_count = count($variation_data);
@@ -104,7 +104,7 @@ function gateway_chronopay($separator, $sessionid)
           		}
         		$value_id = $variation['venue_id'];
         		$value_data = $wpdb->get_results("SELECT * FROM `".WPSC_TABLE_VARIATION_VALUES."` WHERE `id`='".$value_id."' LIMIT 1",ARRAY_A);
-        		$variation_list .= $value_data[0]['name'];              
+        		$variation_list .= $value_data[0]['name'];
         		$j++;
         	}
       		$variation_list .= ")";
@@ -113,37 +113,37 @@ function gateway_chronopay($separator, $sessionid)
         {
         	$variation_list = '';
         }
-    
+
     	$local_currency_productprice = $item['price'];
 
 			$local_currency_shipping = $item['pnp'];
-    	
+
 
 			$chronopay_currency_productprice = $local_currency_productprice;
 			$chronopay_currency_shipping = $local_currency_shipping;
-			
+
     	$data['item_name_'.$i] = $product_data['name'].$variation_list;
     	$data['amount_'.$i] = number_format(sprintf("%01.2f", $chronopay_currency_productprice),$decimal_places,'.','');
     	$data['quantity_'.$i] = $item['quantity'];
     	$data['item_number_'.$i] = $product_data['id'];
-    	
+
 		if($item['donation'] !=1)
       	{
       		$all_donations = false;
       		$data['shipping_'.$i] = number_format($chronopay_currency_shipping,$decimal_places,'.','');
-      		$data['shipping2_'.$i] = number_format($chronopay_currency_shipping,$decimal_places,'.','');      
+      		$data['shipping2_'.$i] = number_format($chronopay_currency_shipping,$decimal_places,'.','');
       	}
       	else
       	{
       		$data['shipping_'.$i] = number_format(0,$decimal_places,'.','');
       		$data['shipping2_'.$i] = number_format(0,$decimal_places,'.','');
       	}
-        
+
     	if($product_data['no_shipping'] != 1) {
       		$all_no_shipping = false;
       	}
-    
-		
+
+
 		$total_price = $total_price + ($data['amount_'.$i] * $data['quantity_'.$i]);
 
 		if( $all_no_shipping != false )
@@ -159,8 +159,8 @@ function gateway_chronopay($separator, $sessionid)
     }
 
 	$data['product_price'] = $total_price;
-	
-	
+
+
 	if(WPSC_GATEWAY_DEBUG == true ) {
   	exit("<pre>".print_r($data,true)."</pre>");
 	}
@@ -169,25 +169,25 @@ function gateway_chronopay($separator, $sessionid)
 	// Create Form to post to ChronoPay
 	$output = "
 		<form id=\"chronopay_form\" name=\"chronopay_form\" method=\"post\" action=\"$chronopay_url\">\n";
-	
+
 	foreach($data as $n=>$v) {
 			$output .= "			<input type=\"hidden\" name=\"$n\" value=\"$v\" />\n";
 	}
-	
+
 	$output .= "			<input type=\"submit\" value=\"Continue to ChronoPay\" />
 		</form>
 	";
 
-	// echo form.. 
+	// echo form..
 	if( get_option('chronopay_debug') == 1)
 	{
 		echo ("DEBUG MODE ON!!<br/>");
 		echo("The following form is created and would be posted to ChronoPay for processing.  Press submit to continue:<br/>");
 		echo("<pre>".htmlspecialchars($output)."</pre>");
 	}
-	
+
 	echo($output);
-	
+
 	if(get_option('chronopay_debug') == 0)
 	{
 		echo "<script language=\"javascript\" type=\"text/javascript\">document.getElementById('chronopay_form').submit();</script>";
@@ -195,7 +195,7 @@ function gateway_chronopay($separator, $sessionid)
 
   	exit();
 }
-  
+
 function nzshpcrt_chronopay_callback()
 {
 	global $wpdb;
@@ -206,14 +206,14 @@ function nzshpcrt_chronopay_callback()
     	// This is a call from chronopay.  validate that it is from a chronopay server in the and process.
 		// validate cs3 variable to see if it makes sense for security
 		$salt = get_option('chronopay_salt');
-		$gen_hash = md5($salt . md5($_POST['cs1'] . $salt));	
-		
+		$gen_hash = md5($salt . md5($_POST['cs1'] . $salt));
+
 		if($gen_hash == $_POST['cs3'])
 		{
 			// Added in to fake a TX number for testing.  ChronoPay dev accounts do not return a trans_id.
 			//if($_POST['transaction_id'] == '')
 			//	$_POST['transaction_id'] = 'testid123123';
-				
+
 			// process response.
 		    $sessionid = trim(stripslashes($_POST['cs1']));
 			$transaction_id = trim(stripslashes($_POST['transaction_id']));
@@ -225,41 +225,42 @@ function nzshpcrt_chronopay_callback()
 				case 'onetime': // All successful processing statuses.
 	            case 'initial':
 				case 'rebill':
-	            	$wpdb->query("UPDATE `".WPSC_TABLE_PURCHASE_LOGS."` SET 
-										`processed` = '2', 
-										`transactid` = '".$transaction_id."', 
-										`date` = '".time()."'
-									WHERE `sessionid` = ".$sessionid." LIMIT 1");
-					
+					$data = array(
+						'processed'  => 2,
+						'transactid' => $transact_id,
+						'date'       => time(),
+					);
+					$where = array( 'sessionid' => $sessionid );
+					$format = array( '%d', '%s', '%s' );
+					$wpdb->update( WPSC_TABLE_PURCHASE_LOGS, $data, $where, $format );
 					transaction_results($sessionid, false, $transaction_id);
-	            	break;                        
-            
+	            	break;
+
 	            case 'decline': // if it fails, delete it
-	            	$log_id = $wpdb->get_var("SELECT `id` FROM `".WPSC_TABLE_PURCHASE_LOGS."` WHERE `sessionid`='$sessionid' LIMIT 1");
-	            	$delete_log_form_sql = "SELECT * FROM `".WPSC_TABLE_CART_CONTENTS."` WHERE `purchaseid`='$log_id'";
+					$log_id = $wpdb->get_var( $wpdb->prepare( "SELECT `id` FROM `".WPSC_TABLE_PURCHASE_LOGS."` WHERE `sessionid`=%s LIMIT 1", $sessionid ) );
+	            	$delete_log_form_sql = $wpdb->prepare( "SELECT * FROM `".WPSC_TABLE_CART_CONTENTS."` WHERE `purchaseid`=%d", $log_id );
 	            	$cart_content = $wpdb->get_results($delete_log_form_sql,ARRAY_A);
 	            	foreach((array)$cart_content as $cart_item)
 	              	{
-	              		$cart_item_variations = $wpdb->query("DELETE FROM `".WPSC_TABLE_CART_ITEM_VARIATIONS."` WHERE `cart_id` = '".$cart_item['id']."'", ARRAY_A);
+	              		$cart_item_variations = $wpdb->query( $wpdb->prepare( "DELETE FROM `".WPSC_TABLE_CART_ITEM_VARIATIONS."` WHERE `cart_id` = %d", $cart_item['id'] ), ARRAY_A);
 	              	}
-	            	$wpdb->query("DELETE FROM `".WPSC_TABLE_CART_CONTENTS."` WHERE `purchaseid`='$log_id'");
-	            	$wpdb->query("DELETE FROM `".WPSC_TABLE_SUBMITED_FORM_DATA."` WHERE `log_id` IN ('$log_id')");
-	            	$wpdb->query("DELETE FROM `".WPSC_TABLE_PURCHASE_LOGS."` WHERE `id`='$log_id' LIMIT 1");
+	            	$wpdb->query( $wpdb->prepare( "DELETE FROM `".WPSC_TABLE_CART_CONTENTS."` WHERE `purchaseid`=%d", $log_id ) );
+	            	$wpdb->query( $wpdb->prepare( "DELETE FROM `".WPSC_TABLE_SUBMITED_FORM_DATA."` WHERE `log_id` IN ( %d )", $log_id ) );
+	            	$wpdb->query( $wpdb->prepare( "DELETE FROM `".WPSC_TABLE_PURCHASE_LOGS."` WHERE `id`=%d LIMIT 1", $log_id ) );
 	            	break;
-            
+
 	            case 'Pending':      // need to wait for "Completed" before processing
-	            	$sql = "UPDATE `".WPSC_TABLE_PURCHASE_LOGS."` SET `transactid` = '".$transaction_id."', `date` = '".time()."'  WHERE `sessionid` = ".$sessionid." LIMIT 1";
-	            	$wpdb->query($sql) ;
+					$wpdb->update( WPSC_TABLE_PURCHASE_LOGS, array( 'transactid' => $transaction_id, 'date' => time() ), array( 'sessionid' => $sessionid ), array( '%d', '%s' ) );
 	            	break;
-            
+
 	            default: // if nothing, do nothing, safest course of action here.
 	            	break;
-            
+
 			}
 		}
 		else
 		{
-			// Security Hash failed!!.. notify someone.. 
+			// Security Hash failed!!.. notify someone..
 			$message = "This message has been sent because a call to your ChronoPay function was made by a server that did not have the correct security key.  This could mean someone is trying to hack your payment site.  The details of the call are below.\n\r\n\r";
 			$message .= "OUR_POST:\n\r".print_r($header . $req,true)."\n\r\n\r";
 			$message .= "THEIR_POST:\n\r".print_r($_POST,true)."\n\r\n\r";
@@ -267,7 +268,7 @@ function nzshpcrt_chronopay_callback()
 			$message .= "SERVER:\n\r".print_r($_SERVER,true)."\n\r\n\r";
 			mail(get_option('purch_log_email'), "ChronoPay Security Key Failed!", $message);
 		}
-			
+
 		// If in debug, email details
 		if(get_option('chronopay_debug') == 1)
 		{
@@ -278,7 +279,7 @@ function nzshpcrt_chronopay_callback()
 			$message .= "SERVER:\n\r".print_r($_SERVER,true)."\n\r\n\r";
 			mail(get_option('purch_log_email'), "ChronoPay Data", $message);
 		}
-	} 
+	}
 }
 
 function nzshpcrt_chronopay_results()
@@ -291,27 +292,27 @@ function nzshpcrt_chronopay_results()
 }
 
 function submit_chronopay()
-{  
+{
 	if(isset($_POST['chronopay_product_id']))
     {
     	update_option('chronopay_product_id', $_POST['chronopay_product_id']);
     }
-    
+
   	if(isset($_POST['chronopay_product_name']))
     {
     	update_option('chronopay_product_name', $_POST['chronopay_product_name']);
     }
-    
+
   	if(isset($_POST['chronopay_curcode']))
     {
     	update_option('chronopay_curcode', $_POST['chronopay_curcode']);
     }
-    
+
   	if(isset($_POST['chronopay_language']))
     {
     	update_option('chronopay_language', $_POST['chronopay_language']);
     }
-    
+
   	if(isset($_POST['chronopay_url']))
     {
     	update_option('chronopay_url', $_POST['chronopay_url']);
@@ -326,7 +327,7 @@ function submit_chronopay()
     {
     	update_option('chronopay_debug', $_POST['chronopay_debug']);
     }
-    
+
     if (!isset($_POST['chronopay_form'])) $_POST['chronopay_form'] = array();
 	foreach((array)$_POST['chronopay_form'] as $form => $value)
     {
@@ -336,12 +337,12 @@ function submit_chronopay()
 }
 
 function form_chronopay()
-{	
+{
 	$select_currency[get_option('chronopay_curcode')] = "selected='selected'";
 	$select_language[get_option('chronopay_language')] = "selected='selected'";
 	$chronopay_url = ( get_option('chronopay_url')=='' ? 'https://secure.chronopay.com/index_shop.cgi' : get_option('chronopay_url') );
 	$chronopay_salt = ( get_option('chronopay_salt')=='' ? 'changeme' : get_option('chronopay_salt') );
-	
+
 	$chronopay_debug = get_option('chronopay_debug');
 	$chronopay_debug1 = "";
 	$chronopay_debug2 = "";
@@ -354,14 +355,14 @@ function form_chronopay()
 			$chronopay_debug1 = "checked ='checked'";
 			break;
 	}
-	
-	if (!isset($select_currency['USD'])) $select_currency['USD'] = ''; 
-	if (!isset($select_currency['EUR'])) $select_currency['EUR'] = ''; 
+
+	if (!isset($select_currency['USD'])) $select_currency['USD'] = '';
+	if (!isset($select_currency['EUR'])) $select_currency['EUR'] = '';
 	if (!isset($select_language['EN'])) $select_language['EN'] = '';
 	if (!isset($select_language['ES'])) $select_language['ES'] = '';
 	if (!isset($select_language['NL'])) $select_language['NL'] = '';
 	if (!isset($select_language['RU'])) $select_language['RU'] = '';
-	
+
 	$output = "
 		<tr>
 			<td>Product ID</td>
@@ -385,7 +386,7 @@ function form_chronopay()
 			<td><select name='chronopay_curcode'>
 					<option ".$select_currency['USD']." value='USD'>USD - U.S. Dollar</option>
 					<option ".$select_currency['EUR']." value='EUR'>EUR - Euros</option>
-				</select> 
+				</select>
 			</td>
 		</tr>
 		<tr>
@@ -399,7 +400,7 @@ function form_chronopay()
 					<option ".$select_language['ES']." value='ES'>Spanish</option>
 					<option ".$select_language['NL']." value='NL'>Dutch</option>
 					<option ".$select_language['RU']." value='RU'>Russian</option>
-				</select> 
+				</select>
 			</td>
 		</tr>
 		<tr>
@@ -441,8 +442,8 @@ function form_chronopay()
 			<td>&nbsp;</td>
 			<td><small>Debug mode is used to write HTTP communications between the ChronoPay server and your host to a log file.  This should only be activated for testing!</small></td>
 		</tr>
-	
-   
+
+
 	<tr class='update_gateway' >
 		<td colspan='2'>
 			<div class='submit'>
@@ -450,13 +451,13 @@ function form_chronopay()
 		</div>
 		</td>
 	</tr>
-	
+
 	<tr class='firstrowth'>
 		<td style='border-bottom: medium none;' colspan='2'>
 			<strong class='form_group'>Forms Sent to Gateway</strong>
 		</td>
 	</tr>
-	
+
 		<tr>
 			<td>First Name Field</td>
 			<td><select name='chronopay_form[first_name]'>
@@ -509,12 +510,12 @@ function form_chronopay()
 		   <tr>
            <td colspan='2'>For more help configuring Chronopay, read our documentation <a href='http://docs.getshopped.org/wiki/documentation/payments/chronopay'>here </a></td>
        </tr>";
-	
+
 	return $output;
 }
-  
-  
+
+
 add_action('init', 'nzshpcrt_chronopay_callback');
 add_action('init', 'nzshpcrt_chronopay_results');
-	
+
 ?>
