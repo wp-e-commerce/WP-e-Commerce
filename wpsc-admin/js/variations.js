925b29694a856fc66a4554289578f8e25b19a503
benjaminhuson@gmail.com <benjaminhuson@gmail.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-09-19 13:25:08 +0000

    Added option to check all/none variation checkboxes when editing a product. Fixes issue 691.
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@785 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-admin/js/variations.js
+++ wpsc-admin/js/variations.js
@@ -20,17 +20,25 @@
 
 jQuery('.variation_checkboxes').livequery(function(){
 
-        jQuery('label input:checkbox', this).click(function(){
-
-                jQuery('a.update_variations_action').show();
-        });
-
+	jQuery('label input:checkbox', this).click(function(){
+		 jQuery('a.update_variations_action').show();
+	});
+	
+	jQuery("div.variation_set>label input:checkbox", this).each(function(){
+		if (jQuery(this).is(':checked')) {
+			jQuery(this).parent().append('<span class="all-none"> (<a href="#" class="all">select all</a> | <a href="#" class="none">select none</a>)</span>');
+		}
+	});
+	
 	jQuery("div.variation_set>label input:checkbox", this).click(function(event){
 		var variation_set = jQuery(this).parents("div.variation_set");
 
+		jQuery(this).parent().find('.all-none').remove();
+		
 		if (jQuery(this).is(':checked')) {
 			jQuery('div.variation input:checkbox', variation_set).attr('checked', true);
 			jQuery('div.variation', variation_set).show();
+			jQuery(this).parent().append('<span class="all-none"> (<a href="#" class="all">select all</a> | <a href="#" class="none">select none</a>)</span>');
 		} else {
 			jQuery('div.variation input:checkbox', variation_set).attr('checked', false);
 			jQuery('div.variation', variation_set).hide();
@@ -38,7 +46,17 @@ jQuery('.variation_checkboxes').livequery(function(){
 	
 	});
 	
+	jQuery("div.variation_set>label a.all", this).live('click', function(event){
+		var variation_set = jQuery(this).parents("div.variation_set");
+		jQuery('div.variation input:checkbox', variation_set).attr('checked','checked');
+		event.preventDefault();
+	});
 	
+	jQuery("div.variation_set>label a.none", this).live('click', function(event){
+		var variation_set = jQuery(this).parents("div.variation_set");
+		jQuery('div.variation input:checkbox', variation_set).removeAttr('checked');
+		event.preventDefault();
+	});
 	
 	jQuery("div.variation input:checkbox", this).click(function(event){
 		var variation_set = jQuery(this).parents("div.variation_set");
