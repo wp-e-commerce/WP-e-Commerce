28de4de207da7fb3f292b3c172c8c3525944bbb8
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-10-13 09:40:02 +0000

    Add method to check whether a payment gateway is registered
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/trunk@856 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-includes/payment-gateway.helpers.php
+++ wpsc-includes/payment-gateway.helpers.php
@@ -7,3 +7,7 @@ function wpsc_register_payment_gateway_dir( $dir ) {
 function wpsc_register_payment_gateway_file( $file ) {
 	return WPSC_Payment_Gateway::register_file( $file );
 }
+
+function wpsc_is_payment_gateway_registered( $gateway ) {
+	return WPSC_Payment_Gateway::is_registered( $gateway );
+}
