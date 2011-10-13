af978ee6e40cbaeb5bbee855c52cc4513b1f9d70
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-10-13 09:41:57 +0000

    Payment Gateways implemented using Singleton
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/trunk@863 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-admin/includes/settings-tabs/gateway.php
+++ wpsc-admin/includes/settings-tabs/gateway.php
@@ -21,8 +21,8 @@ class WPSC_Settings_Tab_Gateway
 	private function gateway_list() {
 		$selected_gateways = get_option( 'custom_gateway_options', array() );
 
-		foreach ( WPSC_Payment_Gateway::get_gateways() as $gateway => $class ) {
-			$this->gateway_item( $gateway, call_user_func( array( $class, 'get_title' ) ), in_array( $gateway, $selected_gateways ) );
+		foreach ( WPSC_Payment_Gateway::get_gateways() as $gateway ) {
+			$this->gateway_item( $gateway, WPSC_Payment_Gateway::get( $gateway )->get_title(), in_array( $gateway, $selected_gateways ) );
 		}
 
 		global $nzshpcrt_gateways;
@@ -35,7 +35,9 @@ class WPSC_Settings_Tab_Gateway
 
 	public function display() {
 		global $wpdb, $nzshpcrt_gateways;
+
 		$curgateway = get_option( 'payment_gateway' );
+
 		$payment_gateway_names = get_option( 'payment_gateway_names' );
 
 		if ( empty( $nzshpcrt_gateways ) )
@@ -72,72 +74,62 @@ class WPSC_Settings_Tab_Gateway
 	?>
 
 		<div class='metabox-holder'>
-			<input type='hidden' name='gateway_submits' value='true' />
-			<input type='hidden' name='wpsc_gateway_settings' value='gateway_settings' />
-			<?php
-
-			if ( get_option( 'custom_gateway' ) == 1 ) {
-				$custom_gateway_hide = "style='display:block;'";
-				$custom_gateway1 = 'checked="checked"';
-			} else {
-				$custom_gateway_hide = "style='display:none;'";
-				$custom_gateway2 = 'checked="checked"';
-			}
-			 ?>
-
-			<table id='gateway_options' >
-				<tr>
-					<td class='select_gateway'>
-						<div class='postbox'>
-							<h3 class='hndle'><?php _e( 'General Settings', 'wpsc' ); ?></h3>
-							<div class='inside'>
-								<p><?php _e( 'Activate the payment gateways that you want to make available to your customers by selecting them below.', 'wpsc' ); ?></p>
-								<br />
-								<?php
-								$this->gateway_list();
-								?>
-								<div class='submit gateway_settings'>
-									<input type='hidden' value='true' name='update_gateways' />
-									<input type='submit' value='<?php _e( 'Update &raquo;', 'wpsc' ) ?>' name='updateoption' />
-								</div>
-								</div>
-							</div>
-
-								<h4><?php _e( 'We Recommend', 'wpsc' ); ?></h4>
-								<a style="border-bottom:none;" href="https://www.paypal.com/nz/mrb/pal=LENKCHY6CU2VY" target="_blank"><img src="<?php echo WPSC_CORE_IMAGES_URL; ?>/paypal-referal.gif" border="0" alt="Sign up for PayPal and start accepting credit card payments instantly." /></a> <br /><br />
-								<a style="border-bottom:none;" href="http://checkout.google.com/sell/?promo=seinstinct" target="_blank"><img src="https://checkout.google.com/buyer/images/google_checkout.gif" border="0" alt="Sign up for Google Checkout" /></a>
-
-							</td>
-
-							<td class='gateway_settings' rowspan='2'>
-								<div class='postbox'>
-							<?php
-
-								if ( !isset( $_SESSION['previous_payment_name'] ) )
-									$_SESSION['previous_payment_name'] = "";
-								if ( !isset( $selected_gateway_data ) )
-									$selected_gateway_data = "";
-								$payment_data = wpsc_get_payment_form( $_SESSION['previous_payment_name'], $selected_gateway_data );
-							?>
-								<h3 class='hndle'><?php echo $payment_data['name']; ?></h3>
+				<input type='hidden' name='gateway_submits' value='true' />
+				<input type='hidden' name='wpsc_gateway_settings' value='gateway_settings' />
+				<table id='gateway_options' >
+					<tr>
+						<td class='select_gateway'>
+							<div class='postbox'>
+								<h3 class='hndle'><?php _e( 'General Settings', 'wpsc' ); ?></h3>
 								<div class='inside'>
-								<table class='form-table'>
-									<?php echo $payment_data['form_fields']; ?>
-								</table>
-								<?php
-								if ( $payment_data['has_submit_button'] == 0 )
-									$update_button_css = 'style= "display: none;"';
-								else
-									$update_button_css = '';
-								?>
-								<div class='submit' <?php echo $update_button_css; ?>>
-									<?php wp_nonce_field( 'update-options', 'wpsc-update-options' ); ?>
-									<input type='submit' value='<?php _e( 'Update &raquo;', 'wpsc' ) ?>' name='updateoption' />
+									<p><?php _e( 'Activate the payment gateways that you want to make available to your customers by selecting them below.', 'wpsc' ); ?></p>
+									<br />
+									<?php
+									$this->gateway_list();
+									?>
+									<div class='submit gateway_settings'>
+										<input type='hidden' value='true' name='update_gateways' />
+										<input type='submit' value='<?php _e( 'Update &raquo;', 'wpsc' ) ?>' name='updateoption' />
+									</div>
+									</div>
 								</div>
-							</div>
-					</td>
-				</tr>
-			</table>
+
+									<h4><?php _e( 'We Recommend', 'wpsc' ); ?></h4>
+									<a style="border-bottom:none;" href="https://www.paypal.com/nz/mrb/pal=LENKCHY6CU2VY" target="_blank"><img src="<?php echo WPSC_CORE_IMAGES_URL; ?>/paypal-referal.gif" border="0" alt="Sign up for PayPal and start accepting credit card payments instantly." /></a> <br /><br />
+									<a style="border-bottom:none;" href="http://checkout.google.com/sell/?promo=seinstinct" target="_blank"><img src="https://checkout.google.com/buyer/images/google_checkout.gif" border="0" alt="Sign up for Google Checkout" /></a>
+
+								</td>
+
+								<td class='gateway_settings' rowspan='2'>
+									<div class='postbox'>
+								<?php
+
+									if ( !isset( $_SESSION['previous_payment_name'] ) )
+										$_SESSION['previous_payment_name'] = "";
+									if ( !isset( $selected_gateway_data ) )
+										$selected_gateway_data = "";
+									$payment_data = wpsc_get_payment_form( $_SESSION['previous_payment_name'], $selected_gateway_data );
+								?>
+									<h3 class='hndle'><?php echo $payment_data['name']; ?></h3>
+									<div class='inside'>
+									<table class='form-table'>
+										<?php echo $payment_data['form_fields']; ?>
+									</table>
+									<?php
+									if ( $payment_data['has_submit_button'] == 0 )
+										$update_button_css = 'style= "display: none;"';
+									else
+										$update_button_css = '';
+									?>
+									<div class='submit' <?php echo $update_button_css; ?>>
+	<?php wp_nonce_field( 'update-options', 'wpsc-update-options' ); ?>
+										<input type='submit' value='<?php _e( 'Update &raquo;', 'wpsc' ) ?>' name='updateoption' />
+									</div>
+								</div>
+						</td>
+					</tr>
+				</table>
+
 		</div>
 
 	<?php
--- wpsc-includes/payment-gateway.class.php
+++ wpsc-includes/payment-gateway.class.php
@@ -3,7 +3,24 @@
 abstract class WPSC_Payment_Gateway
 {
 	protected static $gateways = array();
-	
+	protected static $instances = array();
+
+	/**
+	 * Return a particular payment gateway object
+	 *
+	 * @param string $gateway Name of the payment gateway you want to get
+	 * @return object
+	 * @since 3.9
+	 */
+	public static function &get( $gateway ) {
+		if ( empty( self::$instances[$gateway] ) ) {
+			$class_name = self::$gateways[$gateway];
+			self::$instances[$gateway] = new $class_name();
+		}
+
+		return self::$instances[$gateway];
+	}
+
 	/**
 	 * Check to see whether a gateway is registered using this new API
 	 *
@@ -14,7 +31,7 @@ abstract class WPSC_Payment_Gateway
 	public static function is_registered( $gateway ) {
 		return ! empty( self::$gateways[$gateway] );
 	}
-	
+
 	/**
 	 * Automatically scan a directory for payment gateways and load the classes.
 	 *
@@ -47,28 +64,29 @@ abstract class WPSC_Payment_Gateway
 		$main_file = basename( $dir ) . '.php';
 		if ( file_exists( $dir . $main_file ) )
 			return self::register_file( $dir . $main_file );
-		
+
 		// scan files in dir
 		$files = scandir( $dir );
-		
+
 		foreach ( $files as $file ) {
-			if ( in_array( $file, array( '.', '..' ) ) )
+			if ( $file[0] == '.' )
 				continue;
 
 			$path = $dir . $file;
 			$return = is_dir( $path ) ? self::register_dir( $path ) : self::register_file( $path );
+
 			if ( $return instanceof WP_Error )
 				return $return;
 		}
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * Include a file as a payment gateway module.
 	 *
-	 * The payment gateway inside the file must be defined as a subclass of WPSC_Payment_Gateway. 
-	 * 
+	 * The payment gateway inside the file must be defined as a subclass of WPSC_Payment_Gateway.
+	 *
 	 * The file name should be lowercase, using hyphens or underscores between words instead of spaces. The class name must
 	 * have "WPSC_Payment_Gateway_" as the prefix, followed by the file name, in which words are capitalized and connected by underscore.
 	 *
@@ -84,28 +102,25 @@ abstract class WPSC_Payment_Gateway
 		$filename = basename( $file, '.php' );
 		$classname = ucwords( str_replace( '-', ' ', $filename ) );
 		$classname = 'WPSC_Payment_Gateway_' . str_replace( ' ', '_', $classname );
-		
+
 		if ( ! class_exists( $classname ) )
 			return new WP_Error( 'wpsc_invalid_payment_gateway', __( 'Invalid payment gateway file.' ) );
-		
+
 		self::$gateways[$filename] = $classname;
-		
+
 		return true;
 	}
-	
+
 	/**
-	 * Return an array containing gateway file names and class names.
-	 * 
-	 * For example: array( 'paypal-pro' => 'WPSC_Payment_Gateway_Paypal_Pro' ) if 'paypal-pro.php' is registered
-	 * as a payment gateway module.
+	 * Return an array containing registered gateway names.
 	 *
 	 * @return array
 	 * @since 3.9
 	 */
 	public static function get_gateways() {
-		return self::$gateways;
+		return array_keys( self::$gateways );
 	}
-	
+
 	/**
 	 * Return the title of the payment gateway. This method must be overridden by subclasses.
 	 * It is recommended that the payment gateway title be properly localized using __()
@@ -114,8 +129,8 @@ abstract class WPSC_Payment_Gateway
 	 * @since 3.9
 	 * @see __()
 	 */
-	abstract public static function get_title();
-	
+	abstract public function get_title();
+
 	/**
 	 * Display the payment gateway settings form as seen in WP e-Commerce Settings area.
 	 * This method must be overridden by subclasses.
@@ -123,10 +138,9 @@ abstract class WPSC_Payment_Gateway
 	 * @return void
 	 * @since 3.9
 	 */
-	abstract public static function setup_form();
-		
+	abstract public function setup_form();
+
 	public function __construct() {
 	}
 }
-
 WPSC_Payment_Gateway::register_dir( WPSC_FILE_PATH . '/wpsc-payment-gateways' );
