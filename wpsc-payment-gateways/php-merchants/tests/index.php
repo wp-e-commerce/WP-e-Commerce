b952a488ac3b628000e3e3a90ae97fa0c49b0fc5
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-10-13 09:48:32 +0000

    Test suite for PHP Merchant
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/trunk@893 7bdd534d-2f5a-8b57-a18c-7e4837153298

---
+++ wpsc-payment-gateways/php-merchants/tests/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once( 'simpletest/autorun.php' );
+
+class PHP_Merchant_Test_Suite extends TestSuite
+{
+	function __construct() {
+		parent::__construct( 'PHP Merchant Tests' );
+	}
+}
