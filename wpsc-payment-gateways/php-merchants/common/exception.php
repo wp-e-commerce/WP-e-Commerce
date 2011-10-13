9735bcf7faa1edb524382ece1d84c923c977948d
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-10-13 09:50:16 +0000

    Use global constants, and make sure  is an array
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/trunk@895 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-payment-gateways/php-merchants/common/exception.php
+++ wpsc-payment-gateways/php-merchants/common/exception.php
@@ -1,18 +1,20 @@
 <?php
 
+define( 'PHPME_FEATURE_NOT_SUPPORTED', 1 );
+define( 'PHPME_REQUIRED_OPTION_UNDEFINED', 2 );
+
 final class PHP_Merchant_Exception extends Exception {
-	const C_FEATURE_NOT_SUPPORTED = 1;
-	
 	private static $messages = array(
-		C_FEATURE_NOT_SUPPORTED => 'This payment gateway does not support "%s" feature.',
+		PHPME_FEATURE_NOT_SUPPORTED => 'This payment gateway does not support "%s" feature.',
+		PHPME_MISSING_REQUIRED_PARAM => 'Missing required parameter: %s.',
 	);
 	
 	private $other_args;
 	
 	public function __construct( $code, $message_args = array(), $other_args = array() ) {
-		$this->message_args = $message_args;
+		$this->message_args = (array) $message_args;
 		$this->message = vsprintf( self::$messages[$code], $this->message_args );
-		$this->other_args = $other_args;
+		$this->other_args = (array) $other_args;
 	}
 	
 	public function getArguments() {
