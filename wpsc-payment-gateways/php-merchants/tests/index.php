6d6e9b1146e28568eeacb6f65b5f060044cf78ec
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-10-13 09:51:01 +0000

    Test for PHP_Merchant
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/trunk@899 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-payment-gateways/php-merchants/tests/index.php
+++ wpsc-payment-gateways/php-merchants/tests/index.php
@@ -6,5 +6,14 @@ class PHP_Merchant_Test_Suite extends TestSuite
 {
 	function __construct() {
 		parent::__construct( 'PHP Merchant Tests' );
+		$tests = array(
+			'php-merchant',
+		);
+		
+		$dir = dirname( __FILE__ );
+		
+		foreach ( $tests as $test ) {
+			$this->addFile( $dir . '/' . $test . '.php' );
+		}
 	}
 }
---
+++ wpsc-payment-gateways/php-merchants/tests/php-merchant.php
@@ -0,0 +1,8 @@
+<?php
+
+class PHP_Merchant_Test extends UnitTestCase
+{
+	public function __construct() {
+		parent::__construct();
+	}
+}
