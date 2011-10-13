92946d45f8f2500cd15f8a4bee19faa4de7b37f7
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-10-13 09:41:40 +0000

    Documentation for new payment-gateway.class.php functions
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/trunk@862 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-includes/payment-gateway.class.php
+++ wpsc-includes/payment-gateway.class.php
@@ -4,16 +4,42 @@ abstract class WPSC_Payment_Gateway
 {
 	protected static $gateways = array();
 	
+	/**
+	 * Check to see whether a gateway is registered using this new API
+	 *
+	 * @param string $gateway Gateway name (derived from the filename without .php extension)
+	 * @return bool True if it's already registered.
+	 * @since 3.9
+	 */
 	public static function is_registered( $gateway ) {
 		return ! empty( self::$gateways[$gateway] );
 	}
 	
 	/**
-	 * Automatically scan a directory for payment gateways and load the classes
+	 * Automatically scan a directory for payment gateways and load the classes.
+	 *
+	 * The structure of this directory should follow the same rules of the wp-content/plugins structure.
+	 *
+	 * All of the files inside the directory will be assumed as payment gateway modules.
+	 * If the directory has sub-folders, these sub-folders will be scanned as well, and files with the same
+	 * name as those sub-folders will be included as payment gateway modules.
+	 *
+	 * For example, if we have the following directory structure:
+	 * payment-gateways/
+	 * |-- test-gateway-1.php
+	 * |-- test-gateway-2.php
+	 * |-- test-gateway-3/
+	 *     |-- test-gateway-3.php
+	 *     |-- functions.php
+	 *
+	 * The following files will be loaded as payment gateway modules: test-gateway-1.php, test-gateway-2.php, test-gateway-3/test-gateway-3.php
+	 * See WPSC_Payment_Gateway::register_file() for file and class naming convention
 	 *
 	 * @param string $dir Path to the directory
 	 * @param string $main_file File name of the class to load
 	 * @return mixed Return true if successfully loaded all the payment gateway in the directory. Otherwise return a WP_Error object.
+	 * @since 3.9
+	 * @uses WPSC_Payment_Gateway::register_file()
 	 */
 	public static function register_dir( $dir, $main_file = '' ) {
 		$dir = trailingslashit( $dir );
@@ -38,6 +64,21 @@ abstract class WPSC_Payment_Gateway
 		return true;
 	}
 	
+	/**
+	 * Include a file as a payment gateway module.
+	 *
+	 * The payment gateway inside the file must be defined as a subclass of WPSC_Payment_Gateway. 
+	 * 
+	 * The file name should be lowercase, using hyphens or underscores between words instead of spaces. The class name must
+	 * have "WPSC_Payment_Gateway_" as the prefix, followed by the file name, in which words are capitalized and connected by underscore.
+	 *
+	 * For example, if the file name is "paypal-pro.php", then the class name inside the file must be WPSC_Payment_Gateway_Paypal_Pro.
+	 *
+	 * @param string $file Absolute path to the file containing the payment gateway class
+	 * @return mixed Return true if the file is successfully included and contains a valid class. Otherwise, a WP_Error object is returned.
+	 * @since 3.9
+	 * @see WPSC_Payment_Gateway::register_dir()
+	 */
 	public static function register_file( $file ) {
 		require_once( $file );
 		$filename = basename( $file, '.php' );
@@ -52,18 +93,39 @@ abstract class WPSC_Payment_Gateway
 		return true;
 	}
 	
+	/**
+	 * Return an array containing gateway file names and class names.
+	 * 
+	 * For example: array( 'paypal-pro' => 'WPSC_Payment_Gateway_Paypal_Pro' ) if 'paypal-pro.php' is registered
+	 * as a payment gateway module.
+	 *
+	 * @return array
+	 * @since 3.9
+	 */
 	public static function get_gateways() {
 		return self::$gateways;
 	}
 	
+	/**
+	 * Return the title of the payment gateway. This method must be overridden by subclasses.
+	 * It is recommended that the payment gateway title be properly localized using __()
+	 *
+	 * @return string
+	 * @since 3.9
+	 * @see __()
+	 */
 	abstract public static function get_title();
+	
+	/**
+	 * Display the payment gateway settings form as seen in WP e-Commerce Settings area.
+	 * This method must be overridden by subclasses.
+	 *
+	 * @return void
+	 * @since 3.9
+	 */
 	abstract public static function setup_form();
-	
-	protected $params;
-	
-	public function __construct( $params = array() ) {
-		if ( ! empty( $params ) )
-			$this->set_params( $params );
+		
+	public function __construct() {
 	}
 }
 
