9bd569b54afc7816a07d1cfc3c42a0e5ee71c98d
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-10-13 09:50:27 +0000

    PHP_Merchant::format() to determine whether the currency allow fractions
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/trunk@896 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-payment-gateways/php-merchants/common/php-merchant.php
+++ wpsc-payment-gateways/php-merchants/common/php-merchant.php
@@ -3,12 +3,22 @@ require_once( 'exception.php' );
 
 abstract class PHP_Merchant
 {
-	private $options = array(
+	protected $this->currencies_without_fractions = array( 'JPY', 'HUF' );
+	
+	protected $options = array(
 		'currency' => 'USD',
 	);
 	
-	public function __construct() {
-		
+	public function __construct( $options = array() ) {
+		$this->options = array_merge( $this->options, $options );
+	}
+	
+	public function format( $amt, $currency = false ) {
+		if ( ! $currency )
+			$currency = $this->options['currency'];
+			
+		$dec = in_array( $currency, $this->currencies_without_fractions ) ? 0 : 2;
+		return number_format( $amt, $dec );
 	}
 	
 	public function purchase( $amt, $options = array() ) {
@@ -16,23 +26,23 @@ abstract class PHP_Merchant
 	}
 	
 	public function authorize() {
-		throw new PHP_Merchant_Exception( 1, 'authorize' );
+		throw new PHP_Merchant_Exception( PHPME_C_FEATURE_NOT_SUPPORTED, 'authorize' );
 	}
 	
 	public function capture() {
-		throw new PHP_Merchant_Exception( 1, 'capture' );
+		throw new PHP_Merchant_Exception( PHPME_C_FEATURE_NOT_SUPPORTED, 'capture' );
 	}
 	
 	public function void() {
-		throw new PHP_Merchant_Exception( 1, 'void' );
+		throw new PHP_Merchant_Exception( PHPME_C_FEATURE_NOT_SUPPORTED, 'void' );
 	}
 	
 	public function credit() {
-		throw new PHP_Merchant_Exception( 1, 'credit' );
+		throw new PHP_Merchant_Exception( PHPME_C_FEATURE_NOT_SUPPORTED, 'credit' );
 	}
 	
 	public function recurring() {
-		throw new PHP_Merchant_Exception( 1, 'recurring' );
+		throw new PHP_Merchant_Exception( PHPME_C_FEATURE_NOT_SUPPORTED, 'recurring' );
 	}
 	
 	public function get_options( $options ) {
@@ -51,5 +61,16 @@ abstract class PHP_Merchant
 	public function set_option( $key, $value ) {
 		$this->options[$key] = $value;
 		return $this;
+	}
+	
+	protected function requires( $options ) {
+		$missing = array();
+		foreach ( (array) $options as $option ) {
+			if ( ! isset( $this->options[$option] ) )
+				$missing[] = $option;
+		}
+		
+		if ( ! empty( $missing ) )
+			throw new PHP_Merchant_Exception( PHPME_MISSING_REQUIRED_PARAM, implode( ', ', $missing ) );
 	}
 }
