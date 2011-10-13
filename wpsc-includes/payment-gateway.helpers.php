8a44ca9c0655e9a4c506ed21e793ef7fbff5f440
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-10-13 09:39:39 +0000

    Wrapper for WPSC_Payment_Gateway::register_file()
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/trunk@854 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-includes/payment-gateway.helpers.php
+++ wpsc-includes/payment-gateway.helpers.php
@@ -4,12 +4,6 @@ function wpsc_register_payment_gateway_dir( $dir ) {
 	return WPSC_Payment_Gateway::register_dir( $dir );
 }
 
-/**
- * Register a payment gateway class with WP e-Commerce payment gateway API v3.0.
- * Helper function for WPSC_Payment_Gateway::register();
- *
- * @since 3.9
- */
-function wpsc_register_payment_gateway( $class_name, $params = array(), $file = '' ) {
-	return WPSC_Payment_Gateway::register( $class_name, $params, $file );
+function wpsc_register_payment_gateway_file( $file ) {
+	return WPSC_Payment_Gateway::register_file( $file );
 }
