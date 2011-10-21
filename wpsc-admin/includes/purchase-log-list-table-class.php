c7af231b7b4d60dba0dc5347b0b276e5bbbaf3f1
Gary Cao <garyc40@garyc40.com>
2011-10-22 01:46:38 +0900

    Move search purchase logs form to a separate GET form.

--- wpsc-admin/includes/purchase-log-list-table-class.php
+++ wpsc-admin/includes/purchase-log-list-table-class.php
@@ -314,10 +314,14 @@ class WPSC_Purchase_Log_Page
 	        			printf( '<span class="subtitle">' . __('Search results for &#8220;%s&#8221;') . '</span>', esc_html( stripslashes( $_REQUEST['s'] ) ) ); ?>
 	        </h2>
 
+	        <form id="purchase-logs-search" method-"get" action="">
+	        	<input type="hidden" name="page" value="<?php echo esc_attr( $_REQUEST['page'] ); ?>" />
+	        	<?php $this->list_table->search_box( 'Search Sales Logs', 'post' ); ?>
+	        </form>
+
 	        <!-- Forms are NOT created automatically, so you need to wrap the table in one to use features like bulk actions -->
 	        <form id="purchase-logs-filter" method="post" action="">
 	        	<?php do_action( 'wpsc_purchase_logs_list_table_before' ); ?>
-	        	<?php $this->list_table->search_box( 'Search Sales Logs', 'post' ); ?>
 	            <!-- For plugins, we also need to ensure that the form posts back to our current page -->
 	            <!-- Now we can render the completed list table -->
 
