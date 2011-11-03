72cfe22d18d684f21cd6f5897f96c4583d816f07
james@om4.com.au <james@om4.com.au@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-11-03 06:09:23 +0000

    The Terms and conditions checkbox on the checkout page should have a required field asterisk, just like all other required checkout fields do. For trunk.
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/trunk@1072 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-theme/wpsc-shopping_cart_page.php
+++ wpsc-theme/wpsc-shopping_cart_page.php
@@ -450,7 +450,7 @@ endif;
       <?php if(wpsc_has_tnc()) : ?>
          <tr>
             <td colspan='2'>
-                <label for="agree"><input id="agree" type='checkbox' value='yes' name='agree' /> <?php printf(__("I agree to The <a class='thickbox' target='_blank' href='%s' class='termsandconds'>Terms and Conditions</a>", "wpsc"), site_url("?termsandconds=true&amp;width=360&amp;height=400")); ?></label>
+                <label for="agree"><input id="agree" type='checkbox' value='yes' name='agree' /> <?php printf(__("I agree to the <a class='thickbox' target='_blank' href='%s' class='termsandconds'>Terms and Conditions</a>", "wpsc"), site_url("?termsandconds=true&amp;width=360&amp;height=400")); ?> <span class="asterix">*</span></label>
                </td>
          </tr>
       <?php endif; ?>
