a3d3cb95d7f7ad03034ad3f59280f91f0232160c
office@zaowebdesign.com <office@zaowebdesign.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-11-03 18:59:48 +0000

    Checks for existence of jQuery infieldLabel plugin.  Fixes Issues 809.
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/trunk@1073 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-core/js/wp-e-commerce.js
+++ wpsc-core/js/wp-e-commerce.js
@@ -487,12 +487,14 @@ function set_shipping_country(html_form_id, form_id){
 
 jQuery(document).ready(function(){
 	jQuery('.wpsc_checkout_table input, .wpsc_checkout_table textarea').each(function(){
+	    
 		var real_value = jQuery(this).val();
 		value = jQuery('label[for="'+jQuery(this).attr('id')+'"]').html();
 		if(null != value){
 			value = value.replace(/<span class="?asterix"?>\*<\/span>/i,'');
 		}
-		jQuery(this).inlineFieldLabel({label:jQuery.trim(value)});
+		if( jQuery.inlineFieldLabel() )
+		    jQuery(this).inlineFieldLabel({label:jQuery.trim(value)});
 		if(real_value != '')
 			jQuery(this).val(real_value).removeClass('intra-field-label');
 	});
