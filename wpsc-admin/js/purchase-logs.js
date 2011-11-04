1e9e80ba023bb45cc5fd5c2de3077c51fdb9d681
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-11-04 17:47:15 +0000

    Finish up the Tracking ID column.
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@1133 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-admin/js/purchase-logs.js
+++ wpsc-admin/js/purchase-logs.js
@@ -1,10 +1,114 @@
 (function($){
 	$.extend(WPSC_Purchase_Logs_Admin, {
+		blur_timeout : null,
+		reset_textbox_width : true,
+
 		init : function() {
 			$(function(){
-				$('table.purchase-logs').delegate('.wpsc-purchase-log-status', 'change', WPSC_Purchase_Logs_Admin.event_log_status_change);
+				$('table.purchase-logs').delegate('.wpsc-purchase-log-status', 'change', WPSC_Purchase_Logs_Admin.event_log_status_change).
+				                         delegate('.wpsc-purchase-log-tracking-id', 'focus', WPSC_Purchase_Logs_Admin.event_tracking_id_focused).
+				                         delegate('.column-tracking a.add', 'click', WPSC_Purchase_Logs_Admin.event_button_add_clicked).
+				                         delegate('.wpsc-purchase-log-tracking-id', 'blur', WPSC_Purchase_Logs_Admin.event_tracking_id_blurred).
+				                         delegate('.column-tracking a.save', 'click', WPSC_Purchase_Logs_Admin.event_button_save_clicked).
+				                         delegate('.column-tracking .send-email a', 'click', WPSC_Purchase_Logs_Admin.event_button_send_email_clicked).
+				                         delegate('.wpsc-purchase-log-tracking-id', 'keypress', WPSC_Purchase_Logs_Admin.event_enter_key_pressed);
+
 			});
 		},
+
+		event_enter_key_pressed : function(e) {
+			var code = e.keyCode ? e.keyCode : e.which;
+			if (code == 13) {
+				$(this).siblings('.save').click();
+				e.preventDefault();
+			}
+		},
+
+		event_button_send_email_clicked : function() {
+			var t = $(this);
+
+			var post_data = {
+				'action' : 'wpsc_purchase_log_send_tracking_email',
+				'log_id' : t.closest('div').data('log-id'),
+				'nonce'  : WPSC_Purchase_Logs_Admin.nonce
+			};
+
+			var ajax_callback = function(response) {
+				if (response != 'success') {
+					alert(WPSC_Purchase_Logs_Admin.send_tracking_email_error_dialog);
+					t.show().siblings('em').remove();
+				} else {
+					t.siblings('em').addClass('sent').text(WPSC_Purchase_Logs_Admin.sent_message);
+					t.remove();
+				}
+			};
+
+			t.hide().after('<em>' + WPSC_Purchase_Logs_Admin.sending_message + '</em>');
+			$.post(ajaxurl, post_data, ajax_callback);
+
+			return false;
+		},
+
+		event_button_save_clicked : function() {
+			WPSC_Purchase_Logs_Admin.reset_textbox_width = false;
+
+			var t = $(this), textbox = t.siblings('.wpsc-purchase-log-tracking-id'), spinner = t.siblings('.ajax-feedback');
+
+			var post_data = {
+				'action' : 'wpsc_purchase_log_save_tracking_id',
+				'value'  : textbox.val(),
+				'log_id' : t.parent().data('log-id'),
+				'nonce'  : WPSC_Purchase_Logs_Admin.nonce
+			};
+
+			var ajax_callback = function(response) {
+				spinner.toggleClass('ajax-feedback-active');
+				textbox.blur();
+				if (response == 'success') {
+					t.parent().removeClass('empty');
+				} else {
+					alert(WPSC_Purchase_Logs_Admin.tracking_error_dialog);
+				}
+			};
+
+			t.hide();
+			spinner.toggleClass('ajax-feedback-active');
+			textbox.width(160);
+
+			$.post(ajaxurl, post_data, ajax_callback);
+
+			return false;
+		},
+
+		event_button_add_clicked : function() {
+			$(this).siblings('.wpsc-purchase-log-tracking-id').trigger('focus');
+			return false;
+		},
+
+		event_tracking_id_blurred : function() {
+			var t = $(this);
+			var reset_width = function() {
+				if (WPSC_Purchase_Logs_Admin.reset_textbox_width) {
+					t.siblings('a.save').hide();
+					t.width('');
+					if (t.val() === '') {
+						t.siblings('.add').show();
+					}
+				}
+
+				WPSC_Purchase_Logs_Admin.reset_textbox_width = true;
+			};
+
+			WPSC_Purchase_Logs_Admin.blur_timeout = setTimeout(reset_width, 100);
+		},
+
+		event_tracking_id_focused : function() {
+			var t = $(this);
+			t.width(128);
+			t.siblings('a.save').show();
+			t.siblings('a.add').hide();
+		},
+
 		event_log_status_change : function() {
 			var post_data = {
 					nonce : WPSC_Purchase_Logs_Admin.nonce,
