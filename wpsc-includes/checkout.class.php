2b8291bff872fee749f90276403067bb39871fdb
office@zaowebdesign.com <office@zaowebdesign.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-11-10 01:53:40 +0000

    Removing unused xss function (pretty sure that was my bad about a year or two ago...)  Fixes issue 803 in 3.8 branch
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@1177 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-includes/checkout.class.php
+++ wpsc-includes/checkout.class.php
@@ -9,42 +9,6 @@
  * @package wp-e-commerce
  * @subpackage wpsc-checkout-classes
  */
-function wpsc_kill_xss( $string ) {
-	if ( get_magic_quotes_gpc ( ) ) {
-		$string = stripslashes( $string );
-	}
-	$string = str_replace( array( "&amp;", "&lt;", "&gt;" ), array( "&amp;amp;", "&amp;lt;", "&amp;gt;" ), $string );
-	// fix &entitiy\n;
-	$string = preg_replace( '#(&\#*\w+)[\x00-\x20]+;#u', "$1;", $string );
-	$string = preg_replace( '#(&\#x*)([0-9A-F]+);*#iu', "$1$2;", $string );
-	$string = html_entity_decode( $string, ENT_COMPAT, "UTF-8" );
-
-	// remove any attribute starting with "on" or xmlns
-	$string = preg_replace( '#(<[^>]+[\x00-\x20\"\'\/])(on|xmlns)[^>]*>#iUu', "$1>", $string );
-
-	// remove javascript: and vbscript: protocol
-	$string = preg_replace( '#([a-z]*)[\x00-\x20\/]*=[\x00-\x20\/]*([\`\'\"]*)[\x00-\x20\/]*j[\x00-\x20]*a[\x00-\x20]*v[\x00-\x20]*a[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iUu', '$1=$2nojavascript...', $string );
-	$string = preg_replace( '#([a-z]*)[\x00-\x20\/]*=[\x00-\x20\/]*([\`\'\"]*)[\x00-\x20\/]*v[\x00-\x20]*b[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iUu', '$1=$2novbscript...', $string );
-	$string = preg_replace( '#([a-z]*)[\x00-\x20\/]*=[\x00-\x20\/]*([\`\'\"]*)[\x00-\x20\/]*-moz-binding[\x00-\x20]*:#Uu', '$1=$2nomozbinding...', $string );
-	$string = preg_replace( '#([a-z]*)[\x00-\x20\/]*=[\x00-\x20\/]*([\`\'\"]*)[\x00-\x20\/]*data[\x00-\x20]*:#Uu', '$1=$2nodata...', $string );
-
-	//remove any style attributes, IE allows too much stupid things in them, eg.
-	//<span style="width: expression(alert('Ping!'));"></span>
-	// and in general you really don't want style declarations in your UGC
-
-	$string = preg_replace( '#(<[^>]+[\x00-\x20\"\'\/])style[^>]*>#iUu', "$1>", $string );
-
-	//remove namespaced elements (we do not need them...)
-	$string = preg_replace( '#</*\w+:\w[^>]*>#i', "", $string );
-	//remove really unwanted tags
-
-	do {
-		$oldstring = $string;
-		$string = preg_replace( '#</*(applet|meta|xml|blink|link|style|script|embed|object|iframe|frame|frameset|ilayer|layer|bgsound|title|base)[^>]*>#i', "", $string );
-	} while ( $oldstring != $string );
-
-	return $string;
-}
 
 /**
  * wpsc has regions checks to see whether a country has regions or not
