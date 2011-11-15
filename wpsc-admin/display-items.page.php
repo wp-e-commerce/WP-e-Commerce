39dfb5f67b09e9ddd6decb14eea7ff190f11a413
office@zaowebdesign.com <office@zaowebdesign.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-11-15 03:57:36 +0000

    Fix: No admin product page sorting was working at all.  Now it is.  Neat.  Fixes issue 813.
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@1235 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-admin/display-items.page.php
+++ wpsc-admin/display-items.page.php
@@ -40,10 +40,10 @@ function wpsc_additional_column_names( $columns ){
 }
 function wpsc_additional_sortable_column_names( $columns ){
 
-    $columns['stock'] = __('Stock', 'wpsc');
-    $columns['price'] = __('Price', 'wpsc');
-    $columns['sale_price'] = __('Sale Price', 'wpsc');
-    $columns['SKU'] = __('SKU', 'wpsc');
+    $columns['stock'] = 'stock';
+    $columns['price'] = 'price';
+    $columns['sale_price'] = 'sale_price';
+    $columns['SKU'] = 'SKU';
 
     return $columns;
 }
@@ -236,29 +236,52 @@ function wpsc_additional_column_data( $column ) {
         endswitch;
 
 }
-function wpsc_column_sql_orderby( $orderby, $wp_query ) {
-	global $wpdb;
-
-	$wp_query->query = wp_parse_args( $wp_query->query );
-
-        if( isset( $wp_query->query['orderby'] ) ) :
-            switch ( $wp_query->query['orderby'] ) :
+function wpsc_column_sql_orderby( $vars ) {
+    
+	if ( ! isset( $vars['post_type'] ) || 'wpsc-product' != $vars['post_type'] || ! isset( $vars['orderby'] ) )
+	    return $vars;
+	
+            switch ( $vars['orderby'] ) :
                 case 'stock' :
-                    $orderby = "(SELECT meta_value FROM $wpdb->postmeta WHERE post_id = $wpdb->posts.ID AND meta_key = '_wpsc_stock') " . $wp_query->get('order');
-                    break;
-                case 'price' :
-                    $orderby = "(SELECT meta_value FROM $wpdb->postmeta WHERE post_id = $wpdb->posts.ID AND meta_key = '_wpsc_price') " . $wp_query->get('order');
-                    break;
+		    $vars = array_merge(
+				$vars,
+				array(
+					'meta_key' => '_wpsc_stock',
+					'orderby' => 'meta_value_num'
+				)
+			);
+		    break;
+		case 'price' :
+		    $vars = array_merge(
+				$vars,
+				array(
+					'meta_key' => '_wpsc_price',
+					'orderby' => 'meta_value_num'
+				)
+			);
+		    break;
                 case 'sale_price' :
-                    $orderby = "(SELECT meta_value FROM $wpdb->postmeta WHERE post_id = $wpdb->posts.ID AND meta_key = '_wpsc_special_price') " . $wp_query->get('order');
-                    break;
-                case 'SKU' :
-                    $orderby = "(SELECT meta_value FROM $wpdb->postmeta WHERE post_id = $wpdb->posts.ID AND meta_key = '_wpsc_sku') " . $wp_query->get('order');
-                    break;
-           endswitch;
-        endif;
-
-    return $orderby;
+		    $vars = array_merge(
+				$vars,
+				array(
+					'meta_key' => '_wpsc_special_price',
+					'orderby' => 'meta_value_num'
+				)
+			);
+		    
+		    break;
+		case 'SKU' :
+		    $vars = array_merge(
+				$vars,
+				array(
+					'meta_key' => '_wpsc_sku',
+					'orderby' => 'meta_value'
+				)
+			);
+		    break;
+		endswitch;
+	    
+	return $vars;
 }
 function wpsc_cats_restrict_manage_posts() {
     global $typenow;
@@ -296,6 +319,7 @@ function wpsc_cats_restrict_manage_posts_print_terms($taxonomy, $parent = 0, $le
  * Restrict the products page to showing only parent products and not variations.
  * @since 3.8
  */
+
 function wpsc_no_minors_allowed( $vars ) {
     global $current_screen;
 
@@ -307,14 +331,26 @@ function wpsc_no_minors_allowed( $vars ) {
     return $vars;
 }
 
-add_filter( 'request', 'wpsc_no_minors_allowed' );
+/**
+ * wpsc_sortable_column_load
+ * 
+ * Only sorts columns on edit.php page.
+ * @since 3.8.8
+ */
+
+function wpsc_sortable_column_load() {
+    add_filter( 'request', 'wpsc_no_minors_allowed' );
+    add_filter( 'request', 'wpsc_column_sql_orderby', 8 );
+}
+
+add_action( 'load-edit.php', 'wpsc_sortable_column_load' );
 add_action( 'admin_head', 'wpsc_additional_column_name_variations' );
 add_action( 'restrict_manage_posts', 'wpsc_cats_restrict_manage_posts' );
 add_action( 'manage_pages_custom_column', 'wpsc_additional_column_data', 10, 2 );
 add_filter( 'manage_edit-wpsc-product_sortable_columns', 'wpsc_additional_sortable_column_names' );
 add_filter( 'manage_edit-wpsc-product_columns', 'wpsc_additional_column_names' );
 add_filter( 'manage_wpsc-product_posts_columns', 'wpsc_additional_column_names' );
-add_filter( 'posts_orderby', 'wpsc_column_sql_orderby', 10, 2 );
+
 
 
 /**
