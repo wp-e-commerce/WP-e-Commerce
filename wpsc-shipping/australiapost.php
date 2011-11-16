5bde7a9f07c8dbe778a4f0de99f20783874b8367
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-11-16 03:05:12 +0000

    GPL notice isn't necessary. For 3.8 branch.
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/branch-3.8.7@1251 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-shipping/australiapost.php
+++ wpsc-shipping/australiapost.php
@@ -1,20 +1,4 @@
 <?php
-/*  Copyright 2009-2010 OM4 (email: info@om4.com.au    web: http://om4.com.au/)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
 /**
  * WP e-Commerce Australia Post shipping module - http://auspost.com.au
  *
