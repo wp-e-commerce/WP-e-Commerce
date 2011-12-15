907aca71b59a67bf23e38dd60ec98118a3d0a7c5
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2011-12-15 01:33:31 +0000

    Documentation for Settings Page API.
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/trunk@1464 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-admin/settings-page.php
+++ wpsc-admin/settings-page.php
@@ -1,33 +1,158 @@
 <?php
+/**
+ * WP e-Commerce Settings Page API.
+ *
+ * Third-party plugin / theme developers can add their own tabs to WPEC store settings page.
+ *
+ * Let's say you want to create a tab for your plugin called "Recommendation System", for example.
+ * You first need to register the tab ID and title like this:
+ *
+ * <code>
+ * function my_plugin_settings_tabs( $settings_page ) {
+ * 	$settings_page->register_tab( 'recommendation_system', 'Recommendation System' );
+ * }
+ * add_action( 'wpsc_load_settings_tab_class', 'my_plugin_settings_tabs', 10, 1 );
+ * </code>
+ *
+ * Note that you need to hook into 'wpsc_load_settings_tab_class' to do this.
+ *
+ * The next step is to create a class for your tab which inherits from the base <code>WPSC_Settings_Tab</code>.
+ * The name of the class needs to follow this convention: all the words have to be capitalized and
+ * separated with an underscore, and prefixed with 'WPSC_Settings_Tab_'.
+ *
+ * In our example, because we registered our tab ID as 'recommendation_system', the class name should
+ * be 'WPSC_Settings_Tab_Recommendation_System'.
+ *
+ * <code>
+ * class WPSC_Settings_Tab_Recommendation_System extends WPSC_Settings_Tab
+ * {
+ * 	public function display() {
+ * 		echo '<h3>Recommendation System Settings</h3>';
+ * 		// output your tab content here
+ * 	}
+ * }
+ * </code>
+ *
+ * All tab has to implement a method `display()` which outputs the HTML content for the tab.
+ * You don't need to output the <form> element because it will be done for you.
+ *
+ * When outputting your form fields for the tab, name the fields 'wpsc_options[$your_option_name]'
+ * so that they will automatically get saved to the database when the user submits the form. E.g.:
+ *
+ * <code>
+ * <input type="text" value="something" name="wpsc_options[some_option]" />
+ * </code>
+ *
+ * If you need to handle the form submission yourself, create a method in your tab class called
+ * 'callback_submit_options()'. Then process your submitted fields there.
+ *
+ * <code>
+ * class WPSC_Settings_Tab_Recommendation_System extends WPSC_Settings_Tab
+ * {
+ * 	// ...
+ * 	public function callback_submit_options() {
+ * 		if ( isset( $_POST['my_option'] ) )
+ * 			update_option( 'my_option', $_POST['my_option'] );
+ * 	}
+ * 	// ...
+ * }
+ * </code>
+ *
+ * @package wp-e-commerce
+ * @subpackage settings-api
+ */
 
+/**
+ * Abstract class for setting tabs
+ *
+ * @abstract
+ * @since 3.8.8
+ * @package wp-e-commerce
+ * @subpackage settings-api
+ */
 abstract class WPSC_Settings_Tab
 {
+	/**
+	 * Display the content of the tab. This function has to be overridden.
+	 *
+	 * @since 3.8.8
+	 * @abstract
+	 * @access public
+	 */
 	abstract public function display();
+
+	/**
+	 * Constructor
+	 *
+	 * @since 3.8.8
+	 * @access public
+	 */
 	public function __construct() {}
 }
 
+/**
+ * Settings Page class. Singleton pattern.
+ *
+ * @since 3.8.8
+ * @package wp-e-commerce
+ * @subpackage settings-api
+ * @final
+ */
 final class WPSC_Settings_Page
 {
+	/**
+	 * @staticvar object The active object instance
+	 * @since 3.8.8
+	 * @access private
+	 */
 	private static $instance;
+
+	/**
+	 * @staticvar array An array of default tabs containing pairs of id => title
+	 * @since 3.8.8
+	 * @access private
+	 */
 	private static $default_tabs;
 
+	/**
+	 * Initialize default tabs and add necessary action hooks.
+	 *
+	 * @since 3.8.8
+	 *
+	 * @uses add_action() Attaches to wpsc_register_settings_tabs hook
+	 * @uses add_action() Attaches to wpsc_load_settings_tab_class hook
+	 *
+	 * @see wpsc_load_settings_page()
+	 *
+	 * @access public
+	 * @static
+	 */
 	public static function init() {
 		self::$default_tabs = array(
-			'general'      => _x( 'General', 'General settings tab in Settings->Store page', 'wpsc' ),
+			'general'      => _x( 'General'     , 'General settings tab in Settings->Store page'     , 'wpsc' ),
 			'presentation' => _x( 'Presentation', 'Presentation settings tab in Settings->Store page', 'wpsc' ),
-			'admin'        => _x( 'Admin', 'Admin settings tab in Settings->Store page', 'wpsc' ),
-			'taxes'        => _x( 'Taxes', 'Taxes settings tab in Settings->Store page', 'wpsc' ),
-			'shipping'     => _x( 'Shipping', 'Shipping settings tab in Settings->Store page', 'wpsc' ),
-			'gateway'      => _x( 'Payments', 'Payments settings tab in Settings->Store page', 'wpsc' ),
-			'checkout'     => _x( 'Checkout', 'Checkout settings tab in Settings->Store page', 'wpsc' ),
-			'marketing'    => _x( 'Marketing', 'Marketing settings tab in Settings->Store page', 'wpsc' ),
-			'import'       => _x( 'Import', 'Import settings tab in Settings->Store page', 'wpsc' )
+			'admin'        => _x( 'Admin'       , 'Admin settings tab in Settings->Store page'       , 'wpsc' ),
+			'taxes'        => _x( 'Taxes'       , 'Taxes settings tab in Settings->Store page'       , 'wpsc' ),
+			'shipping'     => _x( 'Shipping'    , 'Shipping settings tab in Settings->Store page'    , 'wpsc' ),
+			'gateway'      => _x( 'Payments'    , 'Payments settings tab in Settings->Store page'    , 'wpsc' ),
+			'checkout'     => _x( 'Checkout'    , 'Checkout settings tab in Settings->Store page'    , 'wpsc' ),
+			'marketing'    => _x( 'Marketing'   , 'Marketing settings tab in Settings->Store page'   , 'wpsc' ),
+			'import'       => _x( 'Import'      , 'Import settings tab in Settings->Store page'      , 'wpsc' )
 		);
 
-		add_action( 'wpsc_register_settings_tabs' , array( 'WPSC_Settings_Page', 'register_default_tabs' ), 1 );
+		add_action( 'wpsc_register_settings_tabs' , array( 'WPSC_Settings_Page', 'register_default_tabs'  ), 1 );
 		add_action( 'wpsc_load_settings_tab_class', array( 'WPSC_Settings_Page', 'load_default_tab_class' ), 1 );
 	}
 
+	/**
+	 * Get active object instance
+	 *
+	 * @since 3.8.8
+	 *
+	 * @access public
+	 * @static
+	 * @return object
+	 */
 	public static function get_instance() {
 		if ( ! self::$instance ) {
 			self::$instance = new WPSC_Settings_Page();
@@ -36,6 +161,19 @@ final class WPSC_Settings_Page
 		return self::$instance;
 	}
 
+	/**
+	 * Automatically load tab classes inside wpsc-admin/includes/settings-tabs.
+	 *
+	 * @since 3.8.8
+	 *
+	 * @see WPSC_Settings_Page::init()
+	 *
+	 * @uses WPSC_Settings_Page::get_current_tab_id() Gets current tab ID
+	 *
+	 * @access public
+	 * @param  object $page_instance The WPSC_Settings_Page instance
+	 * @static
+	 */
 	public static function load_default_tab_class( $page_instance ) {
 		$current_tab_id = $page_instance->get_current_tab_id();
 		if ( array_key_exists( $current_tab_id, self::$default_tabs ) ) {
@@ -43,22 +181,78 @@ final class WPSC_Settings_Page
 		}
 	}
 
+	/**
+	 * Register the default tabs' ids and titles.
+	 *
+	 * @since 3.8.8
+	 *
+	 * @see WPSC_Settings_Page::init()
+	 *
+	 * @uses WPSC_Settings_Page::register_tab() Registers default tabs' idds and titles.
+	 *
+	 * @access public
+	 * @param  object $page_instance The WPSC_Settings_Page instance
+	 * @static
+	 */
 	public static function register_default_tabs( $page_instance ) {
 		foreach ( self::$default_tabs as $id => $title ) {
 			$page_instance->register_tab( $id, $title );
 		}
 	}
 
+	/**
+	 * Current tab ID
+	 * @since 3.8.8
+	 * @access private
+	 * @var string
+	 */
 	private $current_tab_id;
+
+	/**
+	 * Current tab object
+	 * @since 3.8.8
+	 * @access private
+	 * @var object
+	 */
 	private $current_tab;
+
+	/**
+	 * An array containing registered tabs
+	 * @since 3.8.8
+	 * @access private
+	 * @var array
+	 */
 	private $tabs;
 
-	public function __construct( $tab_id = false ) {
+	/**
+	 * Constructor
+	 *
+	 * @since 3.8.8
+	 *
+	 * @uses do_action()   Calls wpsc_register_settings_tabs hook.
+	 * @uses apply_filters Calls wpsc_settings_tabs hook.
+	 * @uses WPSC_Settings_Page::set_current_tab() Set current tab to the specified ID
+	 *
+	 * @access public
+	 * @param string $tab_id Optional. If specified then the current tab will be set to this ID.
+	 */
+	public function __construct( $tab_id = null ) {
 		do_action( 'wpsc_register_settings_tabs', $this );
 		$this->tabs = apply_filters( 'wpsc_settings_tabs', $this->tabs );
 		$this->set_current_tab( $tab_id );
 	}
 
+	/**
+	 * Returns the current tab object
+	 *
+	 * @since 3.8.8
+	 *
+	 * @uses do_action()         Calls wpsc_load_settings_tab_class hook.
+	 * @uses WPSC_Settings_Tab() constructing a new settings tab object
+	 *
+	 * @access public
+	 * @return object WPSC_Settings_Tab object
+	 */
 	public function get_current_tab() {
 		if ( ! $this->current_tab ) {
 			do_action( 'wpsc_load_settings_tab_class', $this );
@@ -74,11 +268,30 @@ final class WPSC_Settings_Page
 		return $this->current_tab;
 	}
 
+	/**
+	 * Get current tab ID
+	 * @since  3.8.8
+	 * @access public
+	 * @return string
+	 */
 	public function get_current_tab_id() {
 		return $this->current_tab_id;
 	}
 
-	public function set_current_tab( $tab_id = false ) {
+	/**
+	 * Set current tab to the specified tab ID.
+	 *
+	 * @since 3.8.8
+	 *
+	 * @uses check_admin_referer() Prevent CSRF
+	 * @uses WPSC_Settings_Page::get_current_tab()        Initializes the current tab object.
+	 * @uses WPSC_Settings_Page::save_options()           Saves the submitted options to the database.
+	 * @uses WPSC_Settings_Tab::callback_submit_options() If this method exists in the tab object, it will be called after WPSC_Settings_Page::save_options().
+	 *
+	 * @access public
+	 * @param string $tab_id Optional. The Tab ID. If this is not specified, the $_GET['tab'] variable will be used. If that variable also does not exists, the first tab will be used.
+	 */
+	public function set_current_tab( $tab_id = null ) {
 		if ( ! $tab_id ) {
 			if ( isset( $_GET['tab'] ) && array_key_exists( $_GET['tab'], $this->tabs ) )
 				$this->current_tab_id = $_GET['tab'];
@@ -98,14 +311,37 @@ final class WPSC_Settings_Page
 		}
 	}
 
+	/**
+	 * Register a tab's ID and title
+	 *
+	 * @since 3.8.8
+	 *
+	 * @access public
+	 * @param  string $id    Tab ID.
+	 * @param  string $title Tab title.
+	 */
 	public function register_tab( $id, $title ) {
 		$this->tabs[$id] = $title;
 	}
 
+	/**
+	 * Get an array containing tabs' IDs and titles
+	 *
+	 * @since 3.8.8
+	 *
+	 * @access public
+	 * @return array
+	 */
 	public function get_tabs() {
 		return $this->tabs;
 	}
 
+	/**
+	 * Get the HTML class of a tab.
+	 * @since 3.8.8
+	 * @param  string $id Tab ID
+	 * @return string
+	 */
 	private function tab_class( $id ) {
 		$class = 'nav-tab';
 		if ( $id == $this->current_tab_id )
@@ -113,11 +349,23 @@ final class WPSC_Settings_Page
 		return $class;
 	}
 
+	/**
+	 * Get the form's submit (action) url.
+	 * @since 3.8.8
+	 * @access private
+	 * @return string
+	 */
 	private function submit_url() {
 		$location = add_query_arg( 'tab', $this->current_tab_id );
 		return $location;
 	}
 
+	/**
+	 * Output HTML of tab navigation.
+	 * @since 3.8.8
+	 * @access public
+	 * @uses esc_html Prevents xss
+	 */
 	public function output_tabs() {
 		?>
 			<h2 class="nav-tab-wrapper">
@@ -128,6 +376,13 @@ final class WPSC_Settings_Page
 		<?php
 	}
 
+	/**
+	 * Display the current tab.
+	 * @since 3.8.8
+	 * @uses do_action() Calls wpsc_{$current_tab_id}_settings_page hook.
+	 * @uses WPSC_Settings_Tab::display() Displays the tab.
+	 * @access public
+	 */
 	public function display_current_tab() {
 		?>
 			<div id="options_<?php echo esc_attr( $this->current_tab_id ); ?>">
@@ -142,6 +397,16 @@ final class WPSC_Settings_Page
 		<?php
 	}
 
+	/**
+	 * Display the settings page.
+	 * @since 3.8.8
+	 * @uses esc_html_e()     Sanitize HTML
+	 * @uses esc_attr()       Sanitize HTML attributes
+	 * @uses wp_nonce_field() Prevent CSRF
+	 * @uses WPSC_Settings_Page::output_tabs()         Display tab navigation.
+	 * @uses WPSC_Settings_Page::display_current_tab() Display current tab.
+	 * @access public
+	 */
 	public function display() {
 		?>
 			<div id="wpsc_options" class="wrap">
@@ -165,6 +430,15 @@ final class WPSC_Settings_Page
 		<?php
 	}
 
+	/**
+	 * Save submitted options to the database.
+	 * @since 3.8.8
+	 * @uses check_admin_referer() Prevents CSRF.
+	 * @uses update_option() Saves options to the database.
+	 * @uses wpdb::query() Queries the database.
+	 * @uses wpdb::get_col() Queries the database.
+	 * @access public
+	 */
 	private function save_options( $selected='' ) {
 		global $wpdb, $wpsc_gateways;
 		$updated = 0;
