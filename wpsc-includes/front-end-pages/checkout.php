05be15abba6dde1f5eca280386916af5a0dbbcb8
Gary Cao <garyc40@garyc40.com>
2012-02-20 01:17:52 +0900

    Form validation for first checkout step

--- wpsc-includes/front-end-pages/checkout.php
+++ wpsc-includes/front-end-pages/checkout.php
@@ -14,6 +14,37 @@ class WPSC_Front_End_Page_Checkout extends WPSC_Front_End_Page
 	public function get_current_step() {
 		return $this->current_step;
 	}
+
+	public function process_validate_details() {
+		$checkout_fields = WPSC_Checkout_Form::get()->get_fields();
+
+		$validation_rules = array();
+		foreach ( $checkout_fields as $field ) {
+			$rules = array();
+			if ( $field->mandatory )
+				$rules[] = 'required';
+
+			if ( $field->type == 'email' )
+				$rules[] = 'email';
+
+			if ( ! empty( $rules ) )
+				$validation_rules[$field->id] = array(
+					'title' => $field->name,
+					'rules' => $rules,
+				);
+		}
+
+		$validation = wpsc_validate_form( $validation_rules, $_POST['wpsc_checkout_details'] );
+
+		if ( is_wp_error( $validation ) ) {
+			$this->set_validation_errors( $validation );
+			$this->current_step = 'details';
+			return;
+		}
+
+		$this->current_step = 'payment-delivery';
+	}
+
 	public function main() {
 		$this->current_step = 'details';
 	}
