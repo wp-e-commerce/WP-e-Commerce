6f550b470ab1d196afa23264a88c1f04d8dd259f
Gary Cao <garyc40@garyc40.com>
2012-03-19 01:01:36 +0900

    Fix: Warning messages in permalinks tab.

--- wpsc-admin/settings-form.class.php
+++ wpsc-admin/settings-form.class.php
@@ -104,7 +104,7 @@ class WPSC_Settings_Form
 		foreach ( $options as $radio_value => $radio_label ) {
 			$radio_id  = $id . '-' . sanitize_title_with_dashes( $radio_value );
 			$checked   = $value == $radio_value;
-			$output   .= wpsc_form_radio( $name, $radio_value, $label, $checked, array( 'id' => $radio_id, 'class' => $class ), false );
+			$output   .= wpsc_form_radio( $name, $radio_value, $radio_label, $checked, array( 'id' => $radio_id, 'class' => $class ), false );
 		}
 
 		$output .= '<br />';
