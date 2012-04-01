47bca385f166ab33a1138b4528ffc70a00ca16a9
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2012-04-01 13:06:54 +0000

    Fix: Cannot change sales log satatus via AJAX.
    Fixes issue 1008.
    
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/branch-3.8.8@1689 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-admin/ajax-and-init.php
+++ wpsc-admin/ajax-and-init.php
@@ -779,7 +779,14 @@ function wpsc_purchlog_edit_status( $purchlog_id='', $purchlog_status='' ) {
 	}
 }
 
-add_action( 'wp_ajax_wpsc_change_purchase_log_status', 'wpsc_purchlog_edit_status' );
+function _wpsc_ajax_purchlog_edit_status() {
+	if ( ! wp_verify_nonce( $_POST['nonce'], 'wpsc_purchase_logs' ) )
+		die( '-1' );
+
+	wpsc_purchlog_edit_status( $_POST['id'], $_POST['new_status'] );
+}
+
+add_action( 'wp_ajax_wpsc_change_purchase_log_status', '_wpsc_ajax_purchlog_edit_status' );
 
 function wpsc_save_product_order() {
 	global $wpdb;
@@ -1219,7 +1226,6 @@ function prod_upload() {
 			// Save the data
 			$id = wp_insert_post( $attachment );
 		}
-
 		$deletion_url = wp_nonce_url( "admin.php?wpsc_admin_action=delete_file&amp;file_name={$attachment['post_title']}&amp;product_id={$product_id}", 'delete_file_' . $attachment['post_title'] );
 
 		$output .= '<tr class="wpsc_product_download_row ' . $class . '"  id="elect_product_file_row_id_' . $id . '">';
@@ -1227,7 +1233,7 @@ function prod_upload() {
 		$output .= '<td>' . wpsc_convert_byte( $file_size ) . '</td>';
 		$output .= '<td>.' . wpsc_get_extension( $attachment['post_title'] ) . '</td>';
 		$output .= "<td><a class='file_delete_button' href='{$deletion_url}' >" . _x( 'Delete', 'Digital Downliad UI row', 'wpsc' ) . "</a></td>";
-		$output .= '<td><a href=' .$file_url .'>' . _x( 'Download', 'Digital Downliad UI row', 'wpsc' ) . '</a></td>';				
+		$output .= '<td><a href=' .$file_url .'>' . _x( 'Download', 'Digital Downliad UI row', 'wpsc' ) . '</a></td>';
 		$output .= '</tr>';
 	}
 
@@ -1697,4 +1703,3 @@ function wpsc_delete_variant_from_products_page(){
 	exit();
 }
 add_action( 'wp_ajax_wpsc_delete_variant_from_products_page', 'wpsc_delete_variant_from_products_page' );
-?>
