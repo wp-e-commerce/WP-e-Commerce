a1f1068be330cf63f972584730065a64eaaa59e5
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2012-05-17 14:06:32 +0000

    Fix: PayPal currency converter is wrong.
    Fixes issue 1057.
    
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@1702 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-includes/currency_converter.inc.php
+++ wpsc-includes/currency_converter.inc.php
@@ -1,7 +1,7 @@
 <?php
-	
+
 	/*
-		CURRENCYCONVERTER 
+		CURRENCYCONVERTER
 		Date - Feb 23,2005
 		Author - Harish Chauhan
 		Email - harishc@ultraglobal.biz
@@ -9,7 +9,7 @@
 		ABOUT
 		This PHP script will use for conversion of currency.
 		you can find it is tricky but it is usefull.
-		
+
 		Modified by Brian Barnes to change from one service that was
 		not meant to be used from automated purposes to another that
 		had no such restriction
@@ -20,7 +20,7 @@
 		var $_amt=1;
 		var $_to="";
 		var $_from="";
-		var $_error=""; 
+		var $_error="";
 		function CURRENCYCONVERTER($amt=1,$to="",$from="")
 		{
 			$this->_amt=$amt;
@@ -31,26 +31,27 @@
 		{
 			return $this->_error;
 		}
-		
+
 		/**
 		 * Given all details converts currency amount
-		 * 
+		 *
 		 * @param $amt double
 		 *   The amount to convert.
-		 *   
+		 *
 		 * @param $to string
 		 *   The currency you wish to convert to.
-		 *   
+		 *
 		 * @param $from string
 		 *   The currency you are converting from.
 		 */
 		function convert($amt = NULL, $to = "", $from = ""){
-			
+
 			$amount = urlencode(round($amt,2));
 			$from_Currency = urlencode($from);
 			$to_Currency = urlencode($to);
-		
+
 			$url = "http://www.google.com/ig/calculator?hl=en&q=$amount$from_Currency=?$to_Currency";
+
 			$ch = curl_init();
 			$timeout = 0;
 			curl_setopt ($ch, CURLOPT_URL, $url);
@@ -61,13 +62,12 @@
 			if(empty($rawdata)){
 				throw new Exception('unable to connect to currency conversion service ');
 			}
-			
-			$data = explode('"', $rawdata);
-			$data = explode(' ', $data['3']);
-			$var = $data['0'];
-			$var = round($var,2);
-			return $var;
-			
+
+			$rawdata = preg_replace( '/(\{|,\s*)([^\s:]+)(\s*:)/', '$1"$2"$3', $rawdata );
+			$data = json_decode( $rawdata );
+			$to_amount = round( $data->rhs, 2 );
+			return $to_amount;
+
 		}
 	}
 ?>
