c462e513efe925842522ce1c1efe452725ddfcb7
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2012-07-23 23:01:27 +0000

    [init] wpsc_backup_theme
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@1881 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-admin/init.php
+++ wpsc-admin/init.php
@@ -685,3 +685,15 @@ function wpsc_force_flush_theme_transients() {
 }
 if ( isset( $_REQUEST['wpsc_flush_theme_transients'] ) && ( $_REQUEST['wpsc_flush_theme_transients'] == 'true' ) )
 	add_action( 'admin_init', 'wpsc_force_flush_theme_transients' );
+
+function wpsc_backup_theme() {
+	$wp_theme_path = get_stylesheet_directory();
+	wpsc_recursive_copy( $wp_theme_path, WPSC_THEME_BACKUP_DIR );
+	$_SESSION['wpsc_themes_backup'] = true;
+	$sendback = wp_get_referer();
+	wp_redirect( $sendback );
+
+	exit();
+}
+if ( isset( $_REQUEST['wpsc_admin_action'] ) && ( $_REQUEST['wpsc_admin_action'] == 'backup_themes' ) )
+	add_action( 'admin_init', 'wpsc_backup_theme' );
