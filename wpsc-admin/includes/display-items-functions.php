9eb1f73169aca6f6c4ad8e40a2bf1f61115f4eec
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2012-07-23 23:02:56 +0000

    move a bunch of variation taxonomy functions to display-items-functions.php
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@1884 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-admin/includes/display-items-functions.php
+++ wpsc-admin/includes/display-items-functions.php
@@ -1291,4 +1291,208 @@ function wpsc_remove_bulk_edit($options){
 add_action( 'quick_edit_custom_box', 'wpsc_quick_edit_boxes', 10, 2 );
 add_action( 'save_post', 'wpsc_save_quickedit_box' );
 add_action( 'bulk_actions-edit-wpsc-product', 'wpsc_remove_bulk_edit');
+
+/**
+ * If it doesn't exist, let's create a multi-dimensional associative array
+ * that will contain all of the term/price associations
+ *
+ * @param <type> $variation
+ */
+function variation_price_field( $variation ) {
+	$term_prices = get_option( 'term_prices' );
+
+	if ( is_object( $variation ) )
+		$term_id = $variation->term_id;
+
+	if ( empty( $term_prices ) || !is_array( $term_prices ) ) {
+
+		$term_prices = array( );
+		if ( isset( $term_id ) ) {
+			$term_prices[$term_id] = array( );
+			$term_prices[$term_id]["price"] = '';
+			$term_prices[$term_id]["checked"] = '';
+		}
+		add_option( 'term_prices', $term_prices );
+	}
+
+	if ( isset( $term_id ) && is_array( $term_prices ) && array_key_exists( $term_id, $term_prices ) )
+		$price = esc_attr( $term_prices[$term_id]["price"] );
+	else
+		$price = '';
+
+	if( !isset( $_GET['action'] ) ) {
+	?>
+	<div class="form-field">
+		<label for="variation_price"><?php _e( 'Variation Price', 'wpsc' ); ?></label>
+		<input type="text" name="variation_price" id="variation_price" style="width:50px;" value="<?php echo $price; ?>"><br />
+		<span class="description"><?php _e( 'You can list a default price here for this variation.  You can list a regular price (18.99), differential price (+1.99 / -2) or even a percentage-based price (+50% / -25%).', 'wpsc' ); ?></span>
+	</div>
+	<script type="text/javascript">
+		jQuery('#parent option:contains("   ")').remove();
+		jQuery('#parent').mousedown(function(){
+			jQuery('#parent option:contains("   ")').remove();
+		});
+	</script>
+	<?php
+	} else{
+	?>
+	<tr class="form-field">
+            <th scope="row" valign="top">
+		<label for="variation_price"><?php _e( 'Variation Price', 'wpsc' ); ?></label>
+            </th>
+            <td>
+		<input type="text" name="variation_price" id="variation_price" style="width:50px;" value="<?php echo $price; ?>"><br />
+		<span class="description"><?php _e( 'You can list a default price here for this variation.  You can list a regular price (18.99), differential price (+1.99 / -2) or even a percentage-based price (+50% / -25%).', 'wpsc' ); ?></span>
+            </td>
+	</tr>
+	<?php
+	}
+}
+add_action( 'wpsc-variation_edit_form_fields', 'variation_price_field' );
+add_action( 'wpsc-variation_add_form_fields', 'variation_price_field' );
+
+/*
+WordPress doesnt let you change the custom post type taxonomy form very easily
+Use Jquery to move the set variation (parent) field to the top and add a description
+*/
+function variation_set_field(){
 ?>
+	<script>
+		/* change the text on the variation set from (none) to new variation set*/
+		jQuery("#parent option[value='-1']").text("New Variation Set");
+		/* Move to the top of the form and add a description */
+		jQuery("#tag-name").parent().before( jQuery("#parent").parent().append('<p>Choose the Variation Set you want to add variants to. If your\'e creating a new variation set then select "New Variation Set"</p>') );
+		/*
+		create a small description about variations below the add variation / set title
+		we can then get rid of the big red danger warning
+		*/
+		( jQuery("div#ajax-response").after('<p>Variations allow you to create options for your products, for example if you\'re selling T-Shirts they will have a size option you can create this as a variation. Size will be the Variation Set name, and it will be a "New Variant Set". You will then create variants (small, medium, large) which will have the "Variation Set" of Size. Once you have made your set you can use the table on the right to manage them (edit, delete). You will be able to order your variants by draging and droping them within their Variation Set.</p>') );
+	</script>
+<?php
+}
+add_action( 'wpsc-variation_edit_form_fields', 'variation_set_field' );
+add_action( 'wpsc-variation_add_form_fields', 'variation_set_field' );
+
+
+function category_edit_form(){
+?>
+	<script type="text/javascript">
+
+	</script>
+<?php
+}
+
+function variation_price_field_check( $variation ) {
+
+	$term_prices = get_option( 'term_prices' );
+
+	if ( is_array( $term_prices ) && array_key_exists( $variation->term_id, $term_prices ) )
+		$checked = ($term_prices[$variation->term_id]["checked"] == 'checked') ? 'checked' : '';
+	else
+		$checked = ''; ?>
+
+	<tr class="form-field">
+		<th scope="row" valign="top"><label for="apply_to_current"><?php _e( 'Apply to current variations?', 'wpsc' ) ?></label></th>
+		<td>
+			<span class="description"><input type="checkbox" name="apply_to_current" id="apply_to_current" style="width:2%;" <?php echo $checked; ?> /><?php _e( 'By checking this box, the price rule you implement above will be applied to all variations that currently exist.  If you leave it unchecked, it will only apply to products that use this variation created or edited from now on.  Take note, this will apply this rule to <strong>every</strong> product using this variation.  If you need to override it for any reason on a specific product, simply go to that product and change the price.', 'wpsc' ); ?></span>
+		</td>
+	</tr>
+<?php
+}
+add_action( 'wpsc-variation_edit_form_fields', 'variation_price_field_check' );
+
+
+
+/**
+ * @todo - Should probably refactor this at some point - very procedural,
+ *		   WAY too many foreach loops for my liking :)  But it does the trick
+ *
+ * @param <type> $term_id
+ */
+function save_term_prices( $term_id ) {
+
+	// First - Saves options from input
+	if ( isset( $_POST['variation_price'] ) || isset( $_POST["apply_to_current"] ) ) {
+
+		$term_prices = get_option( 'term_prices' );
+
+		$term_prices[$term_id]["price"] = $_POST["variation_price"];
+		$term_prices[$term_id]["checked"] = (isset( $_POST["apply_to_current"] )) ? "checked" : "unchecked";
+
+		update_option( 'term_prices', $term_prices );
+	}
+
+	// Second - If box was checked, let's then check whether or not it was flat, differential, or percentile, then let's apply the pricing to every product appropriately
+	if ( isset( $_POST["apply_to_current"] ) ) {
+
+		//Check for flat, percentile or differential
+		$var_price_type = '';
+
+		if ( flat_price( $_POST["variation_price"] ) )
+			$var_price_type = 'flat';
+		elseif ( differential_price( $_POST["variation_price"] ) )
+			$var_price_type = 'differential';
+		elseif ( percentile_price( $_POST["variation_price"] ) )
+			$var_price_type = 'percentile';
+
+		//Now, find all products with this term_id, update their pricing structure (terms returned include only parents at this point, we'll grab relevent children soon)
+		$products_to_mod = get_objects_in_term( $term_id, "wpsc-variation" );
+		$product_parents = array( );
+
+		foreach ( (array)$products_to_mod as $get_parent ) {
+
+			$post = get_post( $get_parent );
+
+			if ( !$post->post_parent )
+				$product_parents[] = $post->ID;
+		}
+
+		//Now that we have all parent IDs with this term, we can get the children (only the ones that are also in $products_to_mod, we don't want to apply pricing to ALL kids)
+
+		foreach ( $product_parents as $parent ) {
+			$args = array(
+				'post_parent' => $parent,
+				'post_type' => 'wpsc-product'
+			);
+			$children = get_children( $args, ARRAY_A );
+
+			foreach ( $children as $childrens ) {
+				$parent = $childrens["post_parent"];
+				$children_ids[$parent][] = $childrens["ID"];
+				$children_ids[$parent] = array_intersect( $children_ids[$parent], $products_to_mod );
+			}
+		}
+
+		//Got the right kids, let's grab their parent pricing and modify their pricing based on var_price_type
+
+		foreach ( (array)$children_ids as $parents => $kids ) {
+
+			$kids = array_values( $kids );
+
+			foreach ( $kids as $kiddos ) {
+				$price = wpsc_determine_variation_price( $kiddos );
+				update_product_meta( $kiddos, 'price', $price );
+			}
+		}
+	}
+}
+add_action( 'edited_wpsc-variation', 'save_term_prices' );
+add_action( 'created_wpsc-variation', 'save_term_prices' );
+
+function wpsc_delete_variations( $postid ) {
+	$post = get_post( $postid );
+	if ( $post->post_type != 'wpsc-product' || $post->post_parent != 0 )
+		return;
+	$variations = get_posts( array(
+		'post_type' => 'wpsc-product',
+		'post_parent' => $postid,
+		'post_status' => 'any',
+		'numberposts' => -1,
+	) );
+
+	if ( ! empty( $variations ) )
+		foreach ( $variations as $variation ) {
+			wp_delete_post( $variation->ID, true );
+		}
+}
+add_action( 'delete_post', 'wpsc_delete_variations' );
