d27ac62ffe348a4d61b957c841e8e1d6daa7b558
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2012-08-06 04:33:54 +0000

    Fix: Colorbox Gallery doesn't work. Props Justin Sainton.
    Fixes issue 993.
    
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@1904 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-core/js/wpsc_colorbox.js
+++ wpsc-core/js/wpsc_colorbox.js
@@ -1,21 +1,14 @@
-jQuery(document).ready(function(){
-	jQuery('.imagecol').each(function(){
-		var t = jQuery(this).find('.wpcart_gallery .thickbox');
-		t.colorbox({
-			maxWidth :'90%',
-			maxHeight :'90%',
-			returnFocus : false
-		});
-		
-		jQuery(this).children('.thickbox').click(function(e){
-			var that = jQuery(this);
-			e.preventDefault();
-			t.each(function(){
-				if (jQuery(this).attr('href') == that.attr('href')) {
-					jQuery(this).click();
-					return;
-				}
+jQuery(document).ready(function($){
+
+	if( $.colorbox ) {
+
+		$('.imagecol').each(function(){
+			$( 'a.thickbox', $(this) ).colorbox({
+				maxWidth :'90%',
+				maxHeight :'90%',
+				returnFocus : false
 			});
 		});
-	});
+	}
+
 });
