7a54efaf720b5b15551637193fc08267e10336bc
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2012-08-13 02:43:02 +0000

    Fix issue with payment gateways and sessions.
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@1932 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-merchants/paypal-pro.merchant.php
+++ wpsc-merchants/paypal-pro.merchant.php
@@ -39,9 +39,9 @@ class wpsc_merchant_paypal_pro extends wpsc_merchant {
 	var $name              = '';
 	var $paypal_ipn_values = array( );
 
-	function wpsc_merchant_paypal_pro() {
+	function __construct( $purchase_id = null, $is_receiving = false ) {
 		$this->name = __( 'PayPal Pro 2.0', 'wpsc' );
-		parent::wpsc_merchant();
+		parent::wpsc_merchant( $purchase_id, $is_receiving );
 	}
 
 	function get_local_currency_code() {
