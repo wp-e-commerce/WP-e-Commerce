a2f1f91ea40595fdc4454ab6bba2d6525d5ced5e
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2012-08-14 13:35:46 +0000

    Fix: Pagination does not work with price range widget.
    Update issue 196
    Labels: -Milestone-3.8.9 Milestone-3.9 -Workflow-Commit Workflow-NeedsReview
    
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@1936 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-includes/breadcrumbs.class.php
+++ wpsc-includes/breadcrumbs.class.php
@@ -4,9 +4,9 @@
 * @return boolean - true if we have and use them, false otherwise
 */
 function wpsc_has_breadcrumbs() {
-	global $wpsc_breadcrumbs;	
+	global $wpsc_breadcrumbs;
 	$wpsc_breadcrumbs = new wpsc_breadcrumbs();
-	
+
 	if(($wpsc_breadcrumbs->breadcrumb_count > 0) && (get_option("show_breadcrumbs") == 1)){
 		return true;
 	} else {
@@ -19,7 +19,7 @@ function wpsc_has_breadcrumbs() {
 * @return boolean - true if we have breadcrumbs to loop through
 */
 function wpsc_have_breadcrumbs() {
-	global $wpsc_breadcrumbs;	
+	global $wpsc_breadcrumbs;
 
 	return $wpsc_breadcrumbs->have_breadcrumbs();
 }
@@ -29,27 +29,27 @@ function wpsc_have_breadcrumbs() {
 * @return nothing - iterate through the breadcrumbs
 */
 function wpsc_the_breadcrumb() {
-	global $wpsc_breadcrumbs;	
+	global $wpsc_breadcrumbs;
 
 	$wpsc_breadcrumbs->the_breadcrumb();
 }
 
 /**
 * wpsc breadcrumb name function
-* @return string - the breadcrumb name 
+* @return string - the breadcrumb name
 */
 function wpsc_breadcrumb_name() {
-	global $wpsc_breadcrumbs;	
+	global $wpsc_breadcrumbs;
 
 	return $wpsc_breadcrumbs->breadcrumb['name'];
 }
 
 /**
 * wpsc breadcrumb slug function
-* @return string - the breadcrumb slug - for use in the CSS ID 
+* @return string - the breadcrumb slug - for use in the CSS ID
 */
 function wpsc_breadcrumb_slug() {
-	global $wpsc_breadcrumbs;	
+	global $wpsc_breadcrumbs;
 
 	return (isset($wpsc_breadcrumbs->breadcrumb['slug']) ? $wpsc_breadcrumbs->breadcrumb['slug'] : '');
 }
@@ -59,7 +59,7 @@ function wpsc_breadcrumb_slug() {
 * @return string - the breadcrumb URL
 */
 function wpsc_breadcrumb_url() {
-	global $wpsc_breadcrumbs;	
+	global $wpsc_breadcrumbs;
 
 	if($wpsc_breadcrumbs->breadcrumb['url'] == '') {
 		return false;
@@ -73,7 +73,7 @@ function wpsc_breadcrumb_url() {
 * @return None - outputs breadcrumb HTML
 */
 function wpsc_output_breadcrumbs( $options = null ) {
-	
+
 	// Defaults
 	$options = apply_filters( 'wpsc_output_breadcrumbs_options', $options );
 	$options = wp_parse_args( (array)$options, array(
@@ -86,7 +86,7 @@ function wpsc_output_breadcrumbs( $options = null ) {
 		'show_products_page' => true,
 		'echo'               => true
 	) );
-	
+
 	$output = '';
 	$products_page_id = wpec_get_the_post_id_by_shortcode( '[productspage]' );
 	$products_page = get_post( $products_page_id );
@@ -98,7 +98,7 @@ function wpsc_output_breadcrumbs( $options = null ) {
 		'name' => apply_filters ( 'the_title', $products_page->post_title )
 	);
 	$filtered_products_page = apply_filters( 'wpsc_change_pp_breadcrumb', $filtered_products_page );
-	
+
 	// Home Page Crumb
 	// If home if the same as products page only show the products-page link and not the home link
 	if ( get_option( 'page_on_front' ) != $products_page_id && $options['show_home_page'] ) {
@@ -106,7 +106,7 @@ function wpsc_output_breadcrumbs( $options = null ) {
 		$output .= '<a class="wpsc-crumb" id="wpsc-crumb-home" href="' . get_option( 'home' ) . '">' . get_option( 'blogname' ) . '</a>';
 		$output .= $options['after-crumb'];
 	}
-	
+
 	// Products Page Crumb
 	if ( $options['show_products_page'] ) {
 		if ( !empty( $output ) ) {
@@ -116,7 +116,7 @@ function wpsc_output_breadcrumbs( $options = null ) {
 		$output .= '<a class="wpsc-crumb" id="wpsc-crumb-' . $products_page_id . '" href="' . $filtered_products_page['url'] . '">' . $filtered_products_page['name'] . '</a>';
 		$output .= $options['after-crumb'];
 	}
-	
+
 	// Remaining Crumbs
 	while ( wpsc_have_breadcrumbs() ) {
 		wpsc_the_breadcrumb();
@@ -141,7 +141,7 @@ function wpsc_output_breadcrumbs( $options = null ) {
 
 /**
  * wpsc_breadcrumbs class.
- * 
+ *
  */
 class wpsc_breadcrumbs {
 	var $breadcrumbs;
@@ -151,7 +151,7 @@ class wpsc_breadcrumbs {
 
 	/**
 	 * wpsc__breadcrumbs function.
-	 * 
+	 *
 	 * @access public
 	 * @return void
 	 */
@@ -159,13 +159,12 @@ class wpsc_breadcrumbs {
 		global $wp_query, $wpsc_query;
 		$this->breadcrumbs = array();
 		$query_data = Array();
-		if ( isset($wp_query->query_vars['post_type']) && 'wpsc-product' == $wp_query->query_vars['post_type'] && 1 == $wp_query->query_vars['posts_per_page'] && isset($wp_query->post)) 
+		if ( isset($wp_query->query_vars['post_type']) && 'wpsc-product' == $wp_query->query_vars['post_type'] && 1 == $wp_query->query_vars['posts_per_page'] && isset($wp_query->post) && is_single() )
 			$query_data['product'] = $wp_query->post->post_title;
 
-		if ( !empty($wpsc_query->query_vars['wpsc_product_category']) ) 
-			$query_data['category'] = $wpsc_query->query_vars['wpsc_product_category']; 
+		if ( !empty($wpsc_query->query_vars['wpsc_product_category']) )
+			$query_data['category'] = $wpsc_query->query_vars['wpsc_product_category'];
 
-		
 		if(!empty($query_data['product']) && !empty($wp_query->post)) {
 			$this->breadcrumbs[] = array(
 				'name' => htmlentities($wp_query->post->post_title, ENT_QUOTES, 'UTF-8'),
@@ -173,29 +172,29 @@ class wpsc_breadcrumbs {
 				'slug' => $query_data['product']
 			);
 		}
-		if(1 == $wp_query->post_count){
+		if( is_single() ){
 			$categories = wp_get_object_terms( $wp_query->post->ID , 'wpsc_product_category' );
 			//if product is associated w more than one category
 			if(count($categories) > 1 && isset($wpsc_query->query_vars['wpsc_product_category']))
 				$query_data['category'] = $wpsc_query->query_vars['wpsc_product_category'];
 			elseif(count($categories) > 0)
 				$query_data['category'] = $categories[0]->slug;
-			
-		}		
+
+		}
 		if( isset( $query_data['category'] ) )
 			$term_data = get_term_by('slug', $query_data['category'], 'wpsc_product_category');
 		else
 			$term_data = get_term_by('slug', 'uncategorized', 'wpsc_product_category');
-		
+
 		if( $term_data != false) {
 			$this->breadcrumbs[] = array(
 				'name' => htmlentities( $term_data->name, ENT_QUOTES, 'UTF-8'),
 				'url' => get_term_link( $term_data->slug, 'wpsc_product_category'),
 				'slug' => $term_data->slug
 			);
-			
+
 			$i = 0;
-			
+
 			while(($term_data->parent > 0) && ($i <= 20)) {
 				$term_data = get_term($term_data->parent, 'wpsc_product_category');
 				$this->breadcrumbs[] = array(
@@ -208,10 +207,10 @@ class wpsc_breadcrumbs {
 		$this->breadcrumbs = apply_filters( 'wpsc_breadcrumbs', array_reverse( $this->breadcrumbs ) );
 		$this->breadcrumb_count = count($this->breadcrumbs);
 	}
-	
+
 	/**
 	 * next_breadcrumbs function.
-	 * 
+	 *
 	 * @access public
 	 * @return void
 	 */
@@ -221,10 +220,10 @@ class wpsc_breadcrumbs {
 		return $this->breadcrumb;
 	}
 
-	
+
 	/**
 	 * the_breadcrumb function.
-	 * 
+	 *
 	 * @access public
 	 * @return void
 	 */
@@ -234,7 +233,7 @@ class wpsc_breadcrumbs {
 
 	/**
 	 * have_breadcrumbs function.
-	 * 
+	 *
 	 * @access public
 	 * @return void
 	 */
@@ -249,7 +248,7 @@ class wpsc_breadcrumbs {
 
 	/**
 	 * rewind_breadcrumbs function.
-	 * 
+	 *
 	 * @access public
 	 * @return void
 	 */
@@ -258,7 +257,7 @@ class wpsc_breadcrumbs {
 		if ($this->breadcrumb_count > 0) {
 			$this->breadcrumb = $this->breadcrumbs[0];
 		}
-	}	
+	}
 
 }
 
