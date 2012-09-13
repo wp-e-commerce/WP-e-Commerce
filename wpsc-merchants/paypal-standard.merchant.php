9dd5790fc9b8d4ec29366716b3ef04438fc132ed
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2012-09-13 02:01:48 +0000

    Fix: Restore 'wpsc_activated_subscription' action for backward compatibility.
    Fixes issue 1200.
    
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@2047 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-merchants/paypal-standard.merchant.php
+++ wpsc-merchants/paypal-standard.merchant.php
@@ -411,6 +411,7 @@ class wpsc_merchant_paypal_standard extends wpsc_merchant {
 					foreach($this->cart_items as $cart_row) {
 						if($cart_row['is_recurring'] == true) {
 							do_action('wpsc_activate_subscription', $cart_row['cart_item_id'], $this->paypal_ipn_values['subscr_id']);
+							do_action('wpsc_activated_subscription', $cart_row['cart_item_id'], $this );
 						}
 					}
 				break;
