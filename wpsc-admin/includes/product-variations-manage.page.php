d68c58d4b5796cf76e904cdac04494566072b479
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2012-10-03 14:04:03 +0000

    New: Pagination UI for product variations.
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@2116 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-admin/includes/product-variations-manage.page.php
+++ wpsc-admin/includes/product-variations-manage.page.php
@@ -1,7 +1,13 @@
 <?php $this->list_table->display_messages(); ?>
 <form action="" method="post">
 	<?php $this->list_table->views(); ?>
+	<div class="alignright actions wpsc-save-product-variations top">
+		<?php submit_button( __( 'Save Variations', 'wpsc' ) ); ?>
+	</div>
 	<input type="hidden" name="post_status" class="post_status_page" value="<?php echo !empty($_REQUEST['post_status']) ? esc_attr($_REQUEST['post_status']) : 'all'; ?>" />
 	<?php wp_nonce_field( 'wpsc_save_variations_meta', '_wpsc_save_meta_nonce' ); ?>
 	<?php $this->list_table->display(); ?>
+	<div class="alignright actions wpsc-save-product-variations bottom">
+		<?php submit_button( __( 'Save Variations', 'wpsc' ) ); ?>
+	</div>
 </form>
