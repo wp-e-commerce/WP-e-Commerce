04ca82f911bac6d27619fc99a9fc11c154f1d535
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2012-10-23 10:00:48 +0000

    Fix: post_status is not formatted correctly in wpsc_start_the_query().
    Apparently, 'post_status' accepts either an array, or a comma-
    delimited list of post status. But make sure that there's no spaces
    between the commas and the actual statuses.
    
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@2146 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-widgets/latest_product_widget.php
+++ wpsc-widgets/latest_product_widget.php
@@ -156,7 +156,7 @@ function wpsc_latest_product( $args = null, $instance ) {
 				$output .= '</div>';
 			}
 			// Link
-			$output .= '<a href="' . esc_url( wpsc_product_url( $latest_product->ID, null ) ) . '" class="wpsc-product-title">'.esc_html( $latest_product->post_title ).'</a>';
+			$output .= '<a href="' . esc_url( wpsc_product_url( $latest_product->ID, null ) ) . '" class="wpsc-product-title">'. apply_filters( 'the_title', $latest_product->post_title ).'</a>';
 			$output .= '</li>';
 		}
 		$output .= "</ul>";
