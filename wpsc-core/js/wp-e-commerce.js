691b7c64d951de1c56f594626a4c6377d0cf1828
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2012-11-27 03:28:18 +0000

    Use .text() instead of .html() when inserting placeholders for form fields.
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@2218 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-core/js/wp-e-commerce.js
+++ wpsc-core/js/wp-e-commerce.js
@@ -572,11 +572,7 @@ function wpsc_set_profile_country(html_form_id, form_id) {
 jQuery(document).ready(function(){
 	jQuery('.wpsc_checkout_table input, .wpsc_checkout_table textarea').each(function(){
 		var real_value = jQuery(this).val();
-		value = jQuery('label[for="'+jQuery(this).attr('id')+'"]').html();
-		if(null != value){
-			value = value.replace(/<span class="?asterix"?>\*<\/span>/i,'');
-		}
-
+		value = jQuery('label[for="'+jQuery(this).attr('id')+'"]').text();
 		if( jQuery.fn.inlineFieldLabel )
 		    jQuery(this).inlineFieldLabel({label:jQuery.trim(value)});
 		if(real_value != '')
