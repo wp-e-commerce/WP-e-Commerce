3061f8e49ab6286d6732998f8d1246035e784092
garyc40@garyc40.com <garyc40@garyc40.com@7bdd534d-2f5a-8b57-a18c-7e4837153298>
2012-12-05 14:39:56 +0000

    Fix: Can't add to cart on some server.
    Some server chokes when method_exists() is called when $method
    is null in this context:
    method_exists( $wpsc_shipping_modules[$method], "get_item_shipping"  )
    
    Server just chokes, no error message. PHP segfaults.
    
    Vast majority of servers are fine and walk over that condition merrily.
    
    git-svn-id: https://wp-e-commerce.googlecode.com/svn/branches/3.8@2235 7bdd534d-2f5a-8b57-a18c-7e4837153298

--- wpsc-includes/cart.class.php
+++ wpsc-includes/cart.class.php
@@ -1871,7 +1871,7 @@ class wpsc_cart_item {
       if($method === null)
         $method = $this->cart->selected_shipping_method;
 
-      if(method_exists( $wpsc_shipping_modules[$method], "get_item_shipping"  ))
+      if( $method && isset( $wpsc_shipping_modules[$method] ) && method_exists( $wpsc_shipping_modules[$method], "get_item_shipping"  ))
          $shipping = $wpsc_shipping_modules[$method]->get_item_shipping($this);
 
       if($method == $this->cart->selected_shipping_method && !empty( $shipping ) )
