db109f1c440f547432cbc96206dbc4494a1b376a
Gary Cao <garyc40@garyc40.com>
2012-12-11 16:10:07 +0900

    Fix: Decouple merchant components' configuration UI from settings page.

--- wpsc-admin/includes/settings-tabs/gateway.php
+++ wpsc-admin/includes/settings-tabs/gateway.php
@@ -14,62 +14,7 @@ class WPSC_Settings_Tab_Gateway extends WPSC_Settings_Tab
 	}
 
 	private function get_gateway_form( $selected_gateway ) {
-		global $nzshpcrt_gateways;
-
-		$payment_gateway_names = get_option('payment_gateway_names');
-		$return                = false;
-		$selected_gateway_data = false;
-		foreach ( $nzshpcrt_gateways as $gateway ) {
-			if ( $gateway['internalname'] == $selected_gateway ) {
-				$selected_gateway_data = $gateway;
-				break;
-			}
-		}
-
-		if ( $selected_gateway_data ) {
-			if ( array_key_exists( $selected_gateway, $payment_gateway_names ) ) {
-				$display_name = $payment_gateway_names[$selected_gateway];
-			} elseif ( ! empty( $selected_gateway_data['display_name'] ) ) {
-				$display_name = $selected_gateway_data['display_name'];
-			} else {
-				switch($selected_gateway_data['payment_type']) {
-					case "paypal";
-						$display_name = __( 'PayPal', 'wpsc' );
-						break;
-
-					case "manual_payment":
-						$display_name = __( 'Manual Payment', 'wpsc' );
-						break;
-
-					case "google_checkout":
-						$display_name = __( 'Google Checkout', 'wpsc' );
-						break;
-
-					case "credit_card":
-					default:
-						$display_name = __( 'Credit Card', 'wpsc' );
-						break;
-				}
-			}
-
-			ob_start();
-			?>
-				<tr>
-					<td><?php esc_html_e( 'Display Name', 'wpsc' ); ?></td>
-					<td>
-						<input type="text" name="user_defined_name[<?php echo esc_attr( $selected_gateway ); ?>]" value="<?php echo esc_html( $display_name ); ?>" /><br />
-						<small><?php esc_html_e( 'The text that people see when making a purchase.', 'wpsc' ); ?></small>
-					</td>
-				</tr>
-			<?php
-			$output = ob_get_clean();
-			$return = array(
-				'name'        => $selected_gateway_data['name'],
-				'form_fields' => $output . $selected_gateway_data['form'](),
-			);
-		}
-
-		return $return;
+		return apply_filters( 'wpsc_settings_gateway_form', array(), $selected_gateway );
 	}
 
 	private function get_gateway_settings_url( $gateway ) {
@@ -90,8 +35,9 @@ class WPSC_Settings_Tab_Gateway extends WPSC_Settings_Tab
 		$payment_data = $this->get_gateway_form( $selected_gateway );
 		if ( ! $payment_data ) {
 			$payment_data = array(
-				'name'        => __( 'Edit Gateway Settings', 'wpsc' ),
-				'form_fields' => __( 'Modify a payment gateway settings by clicking "Edit" link on the left.', 'wpsc' ),
+				'name'              => __( 'Edit Gateway Settings', 'wpsc' ),
+				'form_fields'       => __( 'Modify a payment gateway settings by clicking "Edit" link on the left.', 'wpsc' ),
+				'has_submit_button' => 1,
 			);
 		}
 		?>
@@ -102,9 +48,11 @@ class WPSC_Settings_Tab_Gateway extends WPSC_Settings_Tab
 			<table class='form-table'>
 				<?php echo $payment_data['form_fields']; ?>
 			</table>
-			<div class='submit'>
-				<input type='submit' value='<?php _e( 'Update &raquo;', 'wpsc' ) ?>' />
-			</div>
+			<?php if ( empty( $payment_data['has_submit_button'] ) ) : ?>
+				<div class='submit'>
+					<input type='submit' value='<?php _e( 'Update &raquo;', 'wpsc' ) ?>' />
+				</div>
+			<?php endif ?>
 		</div>
 </td>
 		<?php
@@ -137,27 +85,7 @@ class WPSC_Settings_Tab_Gateway extends WPSC_Settings_Tab
 							<div class='inside'>
 								<p><?php _e( 'Activate the payment gateways that you want to make available to your customers by selecting them below.', 'wpsc' ); ?></p>
 								<br />
-								<?php
-								$selected_gateways = get_option( 'custom_gateway_options' );
-								foreach ( $nzshpcrt_gateways as $gateway ) {
-									if ( isset( $gateway['admin_name'] ) )
-										$gateway['name'] = $gateway['admin_name'];
-								?>
-
-									<div class="wpsc-select-gateway">
-										<div class='wpsc-gateway-actions'>
-											<span class="edit">
-													<a class='edit-payment-module' data-gateway-id="<?php echo esc_attr( $gateway['internalname'] ); ?>" title="<?php esc_attr_e( "Edit this Payment Gateway's Settings", 'wpsc' ) ?>" href='<?php echo esc_url( $this->get_gateway_settings_url( $gateway['internalname'] ) ); ?>'><?php esc_html_e( 'Edit', 'wpsc' ); ?></a>
-													<img src="<?php echo esc_url( admin_url( 'images/wpspin_light.gif' ) ); ?>" class="ajax-feedback" title="" alt="" />
-											</span>
-									</div>
-									<p>
-										<input name='wpsc_options[custom_gateway_options][]' <?php checked( in_array( $gateway['internalname'], (array) $selected_gateways ) ); ?> type='checkbox' value='<?php echo esc_attr( $gateway['internalname'] ); ?>' id='<?php echo esc_attr( $gateway['internalname'] ); ?>_id' />
-										<label for='<?php echo esc_attr( $gateway['internalname'] ); ?>_id'><?php echo esc_attr( $gateway['name'] ); ?></label>
-									</p>
-								</div>
-					<?php }
-								?>
+								<?php $this->gateway_list(); ?>
 								<div class='submit gateway_settings'>
 									<input type='submit' value='<?php esc_attr_e( 'Update &raquo;', 'wpsc' ) ?>' name='updateoption' />
 								</div>
@@ -176,5 +104,31 @@ class WPSC_Settings_Tab_Gateway extends WPSC_Settings_Tab
 		</div>
 
 	<?php
+	}
+
+	private function gateway_list_item( $gateway ) {
+		$checked = in_array( $gateway['id'], $this->active_gateways );
+		?>
+			<div class="wpsc-select-gateway">
+				<div class='wpsc-gateway-actions'>
+					<span class="edit">
+							<a class='edit-payment-module' data-gateway-id="<?php echo esc_attr( $gateway['id'] ); ?>" title="<?php esc_attr_e( "Edit this Payment Gateway's Settings", 'wpsc' ) ?>" href='<?php echo esc_url( $this->get_gateway_settings_url( $gateway['id'] ) ); ?>'><?php esc_html_e( 'Edit', 'wpsc' ); ?></a>
+							<img src="<?php echo esc_url( admin_url( 'images/wpspin_light.gif' ) ); ?>" class="ajax-feedback" title="" alt="" />
+					</span>
+				</div>
+				<p>
+					<input name='wpsc_options[custom_gateway_options][]' <?php checked( $checked ); ?> type='checkbox' value='<?php echo esc_attr( $gateway['id'] ); ?>' id='<?php echo esc_attr( $gateway['id'] ); ?>_id' />
+					<label for='<?php echo esc_attr( $gateway['id'] ); ?>_id'><?php echo esc_html( $gateway['name'] ); ?></label>
+				</p>
+			</div>
+		<?php
+	}
+
+	private function gateway_list() {
+		$gateways = apply_filters( 'wpsc_settings_get_gateways', array() );
+
+		foreach ( $gateways as $gateway ) {
+			$this->gateway_list_item( $gateway );
+		}
 	}
 }
