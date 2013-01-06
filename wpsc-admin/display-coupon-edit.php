89df4a0052cb93914baa75e80ed0fb5c599d721f
Pippin Williamson <pippin@pippinsplugins.com>
2013-01-05 19:53:48 -0600

    Finished up initial edit screen rebuild

--- wpsc-admin/display-coupon-edit.php
+++ wpsc-admin/display-coupon-edit.php
@@ -6,7 +6,6 @@ if( !defined( 'ABSPATH' ) )
 
 $coupon_id = absint( $_GET['coupon'] );
 $coupon    = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM `" . WPSC_TABLE_COUPON_CODES . "` WHERE `id` = %d", $coupon_id ), ARRAY_A );
-echo '<pre>'; print_r( $coupon ); echo '</pre>';
 ?>
 <div class="wrap" id+"coupon_data">
 	<div id="edit_coupon_box">
@@ -157,79 +156,81 @@ echo '<pre>'; print_r( $coupon ); echo '</pre>';
 							<label for="edit_use-x-times"><strong><?php _e( 'Conditions', 'wpsc' ); ?></strong></label>
 						</th>
 						<td>
-							<div class='coupon_condition' >
-								<div class='first_condition'>
-									<select class="ruleprops" name="rules[property][]">
-										<option value="item_name" rel="order"><?php _e( 'Item name', 'wpsc' ); ?></option>
-										<option value="item_quantity" rel="order"><?php _e( 'Item quantity', 'wpsc' ); ?></option>
-										<option value="total_quantity" rel="order"><?php _e( 'Total quantity', 'wpsc' ); ?></option>
-										<option value="subtotal_amount" rel="order"><?php _e( 'Subtotal amount', 'wpsc' ); ?></option>
-										<?php echo apply_filters( 'wpsc_coupon_rule_property_options', '' ); ?>
-									</select>
+							<?php
+							$conditions = maybe_unserialize( $coupon['condition'] );
+							foreach( $conditions as $condition ) : ?>
+							<div class='coupon_condition'>
+								<select class="ruleprops" name="rules[property][]">
+									<option value="item_name"<?php selected( 'item_name', $condition['property'] ); ?> rel="order"><?php _e( 'Item name', 'wpsc' ); ?></option>
+									<option value="item_quantity"<?php selected( 'item_quantity', $condition['property'] ); ?> rel="order"><?php _e( 'Item quantity', 'wpsc' ); ?></option>
+									<option value="total_quantity"<?php selected( 'total_quantity', $condition['property'] ); ?> rel="order"><?php _e( 'Total quantity', 'wpsc' ); ?></option>
+									<option value="subtotal_amount"<?php selected( 'subtotal_amount', $condition['property'] ); ?> rel="order"><?php _e( 'Subtotal amount', 'wpsc' ); ?></option>
+									<?php do_action( 'wpsc_coupon_rule_property_options' ); ?>
+								</select>
 
-									<select name="rules[logic][]">
-										<option value="equal"><?php _e( 'Is equal to', 'wpsc' ); ?></option>
-										<option value="greater"><?php _e( 'Is greater than', 'wpsc' ); ?></option>
-										<option value="less"><?php _e( 'Is less than', 'wpsc' ); ?></option>
-										<option value="contains"><?php _e( 'Contains', 'wpsc' ); ?></option>
-										<option value="not_contain"><?php _e( 'Does not contain', 'wpsc' ); ?></option>
-										<option value="begins"><?php _e( 'Begins with', 'wpsc' ); ?></option>
-										<option value="ends"><?php _e( 'Ends with', 'wpsc' ); ?></option>
-										<option value="category"><?php _e( 'In Category', 'wpsc' ); ?></option>
-									</select>
+								<select name="rules[logic][]">
+									<option value="equal"<?php selected( 'equal', $condition['logic'] ); ?>><?php _e( 'Is equal to', 'wpsc' ); ?></option>
+									<option value="greater"<?php selected( 'greater', $condition['logic'] ); ?>><?php _e( 'Is greater than', 'wpsc' ); ?></option>
+									<option value="less"<?php selected( 'less', $condition['logic'] ); ?>><?php _e( 'Is less than', 'wpsc' ); ?></option>
+									<option value="contains"<?php selected( 'contains', $condition['logic'] ); ?>><?php _e( 'Contains', 'wpsc' ); ?></option>
+									<option value="not_contain"<?php selected( 'not_contain', $condition['logic'] ); ?>><?php _e( 'Does not contain', 'wpsc' ); ?></option>
+									<option value="begins"<?php selected( 'begins', $condition['logic'] ); ?>><?php _e( 'Begins with', 'wpsc' ); ?></option>
+									<option value="ends"<?php selected( 'ends', $condition['logic'] ); ?>><?php _e( 'Ends with', 'wpsc' ); ?></option>
+									<option value="category"<?php selected( 'category', $condition['logic'] ); ?>><?php _e( 'In Category', 'wpsc' ); ?></option>
+								</select>
 
-									<input type="text" name="rules[value][]" style="width: 300px;"/>
+								<input type="text" name="rules[value][]" value="<?php esc_attr_e( $condition['value'] ); ?>" style="width: 300px;"/>
+							</div>
+							<?php endforeach; ?>
+							<script>
+								var coupon_number=1;
+								function edit_another_property(this_button){
+									var new_property='<div class="coupon_condition">\n'+
+										'<select class="ruleprops" name="rules[property][]"> \n'+
+										'<option value="item_name" rel="order">Item name</option> \n'+
+										'<option value="item_quantity" rel="order">Item quantity</option>\n'+
+										'<option value="total_quantity" rel="order">Total quantity</option>\n'+
+										'<option value="subtotal_amount" rel="order">Subtotal amount</option>\n'+
+										'<?php echo apply_filters( 'wpsc_coupon_rule_property_options', '' ); ?>'+
+										'</select> \n'+
+										'<select name="rules[logic][]"> \n'+
+										'<option value="equal">Is equal to</option> \n'+
+										'<option value="greater">Is greater than</option> \n'+
+										'<option value="less">Is less than</option> \n'+
+										'<option value="contains">Contains</option> \n'+
+										'<option value="not_contain">Does not contain</option> \n'+
+										'<option value="begins">Begins with</option> \n'+
+										'<option value="ends">Ends with</option> \n'+
+										'</select> \n'+
+										'<span> \n'+
+										'<input type="text" name="rules[value][]" style="width:300px"/> \n'+
+										'</span>  \n'+
+										'<img height="16" width="16" class="delete" alt="Delete" src="<?php echo WPSC_CORE_IMAGES_URL; ?>/cross.png" onclick="jQuery(this).parent().remove();"/>\n' +
+										'</div> ';
 
-									<script>
-										var coupon_number=1;
-										function edit_another_property(this_button){
-											var new_property='<div class="coupon_condition">\n'+
-												'<select class="ruleprops" name="rules[property][]"> \n'+
-												'<option value="item_name" rel="order">Item name</option> \n'+
-												'<option value="item_quantity" rel="order">Item quantity</option>\n'+
-												'<option value="total_quantity" rel="order">Total quantity</option>\n'+
-												'<option value="subtotal_amount" rel="order">Subtotal amount</option>\n'+
-												'<?php echo apply_filters( 'wpsc_coupon_rule_property_options', '' ); ?>'+
-												'</select> \n'+
-												'<select name="rules[logic][]"> \n'+
-												'<option value="equal">Is equal to</option> \n'+
-												'<option value="greater">Is greater than</option> \n'+
-												'<option value="less">Is less than</option> \n'+
-												'<option value="contains">Contains</option> \n'+
-												'<option value="not_contain">Does not contain</option> \n'+
-												'<option value="begins">Begins with</option> \n'+
-												'<option value="ends">Ends with</option> \n'+
-												'</select> \n'+
-												'<span> \n'+
-												'<input type="text" name="rules[value][]" style="width:300px"/> \n'+
-												'</span>  \n'+
-												'<img height="16" width="16" class="delete" alt="Delete" src="<?php echo WPSC_CORE_IMAGES_URL; ?>/cross.png" onclick="jQuery(this).parent().remove();"/>\n' +
-												'</div> ';
+									jQuery('.coupon_condition :last').after(new_property);
+									coupon_number++;
+								}
 
-											jQuery('.coupon_condition :first').after(new_property);
-											coupon_number++;
-										}
+								//displays the free shipping options
+								function show_shipping_options() {
+									var discount_type = document.getElementById("edit_discount_type").value;
+									if (discount_type == "2") {
+										document.getElementById("free_shipping_options").style.display='block';
+										document.getElementById("discount_amount").style.display='none';
+									}else{
+										document.getElementById("free_shipping_options").style.display='none';
+										document.getElementById("discount_amount").style.display='table-row';
+									}
+								}
 
-										//displays the free shipping options
-										function show_shipping_options() {
-											var discount_type = document.getElementById("edit_discount_type").value;
-											if (discount_type == "2") {
-												document.getElementById("free_shipping_options").style.display='block';
-												document.getElementById("discount_amount").style.display='none';
-											}else{
-												document.getElementById("free_shipping_options").style.display='none';
-												document.getElementById("discount_amount").style.display='table-row';
-											}
-										}
+								//need to send the selected country off via ajax to return the region select box for that country
+								function show_region_list(){
+									var country_id = document.getElementById("coupon_country_list").value;
+								}
 
-										//need to send the selected country off via ajax to return the region select box for that country
-										function show_region_list(){
-											var country_id = document.getElementById("coupon_country_list").value;
-										}
-
-									</script>
-								</div>
-							</div><br/>
+							</script>
+							<br/>
 							<a class="wpsc_coupons_condition_add button-secondary" onclick="edit_another_property(jQuery(this));">
 								<?php _e( 'Add New Condition', 'wpsc' ); ?>
 							</a>
