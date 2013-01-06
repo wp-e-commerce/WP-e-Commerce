f1dd5441be323d43fba003e78971b6fca137b968
Pippin Williamson <pippin@pippinsplugins.com>
2013-01-06 00:37:57 -0600

    Use new list table for coupon display

--- wpsc-admin/display-coupons.php
+++ wpsc-admin/display-coupons.php
@@ -13,6 +13,26 @@ function wpsc_display_coupons_page() {
 
 	} else {
 
+
+		require_once WPSC_FILE_PATH . '/wpsc-admin/includes/coupon-list-table-class.php';
+		$coupons_table = new WPSC_Coupons_List_Table();
+		$coupons_table->prepare_items();
+	?>
+	<div class="wrap">
+		<h2><?php _e( 'Coupons', 'wpsc' ); ?></h2>
+		<?php do_action( 'wpsc_coupons_page_top' ); ?>
+		<form id="wpsc-coupons-filter" method="get" action="<?php echo admin_url( 'edit.php?post_type=wpsc-product&page=wpsc-edit-coupons' ); ?>">
+
+			<input type="hidden" name="post_type" value="wpsc-product" />
+			<input type="hidden" name="page" value="wpsc-edit-coupons" />
+
+			<?php $coupons_table->views() ?>
+			<?php $coupons_table->display() ?>
+		</form>
+		<?php do_action( 'wpsc_coupons_page_bottom' ); ?>
+	</div>
+	<?php
+
 		if ( isset( $_POST ) && is_array( $_POST ) && !empty( $_POST ) ) {
 
 			if ( isset( $_POST['add_coupon'] ) && (!isset( $_POST['is_edit_coupon'] ) || !($_POST['is_edit_coupon'] == 'true')) ) {
@@ -128,148 +148,18 @@ function wpsc_display_coupons_page() {
 				$conditions[]         = $new_cond;
 
 				$wpdb->update(
-					    WPSC_TABLE_COUPON_CODES,
-					    array(
-						'condition' => serialize( $conditions )
-					    ),
-					    array(
-						'id' => $_POST['coupon_id']
-					    ),
-					    '%s',
-					    '%d'
-					);
-			}
-		} ?>
-
-		<script type='text/javascript'>
-			jQuery(".pickdate").datepicker();
-			/* jQuery datepicker selector */
-			if (typeof jQuery('.pickdate').datepicker != "undefined") {
-				jQuery('.pickdate').datepicker({ dateFormat: 'yy-mm-dd' });
-			}
-		</script>
-
-		<div class="wrap">
-			<h2>
-				<?php _e( 'Coupons', 'wpsc' ); ?>
-				<a href="<?php echo add_query_arg( 'view', 'add' ); ?>" id="add_coupon_box_link" class="add_item_link button add-new-h2">
-					<?php _e( 'Add New', 'wpsc' ); ?>
-				</a>
-			</h2>
-
-			<?php
-				$columns = array(
-					'coupon_code' => __( 'Coupon Code', 'wpsc' ),
-					'discount' => __( 'Discount', 'wpsc' ),
-					'start' => __( 'Start', 'wpsc' ),
-					'expiry' => __( 'Expiry', 'wpsc' ),
-					'active' => __( 'Active', 'wpsc' ),
-					'apply_on_prods' => __( 'Apply On All Products', 'wpsc' ),
-					'edit' => __( 'Edit', 'wpsc' )
+				    WPSC_TABLE_COUPON_CODES,
+				    array(
+					'condition' => serialize( $conditions )
+				    ),
+				    array(
+					'id' => $_POST['coupon_id']
+				    ),
+				    '%s',
+				    '%d'
 				);
-				register_column_headers( 'display-coupon-details', $columns );
-			?>
-
-			<table class="wp-list-table widefat fixed coupon-list" cellspacing="0">
-				<thead>
-					<tr>
-						<?php print_column_headers( 'display-coupon-details' ); ?>
-					</tr>
-				</thead>
-
-				<tfoot>
-					<tr>
-						<?php print_column_headers( 'display-coupon-details', false ); ?>
-					</tr>
-				</tfoot>
-
-				<tbody>
-					<?php
-						$i = 1;
-						$coupon_data = $wpdb->get_results( "SELECT * FROM `" . WPSC_TABLE_COUPON_CODES . "` ", ARRAY_A );
-						if( $coupon_data ) :
-							foreach ( (array)$coupon_data as $coupon ) {
-								$alternate = "";
-								$i++;
-								if ( ($i % 2) != 0 ) {
-									$alternate = "class='alternate'";
-								}
-								echo "<tr $alternate>\n\r";
-
-								echo "    <td>\n\r";
-								esc_attr_e( $coupon['coupon_code'] );
-								echo "    </td>\n\r";
-
-								echo "    <td>\n\r";
-								if ( $coupon['is-percentage'] == 1 )
-									echo esc_attr( $coupon['value'] ) . "%";
-
-								else if ( $coupon['is-percentage'] == 2 ){
-									if(!empty($coupon['free-shipping']))
-										echo __("Free Shipping - With Conditions ", 'wpsc');
-									else
-										echo __("Free Shipping - Global", 'wpsc');
-
-								}
-								else
-									echo wpsc_currency_display( esc_attr( $coupon['value'] ) );
-
-								echo "    </td>\n\r";
-
-								echo "    <td>\n\r";
-								echo date( "d/m/Y", strtotime( esc_attr( $coupon['start'] ) ) );
-								echo "    </td>\n\r";
-
-								echo "    <td>\n\r";
-								echo date( "d/m/Y", strtotime( esc_attr( $coupon['expiry'] ) ) );
-								echo "    </td>\n\r";
-
-								echo "    <td>\n\r";
-								switch ( $coupon['active'] ) {
-									case 1:
-										echo "<img src='" . WPSC_CORE_IMAGES_URL . "/yes_stock.gif' alt='' title='' />";
-										break;
-
-									case 0: default:
-										echo "<img src='" . WPSC_CORE_IMAGES_URL . "/no_stock.gif' alt='' title='' />";
-										break;
-								}
-								echo "    </td>\n\r";
-
-								echo "    <td>\n\r";
-								switch ( $coupon['every_product'] ) {
-									case 1:
-										echo "<img src='" . WPSC_CORE_IMAGES_URL . "/yes_stock.gif' alt='' title='' />";
-										break;
-
-									case 0: default:
-										echo "<img src='" . WPSC_CORE_IMAGES_URL . "/no_stock.gif' alt='' title='' />";
-										break;
-								}
-
-								echo "    </td>\n\r";
-								echo "    <td>\n\r";
-								echo "<a title='" . esc_attr( $coupon['coupon_code'] ). "' href='" . add_query_arg( array( 'coupon' => $coupon['id'], 'view' => 'edit' ) ) . "' rel='" . $coupon['id'] . "'>" . __( 'Edit', 'wpsc' ) . "</a>";
-								echo "    </td>\n\r";
-								echo "  </tr>\n\r";
-							}
-						else : ?>
-						<tr>
-							<td colspn="7"><?php _e( 'No coupon codes created yet.', 'wpsc' ); ?></td>
-						</tr>
-						<?php
-						endif;
-					?>
-				</tbody>
-			</table>
-
-			<p style='margin: 10px 0px 5px 0px;'>
-				<?php _e( '<strong>Note:</strong> Due to a current PayPal limitation, when a purchase is made using a coupon we cannot send a detailed list of items through for processing. Instead we send the total amount of the purchase so the customer will see your shop name and the total within PayPal.', 'wpsc' ); ?>
-			</p>
-
-		</div>
-
-	<?php
+			}
+		}
 
 	} // end view check
 
