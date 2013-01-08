e153ae87f204d349d0778c8b7ce0b0030420862c
Gary Cao <garyc40@garyc40.com>
2013-01-08 21:20:06 +0900

    Change: When displaying packing slip, form data should be output in the same order as that of checkout page.
    Props @ifjustone.
    Closes #5.

--- wpsc-admin/admin-form-functions.php
+++ wpsc-admin/admin-form-functions.php
@@ -281,7 +281,7 @@ function wpsc_packing_slip( $purchase_id ) {
 
 
 		if($input_data != null) {
-			$form_data = $wpdb->get_results("SELECT * FROM `".WPSC_TABLE_CHECKOUT_FORMS."` WHERE `active` = '1'",ARRAY_A);
+			$form_data = $wpdb->get_results( "SELECT * FROM `".WPSC_TABLE_CHECKOUT_FORMS."` WHERE `active` = '1' ORDER BY `checkout_order`" , ARRAY_A );
 
 			foreach($form_data as $form_field) {
 
