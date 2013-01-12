e49b45cc6e2a7cc26934caf2c55321d043081a3b
Gary Cao <garyc40@garyc40.com>
2013-01-12 12:02:22 +0900

    Refactor wpsc-account-downloads.php so that it contains less logic and more template.
    Also added HTML classes to the table rows, make sure the table has
    <thead> and <tbody>.

--- wpsc-theme/wpsc-account-downloads.php
+++ wpsc-theme/wpsc-account-downloads.php
@@ -7,50 +7,34 @@
  * @package WPSC
  * @since WPSC 3.8.10
  */
-global $files, $products; ?>
+?>
 
-<?php if ( wpsc_has_downloads() ) : ?>
-
-	<table class="logdisplay">
-		<tr>
-			<th><?php _e( 'File Names', 'wpsc' ); ?> </th>
-			<th><?php _e( 'Downloads Left', 'wpsc' ); ?> </th>
-			<th><?php _e( 'Date', 'wpsc' ); ?> </th>
-		</tr>
-
-		<?php
-			$i = 0;
-			foreach ( (array) $files as $file ) :
-
-				$alternate = "";
-
-				if ( ( $i % 2 ) != 1 )
-					$alternate = "class='alt'";
-		?>
-
-				<tr <?php echo $alternate; ?>>
-					<td>
-		<?php
-			if ( $products[$i]['downloads'] > 0 )
-
-				echo "<a href = " . get_option('siteurl')."?downloadid=".$products[$i]['uniqueid'] . ">" . $file['post_title'] . "</a>";
-			else
-				echo $file['post_title'] . "";
-
-		?>
-
-					</td>
-					<td><?php echo $products[$i]['downloads']; ?></td>
-					<td><?php echo date( get_option( "date_format" ), strtotime( $products[$i]['datetime'] ) ); ?></td>
-				</tr>
-		<?php
-				$i++;
-			endforeach;
-		?>
-
-	</table>
-<?php else : ?>
-
+<?php if ( empty( $items ) ) : ?>
 	<?php _e( 'You have not purchased any downloadable products yet.', 'wpsc' ); ?>
+<?php else : ?>
+	<table class="logdisplay">
+		<thead>
+			<tr>
+				<th class="wpsc-user-log-file-name" scope="col"><?php _e( 'File Names', 'wpsc' ); ?> </th>
+				<th class="wpsc-user-log-downloads-left" scope="col"><?php _e( 'Downloads Left', 'wpsc' ); ?> </th>
+				<th class="wpsc-user-log-file-date" scope="col"><?php _e( 'Date', 'wpsc' ); ?> </th>
+			</tr>
+		</thead>
 
+		<tbody>
+			<?php foreach( $items as $key => $item ): ?>
+				<tr class="wpsc-user-log-file<?php echo ( $key %2 == 1 ) ? '' : ' alt'; ?>">
+					<td class="wpsc-user-log-file-name">
+						<?php echo $item->title; ?>
+					</td>
+					<td class="wpsc-user-log-downloads-left">
+						<?php echo esc_html( $item->downloads ); ?>
+					</td>
+					<td class="wpsc-user-log-file-date">
+						<?php echo esc_html( $item->datetime ); ?>
+					</td>
+				</tr>
+			<?php endforeach; ?>
+		</tbody>
+	</table>
 <?php endif; ?>
