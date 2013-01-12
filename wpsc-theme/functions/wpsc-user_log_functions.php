6a5504247495068665eb33d78b744c45dab82338
Gary Cao <garyc40@garyc40.com>
2013-01-12 11:28:43 +0900

    Use add_query_arg() instead of url concatenation in wpsc_user_profile_links().

--- wpsc-theme/functions/wpsc-user_log_functions.php
+++ wpsc-theme/functions/wpsc-user_log_functions.php
@@ -395,11 +395,13 @@ function wpsc_user_profile_links( $args = array() ) {
 	echo $args['before_link_list'];
 
 	$i = 0;
+	$user_account_url = get_option( 'user_account_url' );
 	foreach ( $profile_tabs as $tab_id => $tab_title ) :
 		echo $args['before_link_item'];
+		$tab_url = add_query_arg( 'tab', $tab_id, $user_account_url );
 		printf(
 			'<a href="%1$s" class="%2$s">%3$s</a>',
-			esc_url( get_option( 'user_account_url' ) . $separator . 'tab=' . $tab_id ),
+			esc_url( $tab_url ),
 			esc_attr( $current_tab == $tab_id ? 'current' : '' ),
 			$tab_title
 		);
