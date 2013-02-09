d8aef65572aef9b6420335c21f993e8ce0f649b2
curtismchale <curtis@curtismchale.ca>
2013-02-09 15:12:31 -0800

    dropping braces to fit in with standards

--- wpsc-includes/theme.functions.php
+++ wpsc-includes/theme.functions.php
@@ -455,9 +455,8 @@ function wpsc_the_category_title( $title='', $id='' ){
 
 	global $wp_query;
 
-	if ( ! empty( $id ) ){
+	if ( ! empty( $id ) )
 		_wpsc_deprecated_argument( __FUNCTION__, '3.8.10', 'The $id param is no longer used' );
-	}
 
 	if ( ! in_the_loop() || _wpsc_is_in_custom_loop() )
 		return $title;
@@ -487,6 +486,7 @@ function wpsc_the_category_title( $title='', $id='' ){
 	} else {
 		return $title;
 	} // ! empty( $category->name )
+
 }
 
 /**
