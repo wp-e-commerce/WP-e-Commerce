34d17378c4eb43e6b7c71ad261cec381b3435765
curtismchale <curtis@curtismchale.ca>
2013-02-08 20:39:00 -0800

    - adding $id deprecated
    Making sure that the $id param, which isn't used, tosses an error for
    the user.

--- wpsc-includes/theme.functions.php
+++ wpsc-includes/theme.functions.php
@@ -455,6 +455,10 @@ function wpsc_the_category_title( $title='', $id='' ){
 
 	global $wp_query;
 
+	if ( ! empty( $id ) ){
+		_wpsc_deprecated_argument( __FUNCTION__, '3.8.10', 'The $id param is no longer used' );
+	}
+
 	if ( ! in_the_loop() || _wpsc_is_in_custom_loop() )
 		return $title;
 
