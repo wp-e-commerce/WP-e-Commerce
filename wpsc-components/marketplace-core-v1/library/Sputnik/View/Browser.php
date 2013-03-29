42692f32baa93126c2b5e321dccb7a7429108f64
Justin Sainton <justinsainton@gmail.com>
2013-03-28 23:40:02 -0700

    Adding marketplace as component

---
+++ wpsc-components/marketplace-core-v1/library/Sputnik/View/Browser.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Product browser view
+ *
+ * @package Sputnik
+ * @subpackage Admin View
+ */
+
+/**
+ * Product browser view
+ *
+ * @package Sputnik
+ * @subpackage Admin View
+ */
+class Sputnik_View_Browser extends Sputnik_View {
+	protected $title = 'Browse';
+
+	protected $view = null;
+	public $view_type = 'grid';
+	protected $connect_error = false;
+
+	public function __construct() {
+		$class = 'Sputnik_View_Browser_Grid';
+		if (!empty($_REQUEST['view'])) {
+			switch ($_REQUEST['view']) {
+				case 'list':
+					$class = 'Sputnik_View_Browser_List';
+					break;
+				case 'grid':
+				default:
+					$class = 'Sputnik_View_Browser_Grid';
+					break;
+			}
+		}
+
+		$this->view = new $class();
+		$this->view->parent = $this;
+		$this->view->prepare_items();
+	}
+
+	public function get_view_url($view) {
+		$bits = array('tab' => $view);
+		if ($this->view->id !== 'grid') {
+			$bits['view'] = $this->view->id;
+		}
+		return Sputnik_Admin::build_url($bits);
+	}
+
+	protected function display() {
+		$this->view->views();
+		$this->view->display();
+	}
+
+	public function get_tabs() {
+		return array();
+
+		global $tab;
+		$tabs = array();
+		$tabs['dashboard'] = __( 'Search', 'sputnik' );
+		if ( 'search' == $tab )
+			$tabs['search']	= __( 'Search Results', 'sputnik' );
+		$tabs['featured'] = _x( 'Featured', 'Plugin Installer', 'sputnik' );
+		$tabs['popular']  = _x( 'Popular', 'Plugin Installer', 'sputnik' );
+		$tabs['new']      = _x( 'Newest', 'Plugin Installer', 'sputnik' );
+		$tabs['updated']  = _x( 'Recently Updated', 'Plugin Installer', 'sputnik' );
+		$tabs['price']    = _x( 'Lowest Priced', 'Plugin Installer', 'sputnik' );
+		return $tabs;
+	}
+
+	public function get_data($tab) {
+		global $paged;
+		try {
+			switch ( $tab ) {
+				case 'search':
+					$term = isset( $_REQUEST['s'] ) ? stripslashes( $_REQUEST['s'] ) : '';
+					$api = Sputnik_API::search($term);
+					break;
+
+				case 'account':
+					$api = Sputnik_API::get_purchased();
+					break;
+
+				case 'featured':
+				case 'popular':
+				case 'new':
+				case 'updated':
+				case 'price':
+				default:
+					$api = Sputnik_API::get_all($paged, array('browse' => $tab));
+					break;
+			}
+		}
+		catch (Exception $e) {
+			$this->connect_error = true;
+			return false;
+		}
+
+		return array('items' => $api['body'], 'pages' => $api['headers']['x-pagecount']);
+	}
+
+	public function no_items() {
+		echo '<p>' . __( 'No plugins match your request.', 'sputnik' ) . '</p>';
+	}
+
+	public function footer() {
+?>
+	<script type="text/html" id="tmpl-sputnik-modal">
+		<div class="sputnik-modal">
+			<h3 class="sputnik-modal-title">{{ title }}</h3>
+			<a class="sputnik-modal-close" href="" title="<?php esc_attr_e('Close'); ?>">&times;</a>
+		</div>
+		<div class="sputnik-modal-backdrop">
+			<div></div>
+		</div>
+	</script>
+<?php
+
+		parent::footer();
+	}
+}
---
+++ wpsc-components/marketplace-core-v1/library/Sputnik/View/Info.php
@@ -0,0 +1,202 @@
+<?php
+/**
+ * Plugin information view
+ *
+ * @package Sputnik
+ * @subpackage Admin View
+ */
+
+/**
+ * Plugin information view
+ *
+ * @package Sputnik
+ * @subpackage Admin View
+ */
+class Sputnik_View_Info extends Sputnik_View_Mini {
+	protected $body_id = 'sputnik-plugin-information';
+
+	protected $plugin;
+	protected $api;
+
+	public function __construct() {
+		parent::__construct( __('Plugin Information', 'sputnik') );
+		$this->plugin = $_GET['info'];
+
+		try {
+			$account = Sputnik::get_account();
+			$this->api = Sputnik::get_plugin($this->plugin, $account->ID);
+		}
+		catch (Exception $e) {
+			status_header(500);
+			iframe_header( __('', 'sputnik') );
+			echo $e->getMessage();
+			iframe_footer();
+			die();
+		}
+	}
+	public function display() {
+		global $tab;
+		require_once(ABSPATH . 'wp-admin/includes/plugin-install.php');
+
+		$api = $this->api;
+
+		$plugins_allowedtags = array('a' => array('href' => array(), 'title' => array(), 'target' => array()),
+									'abbr' => array('title' => array()), 'acronym' => array('title' => array()),
+									'code' => array(), 'pre' => array(), 'em' => array(), 'strong' => array(),
+									'div' => array(), 'p' => array(), 'ul' => array(), 'ol' => array(), 'li' => array(),
+									'h1' => array(), 'h2' => array(), 'h3' => array(), 'h4' => array(), 'h5' => array(), 'h6' => array(),
+									'img' => array('src' => array(), 'class' => array(), 'alt' => array()));
+
+		$plugins_section_titles = array(
+			'description'  => _x('Description',  'Plugin installer section title', 'sputnik'),
+			'installation' => _x('Installation', 'Plugin installer section title', 'sputnik'),
+			'faq'          => _x('FAQ',          'Plugin installer section title', 'sputnik'),
+			'screenshots'  => _x('Screenshots',  'Plugin installer section title', 'sputnik'),
+			'changelog'    => _x('Changelog',    'Plugin installer section title', 'sputnik'),
+			'other_notes'  => _x('Other Notes',  'Plugin installer section title', 'sputnik')
+		);
+		//Sanitize HTML
+		$api->sections = (array) $api->sections;
+		$api->author = links_add_target($api->author, '_blank');
+		foreach ( $api->sections as $section_name => $content )
+			$api->sections[$section_name] = wp_kses($content, $plugins_allowedtags);
+
+		$api->screenshots = (array) $api->screenshots;
+		foreach ( $api->screenshots as &$data ) {
+			if (!isset($data->caption) || !isset($data->location)) {
+				continue;
+			}
+
+			$data->caption = wp_kses($data->caption, $plugins_allowedtags);
+			$data->location = esc_url($data->location, array('http', 'https'));
+		}
+		unset($data);
+
+		foreach ( array( 'version', 'requires', 'tested', 'homepage', 'downloaded', 'slug' ) as $key ) {
+			if ( isset( $api->$key ) )
+				$api->$key = wp_kses( $api->$key, $plugins_allowedtags );
+		}
+
+		$section = isset($_REQUEST['section']) ? stripslashes( $_REQUEST['section'] ) : 'description'; //Default to the Description tab, Do not translate, API returns English.
+		if ( empty($section) || (!isset($api->sections[ $section ]) && ($section !== 'screenshots' || empty($api->screenshots)))  )
+			$section = array_shift( $section_titles = array_keys((array)$api->sections) );
+
+?>
+		<div class="alignleft fyi">
+			<h1><?php echo $api->name ?></h1>
+			<?php if ( ! empty($api->download_link) && ( current_user_can('install_plugins') || current_user_can('update_plugins') ) ) : ?>
+			<p class="action-button">
+<?php
+			$status = Sputnik_Admin::install_status($api);
+			switch ( $status['status'] ) {
+				case 'purchase':
+				default:
+					if ( $status['url'] )
+						echo '<a href="' . $status['url'] . '" target="_parent" class="button-primary buy">' . sprintf(__('<span>$%.2f</span> Buy &amp; Install', 'sputnik'), $api->price) . '</a>';
+					break;
+				case 'install':
+					if ( $status['url'] )
+						echo '<a href="' . $status['url'] . '" class="button-primary install" title="' . __('You have already purchased, install now', 'sputnik') . '">' . __('Install Now', 'sputnik') . '</a>';
+					break;
+				case 'update_available':
+					if ( $status['url'] )
+						echo '<a href="' . $status['url'] . '" class="button-primary install">' . __('Install Update Now', 'sputnik') .'</a>';
+					break;
+				case 'newer_installed':
+					echo '<a>' . sprintf(__('Newer Version (%s) Installed', 'sputnik'), $status['version']) . '</a>';
+					break;
+				case 'latest_installed':
+					echo '<a>' . __('Latest Version Installed', 'sputnik') . '</a>';
+					break;
+			}
+?>
+			</p>
+			<?php endif; ?>
+<?php
+		echo "<div id='plugin-information-header'>\n";
+		echo "<ul id='sidemenu'>\n";
+		foreach ( (array)$api->sections as $section_name => $content ) {
+			if ( isset( $plugins_section_titles[ $section_name ] ) )
+				$title = $plugins_section_titles[ $section_name ];
+			else
+				$title = ucwords( str_replace( '_', ' ', $section_name ) );
+
+			$class = ( $section_name == $section ) ? ' class="current"' : '';
+			$href = add_query_arg( array('tab' => $tab, 'section' => $section_name) );
+			$href = esc_url($href);
+			$san_section = esc_attr($section_name);
+			echo "\t<li><a name='$san_section' href='$href'$class>$title</a></li>\n";
+		}
+		if (!empty($api->screenshots)) {
+			$title = $plugins_section_titles['screenshots'];
+			$class = ( 'screenshots' == $section ) ? ' class="current"' : '';
+			$href = add_query_arg( array('tab' => $tab, 'section' => 'screenshots') );
+			$href = esc_url($href);
+			echo "\t<li><a name='screenshots' href='$href'$class>$title</a></li>\n";
+		}
+		echo "</ul>\n";
+		echo "</div>\n";
+?>
+			<h2 class="mainheader"><?php /* translators: For Your Information */ _e('FYI', 'sputnik') ?></h2>
+			<ul>
+	<?php if ( ! empty($api->version) ) : ?>
+				<li><strong><?php _e('Version:', 'sputnik') ?></strong> <?php echo $api->version ?></li>
+	<?php endif; if ( ! empty($api->author) ) : ?>
+				<li><strong><?php _e('Author:', 'sputnik') ?></strong> <?php echo $api->author ?></li>
+	<?php endif; if ( ! empty($api->last_updated) ) : ?>
+				<li><strong><?php _e('Last Updated:', 'sputnik') ?></strong> <span title="<?php echo $api->last_updated ?>"><?php
+								printf( __('%s ago', 'sputnik'), human_time_diff(strtotime($api->last_updated)) ) ?></span></li>
+	<?php endif; if ( ! empty($api->requires) ) : ?>
+				<li><strong><?php _e('Requires WordPress Version:', 'sputnik') ?></strong> <?php printf(__('%s or higher', 'sputnik'), $api->requires) ?></li>
+	<?php endif; if ( ! empty($api->tested) ) : ?>
+				<li><strong><?php _e('Compatible up to:', 'sputnik') ?></strong> <?php echo $api->tested ?></li>
+	<?php endif; if ( ! empty($api->downloaded) ) : ?>
+				<li><strong><?php _e('Downloaded:', 'sputnik') ?></strong> <?php printf(_n('%s time', '%s times', $api->downloaded, 'sputnik'), number_format_i18n($api->downloaded)) ?></li>
+	<?php endif; if ( ! empty($api->homepage) ) : ?>
+				<li><a target="_blank" href="<?php echo $api->homepage ?>"><?php _e('Plugin Homepage  &#187;', 'sputnik') ?></a></li>
+	<?php endif; ?>
+			</ul>
+
+		</div>
+		<div id="section-holder" class="wrap">
+		<?php
+			if ( !empty($api->tested) && version_compare( substr($GLOBALS['wp_version'], 0, strlen($api->tested)), $api->tested, '>') )
+				echo '<div class="updated"><p>' . __('<strong>Warning:</strong> This plugin has <strong>not been tested</strong> with your current version of WordPress.', 'sputnik') . '</p></div>';
+
+			else if ( !empty($api->requires) && version_compare( substr($GLOBALS['wp_version'], 0, strlen($api->requires)), $api->requires, '<') )
+				echo '<div class="updated"><p>' . __('<strong>Warning:</strong> This plugin has <strong>not been marked as compatible</strong> with your version of WordPress.', 'sputnik') . '</p></div>';
+
+			foreach ( $api->sections as $section_name => $content ) {
+				if ( isset( $plugins_section_titles[ $section_name ] ) )
+					$title = $plugins_section_titles[ $section_name ];
+				else
+					$title = ucwords( str_replace( '_', ' ', $section_name ) );
+
+				$content = links_add_base_url($content, $api->permalink);
+				$content = links_add_target($content, '_blank');
+
+				$san_section = esc_attr($title);
+
+				$display = ( $section_name == $section ) ? 'block' : 'none';
+
+				echo "\t<div id='section-{$san_section}' class='section' style='display: {$display};'>\n";
+				echo "\t\t<h2 class='long-header'>$title</h2>";
+				echo $content;
+				echo "\t</div>\n";
+			}
+
+			if (!empty($api->screenshots)) {
+				$display = ( 'screenshots' == $section ) ? 'block' : 'none';
+				echo "\t<div id='section-screenshots' class='section' style='display: {$display};'>\n";
+				echo "\t\t<h2 class='long-header'>Screenshots</h2>\n";
+				echo "\t\t<ol>\n";
+				foreach ($api->screenshots as $data) {
+					echo "\t\t\t<li><img src='{$data->location}' class='screenshot' /><p>{$data->caption}</p></li>\n";
+				}
+				echo "\t\t</ol>\n";
+				echo "\t</div>\n";
+			}
+
+		echo "</div>\n";
+	}
+}
