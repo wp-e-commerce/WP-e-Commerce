9ba69a74710b3f73339bf8ad61c4550b352e31aa
Brett Taylor <brett@webfroot.co.nz>
2013-04-10 15:10:52 +1200

    move payment gateway settings inline, removing the settings sidebar.

--- wpsc-admin/includes/settings-tabs/gateway.php
+++ wpsc-admin/includes/settings-tabs/gateway.php
@@ -23,16 +23,17 @@ class WPSC_Settings_Tab_Gateway extends WPSC_Settings_Tab {
 		$location = add_query_arg( array(
 			'tab'                => 'gateway',
 			'page'               => 'wpsc-settings',
-			'payment_gateway_id' => $gateway,
+			'payment_gateway_id' => $gateway || "",
 		), $location );
 		return $location;
 	}
 
-	public function display_payment_gateway_settings_form() {
-		$selected_gateway = (string) get_user_option( 'wpsc_settings_selected_payment_gateway', get_current_user_id() );
-		if ( empty( $selected_gateway ) && ! empty( $this->active_gateways ) )
-			$selected_gateway = $this->active_gateways[0];
-
+	public function display_payment_gateway_settings_form( $selected_gateway = null ) {
+		if ( ! $selected_gateway ) {
+			$selected_gateway = (string) get_user_option( 'wpsc_settings_selected_payment_gateway', get_current_user_id() );
+			if ( empty( $selected_gateway ) && ! empty( $this->active_gateways ) )
+				$selected_gateway = $this->active_gateways[0];
+		}
 		$payment_data = $this->get_gateway_form( $selected_gateway );
 		if ( ! $payment_data ) {
 			$payment_data = array(
@@ -41,19 +42,22 @@ class WPSC_Settings_Tab_Gateway extends WPSC_Settings_Tab {
 				'has_submit_button' => 1,
 			);
 		}
+
 		?>
-		<td id='wpsc-payment-gateway-settings-panel' class='wpsc-module-settings' rowspan='2'>
-			<div class='postbox'>
-			<h3 class='hndle'><?php echo $payment_data['name']; ?></h3>
-			<div class='inside'>
+		<div id="gateway_settings_<?php echo esc_attr( $selected_gateway ); ?>_form" class='gateway_settings_form'>
 			<table class='form-table'>
 				<?php echo $payment_data['form_fields']; ?>
 			</table>
-			<?php if ( empty( $payment_data['has_submit_button'] ) ) : ?>
-				<?php submit_button( __( 'Save Changes' ) ); ?>
-			<?php endif ?>
+			<?php // hidden because most gateways provide their own update button. ?>
+			<?php if ($payment_data['has_submit_button'] !== 1) { ?>
+				<!--
+				<div class='submit'>
+					<input type='submit' value='<?php _e( 'Update &raquo;', 'wpsc' ) ?>' />
+				</div>
+				-->
+				<?php submit_button( __( 'Update &raquo;', 'wpsc' ) ); ?>
+			<?php } ?>
 		</div>
-</td>
 		<?php
 	}
 
@@ -63,48 +67,54 @@ class WPSC_Settings_Tab_Gateway extends WPSC_Settings_Tab {
 			$nzshpcrt_gateways     = nzshpcrt_get_gateways();
 	?>
 
-		<div class='metabox-holder'>
+		<!-- <div class='metabox-holder'> -->
 			<table id='wpsc-payment-gateway-settings' class='wpsc-edit-module-options'>
 				<tr>
 					<td>
-						<div class='postbox'>
+						<!-- <div class='postbox'> -->
 							<h3 class='hndle'><?php _e( 'Select Payment Gateways', 'wpsc' ); ?></h3>
 							<div class='inside'>
 								<p><?php _e( 'Activate the payment gateways that you want to make available to your customers by selecting them below.', 'wpsc' ); ?></p>
 								<br />
 								<?php $this->gateway_list(); ?>
 								<?php submit_button( __( 'Save Changes' ) ); ?>
-								</div>
 							</div>
+						<!-- </div> -->
 
-								<h4><?php _e( 'We Recommend', 'wpsc' ); ?></h4>
-								<a style="border-bottom:none;" href="https://www.paypal.com/nz/mrb/pal=LENKCHY6CU2VY" target="_blank"><img src="<?php echo WPSC_CORE_IMAGES_URL; ?>/paypal-referal.gif" border="0" alt="<?php esc_attr_e( 'Sign up for PayPal and start accepting credit card payments instantly.', 'wpsc' ); ?>" /></a> <br /><br />
-								<a style="border-bottom:none;" href="http://checkout.google.com/sell/?promo=seinstinct" target="_blank"><img src="https://checkout.google.com/buyer/images/google_checkout.gif" border="0" alt="<?php esc_attr_e( 'Sign up for Google Checkout', 'wpsc' ); ?>" /></a>
+						<h4><?php _e( 'We Recommend', 'wpsc' ); ?></h4>
+						<a style="border-bottom:none;" href="https://www.paypal.com/nz/mrb/pal=LENKCHY6CU2VY" target="_blank"><img src="<?php echo WPSC_CORE_IMAGES_URL; ?>/paypal-referal.gif" border="0" alt="<?php esc_attr_e( 'Sign up for PayPal and start accepting credit card payments instantly.', 'wpsc' ); ?>" /></a> <br /><br />
+						<a style="border-bottom:none;" href="http://checkout.google.com/sell/?promo=seinstinct" target="_blank"><img src="https://checkout.google.com/buyer/images/google_checkout.gif" border="0" alt="<?php esc_attr_e( 'Sign up for Google Checkout', 'wpsc' ); ?>" /></a>
 
-							</td>
-
-							<?php $this->display_payment_gateway_settings_form(); ?>
+					</td>
 				</tr>
 			</table>
-		</div>
+		<!-- </div> -->
 
 	<?php
 	}
 
-	private function gateway_list_item( $gateway ) {
+	private function gateway_list_item( $gateway, $force ) {
 		$checked = in_array( $gateway['id'], $this->active_gateways );
+		$hidden = ( $checked || $force ? "" : "style='display: none;'" );
+		$edithidden = ( $checked || $force ? "style='display: none;'" : "");
 		?>
-			<div class="wpsc-select-gateway">
+			<div class="wpsc-select-gateway" id="gateway_list_item_<?php echo $gateway['id'];?>">
 				<div class='wpsc-gateway-actions'>
 					<span class="edit">
-							<a class='edit-payment-module' data-gateway-id="<?php echo esc_attr( $gateway['id'] ); ?>" title="<?php esc_attr_e( "Edit this Payment Gateway's Settings", 'wpsc' ) ?>" href='<?php echo esc_url( $this->get_gateway_settings_url( $gateway['id'] ) ); ?>'><?php esc_html_e( 'Edit', 'wpsc' ); ?></a>
-							<img src="<?php echo esc_url( admin_url( 'images/wpspin_light.gif' ) ); ?>" class="ajax-feedback" title="" alt="" />
+						<a class='edit-payment-module' data-gateway-id="<?php echo esc_attr( $gateway['id'] ); ?>" title="<?php esc_attr_e( "Edit this Payment Gateway's Settings", 'wpsc' ) ?>" href='<?php echo esc_url( $this->get_gateway_settings_url( $gateway['id'] ) ); ?>' <?php echo $edithidden; ?>><?php esc_html_e( 'Edit', 'wpsc' ); ?></a>
+						<img src="<?php echo esc_url( admin_url( 'images/wpspin_light.gif' ) ); ?>" class="ajax-feedback" title="" alt="" />
 					</span>
 				</div>
 				<p>
 					<input name='wpsc_options[custom_gateway_options][]' <?php checked( $checked ); ?> type='checkbox' value='<?php echo esc_attr( $gateway['id'] ); ?>' id='<?php echo esc_attr( $gateway['id'] ); ?>_id' />
 					<label for='<?php echo esc_attr( $gateway['id'] ); ?>_id'><?php echo esc_html( $gateway['name'] ); ?></label>
 				</p>
+				<div id="wpsc_gateway_settings_<?php echo esc_attr( $gateway['id'] ); ?>" class='gateway_settings' <?php echo $hidden; ?> >
+					<a class="edit-payment-module-cancel" data-gateway-id="<?php echo esc_attr( $gateway['id'] ); ?>" title="<?php esc_attr_e( "Cancel editing this Payment Gateway's settings", 'wpsc' ) ?>" href='<?php echo esc_url( $this->get_gateway_settings_url( '' ) ); ?>'><?php esc_html_e( 'Cancel', 'wpsc' ); ?></a>
+					<?php if ( $checked || $force ) {
+						$this->display_payment_gateway_settings_form( $gateway['id'] );
+					} ?>
+				</div>
 			</div>
 		<?php
 	}
@@ -112,8 +122,13 @@ class WPSC_Settings_Tab_Gateway extends WPSC_Settings_Tab {
 	private function gateway_list() {
 		$gateways = apply_filters( 'wpsc_settings_get_gateways', array() );
 
+		$selected_gateway = (string) get_user_option( 'wpsc_settings_selected_payment_gateway', get_current_user_id() );
+		if ( empty( $selected_gateway ) && ! empty( $this->active_gateways ) ) {
+			$selected_gateway = $this->active_gateways[0];
+		}
+
 		foreach ( $gateways as $gateway ) {
-			$this->gateway_list_item( $gateway );
+			$this->gateway_list_item( $gateway, $selected_gateway === $gateway['id'] );
 		}
 	}
 
