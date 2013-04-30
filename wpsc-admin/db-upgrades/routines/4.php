df7baaae809fc61584f475c0d4853434b9561adc
Justin Sainton <justinsainton@gmail.com>
2013-04-30 15:14:08 -0700

    Whoopsie.  Coding with 4 screaming toddlers.

--- wpsc-admin/db-upgrades/routines/4.php
+++ wpsc-admin/db-upgrades/routines/4.php
@@ -6,5 +6,5 @@ function _wpsc_db_upgrade_4() {
 
 function _wpsc_maybe_update_submitted_form_data_value() {
 	global $wpdb;
-	$wpdb->query( 'ALTER TABLE ' . WPSC_TABLE_SUBMITTED_FORM_DATA . ' MODIFY columnname INTEGER;' );
+	$wpdb->query( 'ALTER TABLE ' . WPSC_TABLE_SUBMITTED_FORM_DATA . ' MODIFY value LONGTEXT;' );
 }
