5158cb0f2125507966d6c4ab747c5eb830ec365b
JustinSainton <justinsainton@gmail.com>
2013-06-17 18:30:01 -0600

    Filter $json_response object
    Used quite nicely in conjunction with the new custom trigger.

--- wpsc-components/theme-engine-v1/helpers/ajax.php
+++ wpsc-components/theme-engine-v1/helpers/ajax.php
@@ -162,6 +162,8 @@ function wpsc_add_to_cart() {
 		if ( is_numeric( $product_id ) && 1 == get_option( 'fancy_notifications' ) )
 			$json_response['fancy_notification'] = str_replace( array( "\n", "\r" ), array( '\n', '\r' ), fancy_notification_content( $cart_messages ) );
 
+		$json_response = apply_filters( 'wpsc_add_to_cart_json_response', $json_response );
+
 		die( json_encode( $json_response ) );
 	}
 }
