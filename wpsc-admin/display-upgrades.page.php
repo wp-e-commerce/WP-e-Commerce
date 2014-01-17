adcdf9322a7f7bec5a5156612552a599dad54d71
misulicus <jmihaialexandru@gmail.com>
2014-01-18 00:11:29 +0200

    Updates to GC upgrade page
    removed code that is no longer used

--- wpsc-admin/display-upgrades.page.php
+++ wpsc-admin/display-upgrades.page.php
@@ -54,9 +54,10 @@ function wpsc_display_upgrades_page() {
 
 		<h2><?php esc_html_e( 'Upgrades', 'wpsc' ); ?></h2>
 		<div class='wpsc_gold_float'>
+			<?php if ( defined( 'WPSC_GOLD_MODULE_PRESENT' ) && ( true == WPSC_GOLD_MODULE_PRESENT ) ) {?>
 			<p><?php esc_html_e( 'Enter your API Username and API Key below.', 'wpsc' ); ?></p>
 			<p><a href="http://docs.getshopped.org/category/extending-your-store/premium-plugins/gold-cart/"><?php esc_html_e( 'For more information visit our documentation page.', 'wpsc' ); ?></a></p>
-
+			<?php } ?>
 			<div class='metabox-holder'>
 				<form method='post' id='gold_cart_form' action=''>
 
@@ -72,31 +73,7 @@ function wpsc_display_upgrades_page() {
 									<?php esc_html_e( "You don't have any Upgrades yet!", 'wpsc' ); ?>
 								</p>
 							</div>
-
-							<h2><?php _e( 'API Key Reset', 'wpsc' ); ?></h2>
-							<div class='form-wrap' >
-								<p>
-									<?php esc_html_e( 'Enter your API name and key to release it from an old site that you no longer use.', 'wpsc' ); ?> <br /><br />
-								</p>
-							</div>
 						</div>
-
-						<div class='postbox'>
-							<h3 class='hndle'><?php esc_html_e( 'API Key Reset', 'wpsc' ); ?></h3>
-							<p>
-								<label for='activation_name'><?php esc_html_e( 'Name:', 'wpsc' ); ?></label>
-								<input class='text' type='text' size='40' value='<?php echo get_option( 'activation_name' ); ?>' name='activation_name' id='activation_name' />
-							</p>
-							<p>
-								<label for='activation_key'><?php esc_html_e( 'API Key:', 'wpsc' ); ?></label>
-								<input class='text' type='text' size='40' value='<?php echo get_option( 'activation_key' ); ?>' name='activation_key' id='activation_key' />
-							</p>
-							<p>
-								<input type='hidden' value='true' name='reset_api_key' />
-								<input type='submit' class='button-primary' value='<?php esc_html_e( 'Reset API Key', 'wpsc' ); ?>' name='submit_values' />
-							</p>
-						</div>
-
 					<?php } ?>
 				</form>
 			<?php do_meta_boxes('wpsc_upgrade_page', 'top', true); ?>
@@ -106,52 +83,4 @@ function wpsc_display_upgrades_page() {
 
 <?php
 }
-
-function wpsc_reset_api_key() {
-	if ( isset( $_POST['reset_api_key'] ) && ( $_POST['reset_api_key'] == 'true' ) ) {
-		if ( $_POST['activation_name'] != null ) {
-			$target = "http://instinct.co.nz/wp-goldcart-api/api_register.php?name=" . $_POST['activation_name'] . "&key=" . $_POST['activation_key'] . "&url=" . site_url() . "";
-
-			$remote_access_fail = false;
-			$useragent = 'WP e-Commerce plugin';
-
-			$activation_name = urlencode( $_POST['activation_name'] );
-			$activation_key = urlencode( $_POST['activation_key'] );
-			$activation_state = update_option( 'activation_state', "false" );
-
-			$siteurl = urlencode( site_url() );
-			$request = '';
-
-			$http_request = "GET /wp-goldcart-api/api_register.php?name=$activation_name&key=&url=$siteurl HTTP/1.0\r\n";
-			$http_request .= "Host: instinct.co.nz\r\n";
-			$http_request .= "Content-Type: application/x-www-form-urlencoded; charset=" . get_option( 'blog_charset' ) . "\r\n";
-			$http_request .= "Content-Length: " . strlen( $request ) . "\r\n";
-			$http_request .= "User-Agent: $useragent\r\n";
-			$http_request .= "\r\n";
-			$http_request .= $request;
-
-			$response = '';
-
-			if ( false != ( $fs = @fsockopen( 'instinct.co.nz', 80, $errno, $errstr, 10 ) ) ) {
-				fwrite( $fs, $http_request );
-
-				while ( !feof( $fs ) )
-					$response .= fgets( $fs, 1160 ); // One TCP-IP packet
-
-					fclose( $fs );
-			}
-
-			$response = explode( "\r\n\r\n", $response, 2 );
-			$returned_value = (int)trim( $response[1] );
-
-			update_option( 'activation_name', '' );
-			update_option( 'activation_key', '' );
-
-			echo "<div class='updated'><p align='center'>" . esc_html__( 'Your API key has been Reset', 'wpsc' ) . "</p></div>";
-		}
-	}
-}
-
-add_action( 'wpsc_gold_module_activation', 'wpsc_reset_api_key' );
-
 ?>
