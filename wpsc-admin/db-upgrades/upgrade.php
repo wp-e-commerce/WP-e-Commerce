31f837800d07b2d3e6edea175d8a913f83427fe3
Jeffrey Schutzman <jeff@pyebrook.com>
2014-02-14 18:22:50 -0500

    revert files that should not have changed

--- wpsc-admin/db-upgrades/upgrade.php
+++ wpsc-admin/db-upgrades/upgrade.php
@@ -104,7 +104,7 @@ function _wpsc_needs_upgrade() {
 	if ( ! current_user_can( 'update_plugins' ) )
 		return false;
 
-	$current_db_ver = 9;//get_option( 'wpsc_db_version', 0 );
+	$current_db_ver = get_option( 'wpsc_db_version', 0 );
 
 	if ( WPSC_DB_VERSION <= $current_db_ver )
 		return false;
