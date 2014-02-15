05977aa6bedc19605f0527f7b91c5b7627f67b1a
Jeffrey Schutzman <jeff@pyebrook.com>
2014-02-15 17:49:04 -0500

    manually re-merge from upstream/master

--- wpsc-core/wpsc-constants.php
+++ wpsc-core/wpsc-constants.php
@@ -3,20 +3,42 @@
 $wpsc_currency_data = array();
 $wpsc_title_data    = array();
 
+
+/**
+ * _wpsc_is_session_started()
+ *
+ * Check if PHP session is started using method suggested on php.net
+ * @since 3.8.14
+ * @return boolean
+ */
+function _wpsc_is_session_started() {
+
+	if ( version_compare( phpversion(), '5.4.0', '>=' ) ) {
+		return session_status() === PHP_SESSION_ACTIVE;
+	} else {
+		if ( ! isset( $_SESSION ) ) {
+			$_SESSION = null;
+		}
+
+		return session_id() !== '';
+	}
+
+	return false;
+}
+
 /**
  * wpsc_core_load_session()
  *
  * Load up the WPEC session
+ * @return boolean
  */
 function wpsc_core_load_session() {
 
-	if ( ! isset( $_SESSION ) )
-		$_SESSION = null;
-
-	if ( ( !is_array( $_SESSION ) ) xor ( ! isset( $_SESSION['nzshpcrt_cart'] ) ) xor ( !$_SESSION ) )
+	if ( ! _wpsc_is_session_started() ) {
 		session_start();
+	}
 
-	return;
+	return _wpsc_is_session_started();
 }
 
 /**
@@ -190,11 +212,7 @@ function wpsc_core_constants_uploads() {
 
 	// Upload DIR
 	if ( isset( $wp_upload_dir_data['baseurl'] ) )
-		$upload_url = $wp_upload_dir_data['baseurl'];
-
-	// SSL Check for URL
-	if ( is_ssl() )
-		$upload_url = str_replace( 'http://', 'https://', $upload_url );
+		$upload_url = set_url_scheme( $wp_upload_dir_data['baseurl'] );
 
 	// Set DIR and URL strings
 	$wpsc_upload_sub_dir = '/wpsc/';
