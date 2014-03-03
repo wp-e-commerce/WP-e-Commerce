2127e21162b320f21664ddd231989640dad24efa
Jeffrey Schutzman <jeff@pyebrook.com>
2014-03-03 07:20:10 -0500

    Fix: check for set yay_or_boo before doing processing

--- wpsc-includes/stats.functions.php
+++ wpsc-includes/stats.functions.php
@@ -17,28 +17,33 @@ function _wpsc_action_update_product_stats( $log_id, $new_status, $old_status, $
 	$new_status_completed = $log->is_transaction_completed();
 	$old_status_completed = WPSC_Purchase_Log::is_order_status_completed( $old_status );
 
-	// if the order went through without any trouble, then it's a positive thing!
-	if ( $new_status_completed && ! $old_status_completed )
+	$yay_or_boo = '';
+
+	if ( $new_status_completed && ! $old_status_completed ) {
+		// if the order went through without any trouble, then it's a positive thing!
 		$yay_or_boo = 1;
-	// if the order is declined or invalid, sad face :(
-	elseif ( ! $new_status_completed && $old_status_completed )
+	} elseif ( ! $new_status_completed && $old_status_completed ) {
+		// if the order is declined or invalid, sad face :(
 		$yay_or_boo = -1;
+	}
 
-	// this dramatic mood swing affects the stats of each products in the cart
-	foreach ( $cart_contents as $cart_item ) {
-		$product = new WPSC_Product( $cart_item->prodid );
+	if ( ! empty( $yay_or_boo ) ) {
+		// this dramatic mood swing affects the stats of each products in the cart
+		foreach ( $cart_contents as $cart_item ) {
+			$product = new WPSC_Product( $cart_item->prodid );
 
-		$diff_sales    = $yay_or_boo * (int) $cart_item->quantity;
-		$diff_earnings = $yay_or_boo * (int) $cart_item->price * (int) $cart_item->quantity;
+			$diff_sales    = $yay_or_boo * (int) $cart_item->quantity;
+			$diff_earnings = $yay_or_boo * (int) $cart_item->price * (int) $cart_item->quantity;
 
-		$product->sales    += $diff_sales;
-		$product->earnings += $diff_earnings;
+			$product->sales    += $diff_sales;
+			$product->earnings += $diff_earnings;
 
-		// if this product has parent, make the same changes to the parent
-		if ( $product->post->post_parent ) {
-			$parent            = WPSC_Product::get_instance( $product->post->post_parent );
-			$parent->sales    += $diff_sales;
-			$parent->earnings += $diff_earnings;
+			// if this product has parent, make the same changes to the parent
+			if ( $product->post->post_parent ) {
+				$parent            = WPSC_Product::get_instance( $product->post->post_parent );
+				$parent->sales    += $diff_sales;
+				$parent->earnings += $diff_earnings;
+			}
 		}
 	}
 }
