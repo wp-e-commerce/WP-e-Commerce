48a9ee268540abf31dc8c1a2b8462b6ed2ac9e98
Justin Sainton <justinsainton@gmail.com>
2014-03-15 16:59:39 -0700

    Add WP.org forums link, as the getshopped.org forums have been removed.

--- wp-shopping-cart.php
+++ wp-shopping-cart.php
@@ -2,7 +2,7 @@
 /**
   * Plugin Name: WP e-Commerce
   * Plugin URI: http://getshopped.org/
-  * Description: A plugin that provides a WordPress Shopping Cart. See also: <a href="http://getshopped.org" target="_blank">GetShopped.org</a> | <a href="http://getshopped.org/forums/" target="_blank">Support Forum</a> | <a href="http://docs.getshopped.org/" target="_blank">Documentation</a>
+  * Description: A plugin that provides a WordPress Shopping Cart. See also: <a href="http://getshopped.org" target="_blank">GetShopped.org</a> | <a href="https://wordpress.org/support/plugin/wp-e-commerce/" target="_blank">Support Forum</a> | <a href="http://docs.getshopped.org/" target="_blank">Documentation</a>
   * Version: 3.8.14-dev
   * Author: Instinct Entertainment
   * Author URI: http://getshopped.org/
