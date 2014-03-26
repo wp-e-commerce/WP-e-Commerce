03e1e3995ad23b3bfa71a4e5330a77ff7760ff2c
Jeffrey Schutzman <jeff@pyebrook.com>
2014-03-26 08:54:30 -0400

    Updated headers, documentation and confirmed return values

--- wpsc-includes/cart-template-api.php
+++ wpsc-includes/cart-template-api.php
@@ -3,33 +3,23 @@
  * The WPSC Cart API for templates
  */
 
-/**
- * Contains an array of created instances of type WPSC_Checkout_Form
- *
- * @access private
- * @static
- * @since 3.9
- *
- * @var array
- */
-function _wpsc_verify_global_cart_has_been_initialized() {
-
-}
 
 /**
  * Does shipping information need to be recalculated for the current customer cart
  *
  * @since 3.8.14
  *
+ * @uses wpsc_cart
+ *
  */
 function wpsc_cart_need_to_recompute_shipping_quotes() {
 	global $wpsc_cart;
 
-	if ( empty( $wpsc_cart ) ) {
-		$wpsc_cart = wpsc_get_customer_cart();
-	}
+	$result = false;
 
-	return $wpsc_cart->needs_shipping_recalc();
+	if ( _wpsc_verify_global_cart_has_been_initialized( __FUNCTION__ ) ) {
+		$result = $wpsc_cart->needs_shipping_recalc();
+	}
 }
 
 /**
@@ -37,20 +27,23 @@ function wpsc_cart_need_to_recompute_shipping_quotes() {
  *
  * @since 3.8.14
  *
+ * @uses wpsc_cart
+ *
  */
 function wpsc_cart_clear_shipping_info() {
 	global $wpsc_cart;
 
-	if ( empty( $wpsc_cart ) ) {
-		$wpsc_cart = wpsc_get_customer_cart();
+	if ( _wpsc_verify_global_cart_has_been_initialized( __FUNCTION__ ) ) {
+		$wpsc_cart->clear_shipping_info();
 	}
-
-	return $wpsc_cart->clear_shipping_info();
 }
 
 /**
- * tax is included function, no parameters
- * * @return boolean true or false depending on settings>general page
+ * tax is included function
+ *
+ * @uses wpec_taxes_controller
+ *
+ * @return boolean true or false depending on settings>general page
  */
 function wpsc_tax_isincluded() {
 	//uses new wpec_taxes functionality now
@@ -59,91 +52,136 @@ function wpsc_tax_isincluded() {
 }
 
 /**
- * cart item count function, no parameters
- * * @return integer the item count
+ * cart item count function
+ *
+ * @uses wpsc_cart
+ *
+ * @return integer the item count
  */
 function wpsc_cart_item_count() {
 	global $wpsc_cart;
+
 	$count = 0;
-	foreach ( (array)$wpsc_cart->cart_items as $cart_item ) {
-		$count += $cart_item->quantity;
+
+	if ( _wpsc_verify_global_cart_has_been_initialized( __FUNCTION__ ) ) {
+		foreach ( (array)$wpsc_cart->cart_items as $cart_item ) {
+			$count += $cart_item->quantity;
+		}
 	}
+
 	return $count;
 }
 
 /**
- * coupon amount function, no parameters
- * * @return integer the item count
+ * Get WPEC cart coupon value total
+ *
+ * @param boolean 	$format_for_display		should the output formatted for display, or returned as a number
+ *
+ * @uses wpsc_cart
+ *
+ * @return integer the item count
  */
 function wpsc_coupon_amount( $format_for_display = true ) {
 	global $wpsc_cart;
 
-	if ( $format_for_display ) {
-		$output = wpsc_currency_display( $wpsc_cart->coupons_amount );
-	} else {
-		$output = $wpsc_cart->coupons_amount;
+	$output = '';
+
+	if ( _wpsc_verify_global_cart_has_been_initialized( __FUNCTION__ ) ) {
+		if ( $format_for_display ) {
+			$output = wpsc_currency_display( $wpsc_cart->coupons_amount );
+		} else {
+			$output = $wpsc_cart->coupons_amount;
+		}
 	}
+
 	return $output;
 }
 
 /**
- * cart total function, no parameters
+ * cart total function
+ *
+ * @param boolean 	$format_for_display		should the output formatted for display, or returned as a number
+ *
+ * @uses wpsc_cart
+ *
  * @return string the total price of the cart, with a currency sign
  */
 function wpsc_cart_total( $format_for_display = true ) {
 	global $wpsc_cart;
+	$total = 0;
 
-	$total = $wpsc_cart->calculate_total_price();
+	if ( _wpsc_verify_global_cart_has_been_initialized( __FUNCTION__ ) ) {
 
-	if ( $format_for_display ) {
-		return wpsc_currency_display( $total );
+		$total = $wpsc_cart->calculate_total_price();
+
+		if ( $format_for_display ) {
+			$total = wpsc_currency_display( $total );
+		} else {
+			$total = $total;
+		}
+	}
+
+	return $total;
+}
+
+/**
+ * nzshpcrt_overall_total_price function
+ *
+ * @uses wpsc_cart
+ *
+ * @return string the total price of the cart, with a currency sign, empty string on failure
+ */
+function nzshpcrt_overall_total_price() {
+
+	global $wpsc_cart;
+
+	if ( _wpsc_verify_global_cart_has_been_initialized( __FUNCTION__ ) ) {
+		return  $wpsc_cart->calculate_total_price();
 	} else {
-		return $total;
+		return '';
 	}
 
 }
 
 /**
- * nzshpcrt_overall_total_price function, no parameters
- * @return string the total price of the cart, with a currency sign
- */
-function nzshpcrt_overall_total_price() {
-
-	global $wpsc_cart;
-	return  $wpsc_cart->calculate_total_price();
-
-}
-
-/**
- * cart total weight function, no parameters
+ * cart total weight function
+ *
+ * @uses wpsc_cart
+ *
  * @return float the total weight of the cart
  */
 function wpsc_cart_weight_total() {
 	global $wpsc_cart;
 
-	if ( is_object( $wpsc_cart ) ) {
+	if ( _wpsc_verify_global_cart_has_been_initialized( __FUNCTION__ ) ) {
 		return $wpsc_cart->calculate_total_weight( true );
 	} else {
-		return 0;
+		return 0.0;
 	}
 }
 
 /**
  * tax total function, no parameters
  *
+ * @uses wpsc_cart
+ *
  * @return float the total weight of the cart
  */
 function wpsc_cart_tax( $format_for_display = true ) {
 	global $wpsc_cart;
 
-	if ( $format_for_display ) {
-		if ( ! wpsc_tax_isincluded() ) {
-			return wpsc_currency_display( $wpsc_cart->calculate_total_tax() );
+	$cart_tax = $format_for_display ? '' : 0;
+
+	if ( _wpsc_verify_global_cart_has_been_initialized( __FUNCTION__ ) ) {
+		if ( $format_for_display ) {
+			if ( ! wpsc_tax_isincluded() ) {
+				$cart_tax = wpsc_currency_display( $wpsc_cart->calculate_total_tax() );
+			} else {
+				$cart_tax = '(' . wpsc_currency_display( $wpsc_cart->calculate_total_tax() ) . ')';
+			}
 		} else {
-			return '(' . wpsc_currency_display( $wpsc_cart->calculate_total_tax() ) . ')';
+			$cart_tax = $wpsc_cart->calculate_total_tax();
 		}
-	} else {
-		return $wpsc_cart->calculate_total_tax();
 	}
 }
 
@@ -151,15 +189,19 @@ function wpsc_cart_tax( $format_for_display = true ) {
 /**
  * wpsc_cart_show_plus_postage function, no parameters
  * For determining whether to show "+ Postage & tax" after the total price
- * @return boolean true or false, for use with an if statement
+ *
+ * @uses wpsc_cart
+ *
+ * @return boolean true or false
  */
 function wpsc_cart_show_plus_postage() {
-	global $wpsc_cart;
 
+	// TODO: Deprecate the use of $_SESSION for cart state, see github issue report #997
+	// https://github.com/wp-e-commerce/WP-e-Commerce/issues/997
 	if (
 		isset( $_SESSION['wpsc_has_been_to_checkout'] )
 			&& ($_SESSION['wpsc_has_been_to_checkout'] == null )
-				&& (get_option( 'add_plustax' ) == 1
+				&& ( get_option( 'add_plustax' ) == 1
 		) ) {
 
 		return true;
@@ -170,7 +212,10 @@ function wpsc_cart_show_plus_postage() {
 }
 
 /**
- * uses shipping function, no parameters
+ * Does the customers cart require/user shipping
+ *
+ * @uses wpsc_cart
+
  * @return boolean if true, all items in the cart do use shipping
  */
 function wpsc_uses_shipping() {
@@ -184,59 +229,89 @@ function wpsc_uses_shipping() {
 		return false;
 	}
 
-	if ( ( ! ( ( get_option( 'shipping_discount' ) == 1 ) && ( get_option( 'shipping_discount_value' ) <= $wpsc_cart->calculate_subtotal() ) ) )
-				|| ( count( $shippingoptions ) >= 1 && $shippingoptions[0] != '')
-	) {
-		$status = (bool) $wpsc_cart->uses_shipping();
-	} else {
-		$status = false;
+	$uses_shipping = false;
+
+	if ( _wpsc_verify_global_cart_has_been_initialized( __FUNCTION__ ) ) {
+
+		if ( ( ! ( ( get_option( 'shipping_discount' ) == 1 ) && ( get_option( 'shipping_discount_value' ) <= $wpsc_cart->calculate_subtotal() ) ) )
+					|| ( count( $shippingoptions ) >= 1 && $shippingoptions[0] != '')
+		) {
+			$uses_shipping = (bool) $wpsc_cart->uses_shipping();
+		} else {
+			$uses_shipping = false;
+		}
 	}
 
-	return $status;
+	return $uses_shipping;
 }
 
 /**
- * cart has shipping function, no parameters
+ * Check if the shipping charges are non-zero for the customer cart
+ *
+ * @uses wpsc_cart
+ *
  * @return boolean true for yes, false for no
  */
 function wpsc_cart_has_shipping() {
 	global $wpsc_cart;
 
-	if ( $wpsc_cart->calculate_total_shipping() > 0 ) {
-		$output = true;
-	} else {
-		$output = false;
+	$has_shipping = false;
+
+	if ( _wpsc_verify_global_cart_has_been_initialized( __FUNCTION__ ) ) {
+		if ( $wpsc_cart->calculate_total_shipping() > 0 ) {
+			$has_shipping = true;
+		} else {
+			$has_shipping = false;
+		}
 	}
 
-	return $output;
+	return $has_shipping;
 }
 
 /**
- * cart shipping function, no parameters
+ * Get cart total
+ *
+ * @uses wpsc_cart
+ *
  * @return string the total shipping of the cart, with a currency sign
  */
 function wpsc_cart_shipping() {
 	global $wpsc_cart;
-	return apply_filters( 'wpsc_cart_shipping', wpsc_currency_display( $wpsc_cart->calculate_total_shipping() ) );
+
+	if ( _wpsc_verify_global_cart_has_been_initialized( __FUNCTION__ ) ) {
+		$result = wpsc_currency_display( $wpsc_cart->calculate_total_shipping() );
+	} else {
+		$result = '';
+	}
+
+	$result = apply_filters( 'wpsc_cart_shipping', $result );
+
+	return $result;
 }
 
 
 /**
- * cart item categories function, no parameters
+ * Get cart item categories function
+ *
+ * @since
+ * @uses wpsc_cart
+ *
  * @return array array of the categories
  */
 function wpsc_cart_item_categories( $get_ids = false ) {
 	global $wpsc_cart;
 
-	if ( is_object( $wpsc_cart ) ) {
+	$categories = array();
+
+	if ( _wpsc_verify_global_cart_has_been_initialized( __FUNCTION__ ) ) {
 		if ( $get_ids ) {
-			return $wpsc_cart->get_item_category_ids();
+			$categories = $wpsc_cart->get_item_category_ids();
 		} else {
-			return $wpsc_cart->get_item_categories();
+			$categories = $wpsc_cart->get_item_categories();
 		}
-	} else {
-		return array();
 	}
+
+	return $categories;
 }
 
 /**
@@ -284,6 +359,7 @@ function wpsc_product_min_cart_quantity( $product_id = 0 ) {
  *
  * @uses   wpsc_product_max_cart_quantity    Gets the maximum product cart quantity.
  * @uses   wpsc_product_min_cart_quantity    Gets the minimum product cart quantity.
+ * @uses wpsc_cart
  */
 function wpsc_validate_product_cart_quantity( $quantity, $product_id = 0 ) {
 
@@ -312,6 +388,7 @@ function wpsc_validate_product_cart_quantity( $quantity, $product_id = 0 ) {
  * @param array   $parameters                    Cart item parameters.
  * @param object  $cart                          Cart object.
  *
+ * @uses wpsc_cart
  * @uses  wpsc_validate_product_cart_quantity    Filters and restricts the product cart quantity.
  */
 function _wpsc_validate_cart_product_quantity( $product_id, $parameters, $cart ) {
@@ -328,7 +405,11 @@ add_action( 'wpsc_edit_item', '_wpsc_validate_cart_product_quantity', 10, 3 );
 /**
  * cart all shipping quotes, used for google checkout
  * returns all the quotes for a selected shipping method
+ *
+ * @since
  * @access public
+ *
+ * @uses wpsc_cart
  *
  * @return array of shipping options
 */
@@ -344,19 +425,56 @@ function wpsc_strleft( $s1, $s2 ) {
 	return  $values;
 }
 
+/**
+ * WPEC cart API template function
+ *
+ * @since
+ *
+ * @param
+ *
+ * @uses wpsc_cart
+ */
 function wpsc_update_shipping_single_method(){
 	global $wpsc_cart;
-	if ( ! empty( $wpsc_cart->shipping_method ) ) {
-		$wpsc_cart->update_shipping( $wpsc_cart->shipping_method, $wpsc_cart->selected_shipping_option );
-	}
-}
-function wpsc_update_shipping_multiple_methods(){
-	global $wpsc_cart;
-	if ( ! empty( $wpsc_cart->selected_shipping_method ) ) {
-		$wpsc_cart->update_shipping( $wpsc_cart->selected_shipping_method, $wpsc_cart->selected_shipping_option );
+
+	if ( _wpsc_verify_global_cart_has_been_initialized( __FUNCTION__ ) ) {
+		if ( ! empty( $wpsc_cart->shipping_method ) ) {
+			$wpsc_cart->update_shipping( $wpsc_cart->shipping_method, $wpsc_cart->selected_shipping_option );
+		}
 	}
 }
 
+/**
+ * WPEC cart API template function
+ *
+ * @since
+ *
+ * @param
+ *
+ * @uses wpsc_cart
+ */
+function wpsc_update_shipping_multiple_methods(){
+	global $wpsc_cart;
+
+	if ( _wpsc_verify_global_cart_has_been_initialized( __FUNCTION__ ) ) {
+		if ( ! empty( $wpsc_cart->selected_shipping_method ) ) {
+			$wpsc_cart->update_shipping( $wpsc_cart->selected_shipping_method, $wpsc_cart->selected_shipping_option );
+		}
+	}
+}
+
+/**
+ * Get remaining product quantity
+ *
+ * @since
+ * @access public
+ *
+ * @param int     $product_id                    Cart product ID.
+ * @param array   $variations                    Cart item parameters.
+ * @param int     $quantity                      Cart object.
+ *
+ * @uses  wpsc_product_stock    Filters and restricts the product cart quantity.
+ */
 function wpsc_get_remaining_quantity( $product_id, $variations = array(), $quantity = 1 ) {
 
 	$stock = get_post_meta( $product_id, '_wpsc_stock', true );
@@ -374,4 +492,38 @@ function wpsc_get_remaining_quantity( $product_id, $variations = array(), $quant
 	}
 
 	return $output;
+}
+
+
+/**
+ * Prior to using the global cart variable cart template API functions should check
+ * to be sure the global cart variable has been initialized.
+ *
+ * @access private
+ * @static
+ * @since 3.8.14
+ *
+ * @uses wpsc_cart
+ * @return boolean true if we have a valid cart, false otherwise
+ *
+ */
+function _wpsc_verify_global_cart_has_been_initialized( $function = __FUNCTION__ ) {
+
+	global $wpsc_cart;
+
+	$we_have_a_valid_cart = ! empty( $wpsc_cart ) && is_a( $wpsc_cart, 'wpsc_cart' );
+
+	if ( ! $we_have_a_valid_cart ) {
+		$wpsc_cart = wpsc_get_customer_cart();
+		$we_have_a_valid_cart = ! empty( $wpsc_cart ) && is_a( $wpsc_cart, 'wpsc_cart' );
+	}
+
+	// We will try to give a helpful message to the developer so that they can adjust their code
+	static $already_gave_no_valid_cart_message = false;
+	if ( ! $we_have_a_valid_cart && ! $already_gave_no_valid_cart_message ) {
+		_wpsc_doing_it_wrong( $function, __( 'The WPeC global cart is not yet initialized, accessing global cart properties and methods will not work.', 'wpsc' ), '3.8.14' );
+		$already_gave_no_valid_cart_message = true;
+	}
+
+	return $we_have_a_valid_cart;
 }
