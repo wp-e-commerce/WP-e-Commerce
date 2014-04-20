1943501c14fd66f43ecaced2106389c76187b2db
JustinSainton <justinsainton@gmail.com>
2014-04-20 22:17:35 +0000

    First pass at monkey-patching TinyMCE.  We're going for 'functional' in 3.8.14.  Would love to see 'awesome' in 3.9

--- wpsc-core/js/tinymce3/langs/en.js
+++ wpsc-core/js/tinymce3/langs/en.js
@@ -2,5 +2,5 @@
 
 tinyMCE.addI18n({en:{
 WPSC:{	
-desc : 'Add a Category to your Page or Post'
+desc : 'Add a product shortcode'
 }}});
--- wpsc-core/js/tinymce3/tinymce.js
+++ wpsc-core/js/tinymce3/tinymce.js
@@ -114,13 +114,19 @@ function insertWPSCLink() {
 		}
 	}
 
-	if(window.tinyMCE) {
-		window.tinyMCE.execInstanceCommand('content', 'mceInsertContent', false, tagtext);
-		//Peforms a clean up of the current editor HTML.
-		//tinyMCEPopup.editor.execCommand('mceCleanup');
-		//Repaints the editor. Sometimes the browser has graphic glitches.
-		tinyMCEPopup.editor.execCommand('mceRepaint');
-		tinyMCEPopup.close();
+	if ( window.tinyMCE ) {
+		if ( window.tinyMCE.majorVersion < 4 ) {
+
+			window.tinyMCE.execInstanceCommand('content', 'mceInsertContent', false, tagtext);
+			//Peforms a clean up of the current editor HTML.
+			//tinyMCEPopup.editor.execCommand('mceCleanup');
+			//Repaints the editor. Sometimes the browser has graphic glitches.
+			tinyMCEPopup.editor.execCommand('mceRepaint');
+			tinyMCEPopup.close();
+		} else {
+            parent.tinyMCE.execCommand( 'mceInsertContent', false, tagtext );
+            parent.tinyMCE.activeEditor.windowManager.close();
+		}
 	}
 	return;
 }
