8d88b2d4dc1a5f108f26ac4717dbd7e782cf6d17
Jeffrey Schutzman <jeff@pyebrook.com>
2014-05-02 06:12:36 -0400

    Add 'County' as region label for UK * add 'County' as the region label for UK * Incremented database version, added update routine for next version (13)

--- wpsc-core/wpsc-constants.php
+++ wpsc-core/wpsc-constants.php
@@ -74,7 +74,7 @@ function wpsc_core_constants() {
 	}
 
 	// Define the current database version
-	define( 'WPSC_DB_VERSION', 12 );
+	define( 'WPSC_DB_VERSION', 13 );
 
 	// Define Debug Variables for developers, if they haven't already been defined
 	if ( ! defined( 'WPSC_DEBUG' ) ) {
