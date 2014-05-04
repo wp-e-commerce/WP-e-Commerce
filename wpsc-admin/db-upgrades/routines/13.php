1a38dcc232c1cadfe9a63fc3ae66688e3dd43a1b
Jeffrey Schutzman <jeff@pyebrook.com>
2014-05-03 20:28:26 -0400

    fixed typo

--- wpsc-admin/db-upgrades/routines/13.php
+++ wpsc-admin/db-upgrades/routines/13.php
@@ -7,7 +7,7 @@
 *
 */
 function _wpsc_db_upgrade_13() {
-	_wpsc_add_region_labe_to_uk();
+	_wpsc_add_region_label_to_uk();
 	_wpsc_fix_bad_checkout_form_rows();
 }
 
@@ -17,7 +17,7 @@ function _wpsc_db_upgrade_13() {
  * @access private
  * @since 3.8.14.1
  */
-function _wpsc_add_region_labe_to_uk() {
+function _wpsc_add_region_label_to_uk() {
 	$wpsc_country = new WPSC_Country( 'GB' );
 	$wpsc_country->set( 'region_label', __( 'County', 'wpsc' ) );
 }
