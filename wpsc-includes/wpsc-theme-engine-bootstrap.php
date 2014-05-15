73732244d6fadd47129d65bc61e12be4e6ed114d
JustinSainton <justinsainton@gmail.com>
2014-05-15 23:12:30 +0000

    Remove placeholder image.  At this point (TinyMCE 4.0), this is actually causing more damage than it protects against.

--- wpsc-includes/wpsc-theme-engine-bootstrap.php
+++ wpsc-includes/wpsc-theme-engine-bootstrap.php
@@ -1,5 +1,19 @@
 <?php
 
+function _wpsc_theme_engine_v2_has_old_templates() {
+	$current_theme  = trailingslashit( get_stylesheet_directory() );
+	$theme_files    = scandir( $current_theme );
+	$wpsc_files     = array();
+
+	foreach ( $theme_files as $file ) {
+		if ( 'wpsc-' === substr( $file, 0, 5 ) && is_file( $current_theme . $file ) ) {
+			$wpsc_files[] = $file;
+		}
+	}
+
+	return ! empty( $wpsc_files );
+}
+
 function _wpsc_enable_theme_engine_v1( $components ) {
 	$components['theme-engine']['core-v1'] = array(
 		'title'    => __( 'WP e-Commerce Theme Engine v1', 'wpsc' ),
@@ -23,6 +37,28 @@ function _wpsc_enable_theme_engine_v2( $components ) {
 function _wpsc_maybe_activate_theme_engine_v2() {
 
 	$activate = false;
+
+	global $wp_rewrite;
+
+	if ( ! is_a( $wp_rewrite, 'WP_Rewrite' ) ) {
+		$wp_rewrite = new WP_Rewrite();
+	}
+
+	if ( ! $wp_rewrite->using_permalinks() ) {
+		$activate = false;
+	}
+
+	if ( $wp_rewrite->using_index_permalinks() ) {
+		$activate = false;
+	}
+
+	if ( function_exists( '_wpsc_gc_init' ) ) {
+		$activate = false;
+	}
+
+	if ( _wpsc_theme_engine_v2_has_old_templates() ) {
+		$activate = false;
+	}
 
 	return apply_filters( '_wpsc_maybe_activate_theme_engine_v2', $activate );
 }
