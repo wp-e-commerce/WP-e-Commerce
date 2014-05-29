a3ad10ee0765e4b438a55b6cbf83092ae410e5b8
Jeffrey Schutzman <jeff@pyebrook.com>
2014-05-29 09:20:03 -0400

    Fix PHP Product Class and Stats PHP errors Errors were result of instantiating a product using a product id corresponding to a product that was deleted.  As this is a normal occurrence as products come and go from a store the instantiation isn't the problem, but doing some operations on it is a problem.
    Before doing these operations a check is made to see if they are permitted.

--- wpsc-includes/product.class.php
+++ wpsc-includes/product.class.php
@@ -349,6 +349,16 @@ class WPSC_Product {
 	}
 
 	/**
+	 * Does product exists
+	 *
+	 * @since 3.8.14
+	 * @param int|WP_Post $post ID or Post object
+	 */
+	public function exists() {
+		return ! empty( $this->post  );
+	}
+
+	/**
 	 * Initialize stock properties
 	 *
 	 * @since 3.8.14
@@ -621,9 +631,11 @@ class WPSC_Product {
 	 * @since 3.8.14
 	 */
 	private function process_stats() {
-		if ( $this->post->_wpsc_stats === '' ) {
-			$this->stats = WPSC_Purchase_Log::get_stats_for_product( $this->post->ID );
-			update_post_meta( $this->post->ID, '_wpsc_stats', $this->stats );
+		if ( $this->post ) {
+			if ( ! property_exists( $this, '_wpsc_stats' ) || empty( $this->post->_wpsc_stats ) ) {
+				$this->stats = WPSC_Purchase_Log::get_stats_for_product( $this->post->ID );
+				update_post_meta( $this->post->ID, '_wpsc_stats', $this->stats );
+			}
 		}
 	}
 
