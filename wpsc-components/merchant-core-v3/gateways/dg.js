b502dc4347df33ce3dc9ad520be624484406d78b
Abid Omar <contact@omarabid.com>
2014-09-04 13:30:36 +0100

    DG Lightbox pop-up

--- wpsc-components/merchant-core-v3/gateways/dg.js
+++ wpsc-components/merchant-core-v3/gateways/dg.js
@@ -1,11 +1,19 @@
 (function($) {
-	$(window).load(function() {
-		$( '.wpsc-checkout-form-button' ).attr( 'id', 'submitBtn' );
-		var dg = new PAYPAL.apps.DGFlow({
-			// the HTML ID of the form submit button which calls setEC
-			trigger: 'submitBtn',
-			// the experience type: instant or mini
-			expType: 'instant'
+
+	$(window).load(function() {	
+		var $checkout_btn = $( '.wpsc-checkout-form-button' ),
+		$rd_btn = $( 'INPUT[value="paypal-digital-goods"]' );
+
+		$checkout_btn.attr( 'id', 'submitBtn' );
+
+		$checkout_btn.on( 'click', function() {
+			if ( $rd_btn.is(':checked') ) {
+				var dg = new PAYPAL.apps.DGFlow({
+					trigger: 'submitBtn',	
+					expType: ''
+				});
+			}
 		});
 	});
+
 })(jQuery);
