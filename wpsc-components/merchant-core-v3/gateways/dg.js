235d48f59d078440369b9785406a612c76946829
Abid Omar <contact@omarabid.com>
2014-12-20 20:07:04 +0100

    New version of DG javascript file

--- wpsc-components/merchant-core-v3/gateways/dg.js
+++ wpsc-components/merchant-core-v3/gateways/dg.js
@@ -1,28 +1,45 @@
 /*
 * PayPal for Digital Goods script
 */
+;
 (function($) {
 	$(window).load(function() {	
 		// Declare variables/doom elements
 		var $checkout_btn = $( '.wpsc-checkout-form-button' ),
 		$rd_btn = $( 'input[value="paypal-digital-goods"]' ),
-		$inputs = $( 'input[type="radio"]' ),
 		$form = $( '#wpsc-checkout-form' );
 
 		// Change the id of the checkout button
 		$checkout_btn.attr( 'id', 'submitBtn' );
 
-		// Gateway selection changed
-		$inputs.on( 'change', function() {
+		// Disable Form submission if DG is selected
+		$form.on ('submit', function() {
 			if ( $rd_btn.is( ':checked' ) ) {
-				// Starts the DG Flow
-				var dg = new PAYPAL.apps.DGFlow( {
-					trigger: 'submitBtn'
-				} );
-			} else {
-				$form.attr( 'target', '' );
+				return false;			
 			}
-		} );	
-	} );
+		} );
 
+		// Submit button Click handler
+		$checkout_btn.on( 'click', function() {
+			// If DG is selected
+			if ( $rd_btn.is( ':checked' ) ) {
+				// Disable Submit button
+				$checkout_btn.val( 'loading...' ).attr( 'disabled', 'on' );
+				// Submit the FORM with AJAX
+				$.ajax({
+					url: '',
+					type: 'post',
+					data: $form.serialize(),
+					success: function( url ) {
+						// Start the DG flow
+						var dg = new PAYPAL.apps.DGFlow({
+							trigger: null
+						});
+						dg.startFlow( url );
+					}
+				});
+				return false;
+			}
+		} );
+	} );
 } )( jQuery );
