9ea07536058b0b629ccff41482fa5e38817c8a55
Justin Sainton <justinsainton@gmail.com>
2015-04-21 20:01:28 +0000

    Continued stubbing of WPSC_Redirect_Canonical.  I am going to hate my life after this.

--- wpsc-components/theme-engine-v2/classes/redirect-canonical.php
+++ wpsc-components/theme-engine-v2/classes/redirect-canonical.php
@@ -1,5 +1,29 @@
 <?php
 
-class _WPSC_Redirect_Canonical {
+/**
+ * Handles canonical redirects for all single product pages, shortcode pages, archive pages, taxonomy pages.
+ */
+class WPSC_Redirect_Canonical {
+	private static $instance;
 
+	public static function get_instance(){
+		if ( is_null( $instance ) ) {
+			self::$instance = new WPSC_Redirect_Canonical();
+			self::$instance->init();
+		}
+	}
+
+	private function init() {
+
+	}
+
+	public static register_redirect() {
+
+	}
+
+	public static deregister_redirect() {
+
+	}
 }
+
+add_action( 'wpsc_init', 'WPSC_Redirect_Canonical::get_instance' );
