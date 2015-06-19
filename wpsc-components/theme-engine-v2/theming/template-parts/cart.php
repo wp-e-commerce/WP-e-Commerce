2231a60a1eeaa9912a88af5123f2c8a25b790f3f
Justin Sainton <justinsainton@gmail.com>
2015-06-18 17:49:17 -0700

    Don't say 'Oops' about a cart being empty. It adds personality which may not always be appropriate for every site, and it could make someone feel they did something they weren't supposed to.

--- wpsc-components/theme-engine-v2/theming/template-parts/cart.php
+++ wpsc-components/theme-engine-v2/theming/template-parts/cart.php
@@ -5,7 +5,7 @@
 	<?php if ( wpsc_cart_has_items() ): ?>
 		<?php wpsc_cart_item_table(); ?>
 	<?php else: ?>
-		<p><?php esc_html_e( "Oops, there's nothing in your cart.", 'wpsc' ); ?></p>
+		<p><?php esc_html_e( "There's nothing in your cart.", 'wpsc' ); ?></p>
 		<p><?php wpsc_keep_shopping_button(); ?></p>
 	<?php endif ?>
 </div>
