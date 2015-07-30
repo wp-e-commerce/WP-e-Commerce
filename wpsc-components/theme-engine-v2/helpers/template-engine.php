013789059049016748cd2473543016357d46f33b
Justin Sainton <justinsainton@gmail.com>
2015-07-30 08:20:10 -0700

    Don't set posts_per_archive_page setting to -1 haphazardly in wpsc_action_set_product_per_page_query_var(). Fixes #1954.

--- wpsc-components/theme-engine-v2/helpers/template-engine.php
+++ wpsc-components/theme-engine-v2/helpers/template-engine.php
@@ -196,8 +196,6 @@ function wpsc_action_set_product_per_page_query_var( $query ) {
 
 	if ( wpsc_is_pagination_enabled() && $query->is_main_query() && ( wpsc_is_store() || wpsc_is_product_category() || wpsc_is_product_tag() ) ) {
 		$query->set( 'posts_per_archive_page', wpsc_get_option( 'products_per_page' ) );
-	} else {
-		$query->set( 'posts_per_archive_page', -1 );
 	}
 }
 
