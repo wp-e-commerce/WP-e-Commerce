64a62baeee3a23365e3808d5a2737c92bbebb502
Joldis Mihai Alexandru <jmihaialexandru@gmail.com>
2015-10-05 23:17:24 +0300

    Hopefully final change to licenses page
    Added localisation, escaping and stuff. Hope this is final edit

--- wpsc-admin/display-upgrades.page.php
+++ wpsc-admin/display-upgrades.page.php
@@ -8,19 +8,19 @@ function wpsc_display_upgrades_page() {
 			<div class='metabox-holder'>
 				<form method='post' id='product_license_box' action=''>
 					<div class="postbox">
-						<h3 class="hndle"><?php _e( 'Product License Registration', 'wpsc' );?></h3>
+						<h3 class="hndle"><?php _e( 'Product License Registration', 'wp-e-commerce' );?></h3>
 						<p>
-							<label for="activation_key"><?php _e( 'License Key ', 'wpsc' ); ?>:</label>
+							<label for="activation_key"><?php _e( 'License Key ', 'wp-e-commerce' ); ?>:</label>
 							<input type="text" id="activation_key" name="product_license" size="48" value="" class="text" />
 						</p>
 						<p>
 							<input type="hidden" value="true" name="product_license_key" />
-							<button type="submit" name="submit_values" value="submit_values"><?php _e( 'Register License', 'wpsc' ); ?></button>
-							<button type="submit" name="reset_values" value="reset_values"><?php _e( 'Reset License', 'wpsc' ); ?></button>
+							<button type="submit" name="submit_values" value="submit_values" class="button button-primary"><?php _e( 'Register License', 'wp-e-commerce' ); ?></button>
+							<button type="submit" name="reset_values" value="reset_values"><?php _e( 'Reset License', 'wp-e-commerce' ); ?></button>
 						</p>
-						<p style='font-size:8pt; line-height:10pt; font-weight:bold;'>
-							<?php _e( 'In order to receive automatic plugin updates you need to Register your License for each product that provides one. ', 'wpsc' ); ?></br>
-							<?php _e( 'Old API Keys will not work! ', 'wpsc' ); ?>
+						<p>
+							<?php _e( 'In order to receive automatic plugin updates you need to Register your License for each product that provides one. ', 'wp-e-commerce' ); ?></br>
+							<?php _e( 'Old API Keys will not work! ', 'wp-e-commerce' ); ?>
 						</p>
 					</div>
 				</form>
@@ -33,37 +33,39 @@ wpse_license_page_display_licenses();
 
 function wpse_license_page_display_licenses () {
 	$licenses = get_option( 'wpec_license_active_products', array() );
-
-	echo '<table class="wp-list-table widefat striped">
+	?>
+	<table class="wp-list-table widefat striped">
 			<thead>
 				<tr>
-					<th scope="col" id="product_name" class="manage-column column-product_name column-primary">Product Name</th>
-					<th scope="col" id="product_license" class="manage-column column-product_license">License Key</th>
-					<th scope="col" id="product_expiry" class="manage-column column-product_expiry">License Expiration Date</th>
+					<th scope="col" id="product_name" class="manage-column column-product_name column-primary"><?php _e('Product Name', 'wp-e-commerce'); ?></th>
+					<th scope="col" id="product_license" class="manage-column column-product_license"><?php _e('License Key', 'wp-e-commerce'); ?></th>
+					<th scope="col" id="product_expiry" class="manage-column column-product_expiry"><?php _e('License Expiration', 'wp-e-commerce'); ?></th>
 				</tr>
 			</thead>
-			<tbody id="the-list">';
-	
+			<tbody id="the-list">
+	<?php
 	if ( ! empty( $licenses ) ) {
-		foreach ( (array) $licenses as $license ) {
-			
-			echo '<tr><td class="product_name column-product_name"><p><strong>'.$license['name'].'</strong></p></td>';
-			echo '<td class="product_license column-product_license"><p><strong>'.$license['license'].'</strong></p></td>';
-			echo '<td class="product_expiry column-product_expiry"><p><strong>'.$license['expires'].'</strong></p></td></tr>';
-		}
+		foreach ( (array) $licenses as $license ) { ?>
+			<tr><td class="product_name column-product_name"><p><strong><?php echo esc_html( $license['name'] ); ?></strong></p></td>
+			<td class="product_license column-product_license"><p><strong><?php echo esc_html( $license['license'] ); ?>'</strong></p></td>
+			<td class="product_expiry column-product_expiry"><p><strong><?php echo esc_html( $license['expires'] ); ?>'</strong></p></td></tr>
+		<?php }
 	} else {
-		echo '<tr class="no-items"><td class="colspanchange" colspan="4"><p>No Licenses found.</p></td></tr>';
+		?>
+		<tr class="no-items"><td class="colspanchange" colspan="4"><p><?php _e('No Licenses found.', 'wp-e-commerce'); ?></p></td></tr>
+	<?php
 	}
-	
-	echo '</tbody>
+	?>
+	</tbody>
 		<tfoot>
 			<tr>
-				<th scope="col" class="manage-column column-product_name column-primary">Product Name</th>
-				<th scope="col" class="manage-column column-product_license">License Key</th>
-				<th scope="col" class="manage-column column-product_expiry">License Expiry Date</th>
+				<th scope="col" class="manage-column column-product_name column-primary"><?php _e('Product Name', 'wp-e-commerce'); ?></th>
+				<th scope="col" class="manage-column column-product_license"><?php _e('License Key', 'wp-e-commerce'); ?></th>
+				<th scope="col" class="manage-column column-product_expiry"><?php _e('License Expiration', 'wp-e-commerce'); ?></th>
 			</tr>
 		</tfoot>
-		</table>';
+		</table>
+		<?php
 }
 
 /**
@@ -112,7 +114,7 @@ function wpsc_licenses_action_stuff() {
 				);
 				
 				update_option( 'wpec_license_active_products', $licenses );
-				echo '<div class="updated"><p>'.$response->message.'</p></div>';
+				echo '<div class="updated"><p>'.esc_html( $response->message ).'</p></div>';
 
 			} elseif ( 'deactivated' === $response->status ) {
 				
@@ -124,10 +126,10 @@ function wpsc_licenses_action_stuff() {
 				}
 				
 				update_option( 'wpec_license_active_products', $licenses );
-				echo '<div class="updated"><p>'.$response->message.'</p></div>';
+				echo '<div class="updated"><p>'.esc_html( $response->message ).'</p></div>';
 				
 			} else {
-				echo '<div class="error"><p>'.$response->message.'</p></div>';
+				echo '<div class="error"><p>'.esc_html( $response->message ).'</p></div>';
 			}
 		}
 	}
