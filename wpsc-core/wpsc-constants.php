28fac9f7c7734607d84e42d9746f2fbdd0c4e0d6
Justin Sainton <justinsainton@gmail.com>
2016-05-23 14:30:00 -0700

    Version bump and changelog.

--- wpsc-core/wpsc-constants.php
+++ wpsc-core/wpsc-constants.php
@@ -55,15 +55,15 @@ function wpsc_core_constants() {
 
 	// Define Plugin version
 	if ( ! defined( 'WPSC_VERSION' ) ) {
-		define( 'WPSC_VERSION'            , '3.11.2' );
+		define( 'WPSC_VERSION'            , '3.11.3' );
 	}
 
 	if ( ! defined( 'WPSC_MINOR_VERSION' ) ) {
-		define( 'WPSC_MINOR_VERSION'      , 'ffbc44e' );
+		define( 'WPSC_MINOR_VERSION'      , '9cadbff' );
 	}
 
 	if ( ! defined( 'WPSC_PRESENTABLE_VERSION' ) ) {
-		define( 'WPSC_PRESENTABLE_VERSION', '3.11.2' );
+		define( 'WPSC_PRESENTABLE_VERSION', '3.11.3' );
 	}
 
 	// Define a salt to use when we hash, WPSC_SALT may be defined for us in our config file, so check first
