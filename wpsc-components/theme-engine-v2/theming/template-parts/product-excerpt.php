f49142157d77b5c060e754ce5fc30c4b89c1663b
Liz Karaffa <lizkaraffa@gmail.com>
2016-05-24 09:11:59 -0700

    changed product image order in DOM

--- wpsc-components/theme-engine-v2/theming/template-parts/product-excerpt.php
+++ wpsc-components/theme-engine-v2/theming/template-parts/product-excerpt.php
@@ -11,6 +11,16 @@
 ?>
 
 <div id="product-<?php wpsc_product_id(); ?>" <?php wpsc_product_class(); ?>>
+	<div class="wpsc-thumbnail-wrapper">
+		<a class="wpsc-thumbnail wpsc-product-thumbnail" href="<?php wpsc_product_permalink(); ?>">
+			<?php if ( wpsc_has_product_thumbnail() ): ?>
+				<?php wpsc_product_thumbnail(); ?>
+			<?php else: ?>
+				<?php wpsc_product_no_thumbnail_image(); ?>
+			<?php endif; ?>
+		</a>
+	</div>
+
 	<div class="wpsc-product-summary">
 		<div class="wpsc-product-header">
 			<h2 class="wpsc-product-title">
@@ -52,17 +62,4 @@
 			<?php wpsc_edit_product_link() ?>
 		</div><!-- .entry-meta -->
 	</div><!-- .wpsc-product-summary -->
-
-	<div class="wpsc-thumbnail-wrapper">
-		<a
-			class="wpsc-thumbnail wpsc-product-thumbnail"
-			href="<?php wpsc_product_permalink(); ?>"
-		>
-			<?php if ( wpsc_has_product_thumbnail() ): ?>
-				<?php wpsc_product_thumbnail(); ?>
-			<?php else: ?>
-				<?php wpsc_product_no_thumbnail_image(); ?>
-			<?php endif; ?>
-		</a>
-	</div>
 </div><!-- #post-<?php the_ID(); ?> -->
