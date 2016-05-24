f4fc5a24805845ccd9eb5d486b73994e6cdcbcd2
Liz Karaffa <lizkaraffa@gmail.com>
2016-05-24 09:12:34 -0700

    this one actually enqueued the style

--- wpsc-components/theme-engine-v2/helpers/css.php
+++ wpsc-components/theme-engine-v2/helpers/css.php
@@ -39,9 +39,11 @@ function wpsc_add_inline_style( $handle, $output ) {
 
 function _wpsc_te2_enqueue_styles() {
 	wp_register_style( 'wpsc-common', wpsc_locate_asset_uri( 'css/common.css' ), array(), WPSC_VERSION );
+	wp_register_style( 'wpsc-responsive', wpsc_locate_asset_uri( 'css/wpsc-responsive.css' ), array(), WPSC_VERSION );
 	do_action( 'wpsc_register_styles' );
 
 	wpsc_enqueue_style( 'wpsc-common' );
+	wp_enqueue_style( 'wpsc-responsive' );
 	wpsc_add_inline_style( 'wpsc-common', _wpsc_get_inline_style() );
 	do_action( 'wpsc_enqueue_styles' );
 }
