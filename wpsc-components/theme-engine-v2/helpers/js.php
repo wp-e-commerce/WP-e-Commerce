c6f8bbe7fb39936f75b36f257eab6514b72682c2
Liz Karaffa <=>
2016-05-26 21:29:42 -0700

    fixed merge conflict

--- wpsc-components/theme-engine-v2/helpers/js.php
+++ wpsc-components/theme-engine-v2/helpers/js.php
@@ -3,50 +3,6 @@
 add_action( 'wp_enqueue_scripts', '_wpsc_te2_register_scripts', 1 );
 
 function _wpsc_te2_register_scripts() {
-<<<<<<< HEAD
-	wp_register_script(
-		'wpsc-select-autocomplete',
-		wpsc_locate_asset_uri( 'js/jquery.select-to-autocomplete.js' ),
-		array( 'jquery-ui-autocomplete' ),
-		'1.0.5'
-	);
-	wp_register_script(
-		'wpsc-country-region',
-		wpsc_locate_asset_uri( 'js/country-region.js' ),
-		array( 'wpsc-select-autocomplete', 'jquery' ),
-		WPSC_VERSION
-	);
-	wp_register_script(
-		'wpsc-copy-billing-info',
-		wpsc_locate_asset_uri( 'js/copy-billing-info.js' ),
-		array( 'jquery' ),
-		WPSC_VERSION
-	);
-	wp_register_script(
-		'wpsc-shipping-price-simulator',
-		wpsc_locate_asset_uri( 'js/shipping-price-simulator.js' ),
-		array( 'jquery' ),
-		WPSC_VERSION
-	);
-	wp_register_script(
-		'wpsc-checkout-payment',
-		wpsc_locate_asset_uri( 'js/checkout-payment.js' ),
-		array( 'jquery' ),
-		WPSC_VERSION
-	);
-	wp_register_script(
-		'wpsc-stacktable',
-		wpsc_locate_asset_uri( 'js/stacktable.min.js' ),
-		array( 'jquery' ),
-		WPSC_VERSION
-	);
-	wp_register_script(
-		'wpsc-checkout',
-		wpsc_locate_asset_uri( 'js/checkout.js' ),
-		array( 'jquery' ),
-		WPSC_VERSION
-	);
-=======
 
 	$engine = WPSC_Template_Engine::get_instance();
 
@@ -74,14 +30,8 @@ function _wpsc_te2_register_scripts() {
 		// Output our namespace.
 		?><script type='text/javascript'>/* <![CDATA[ */window.WPSC = window.WPSC || {};/* ]]> */</script><?php
 	}
->>>>>>> master
 
 	do_action( 'wpsc_register_scripts' );
-	
-	//Change this later so it's not enqueued everywhere
-	wp_enqueue_script( 'wpsc-stacktable' );
-	wp_enqueue_script( 'wpsc-checkout' );
-
 	do_action( 'wpsc_enqueue_scripts' );
 }
 
