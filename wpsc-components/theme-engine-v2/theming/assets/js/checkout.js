0ee51f39e7706988fd0b78afd280a6f7a6db4159
Liz Karaffa <liz@zao.is>
2017-02-21 15:06:43 -0800

    only allow numbers and add appropriate spaces in credit card field

--- wpsc-components/theme-engine-v2/theming/assets/js/checkout.js
+++ wpsc-components/theme-engine-v2/theming/assets/js/checkout.js
@@ -4,4 +4,19 @@
 	$( '.wpsc-field-select_country' ).FloatLabel();
 	$( '.wpsc-field-password' ).FloatLabel();
 	$( '.wpsc-cc-field' ).FloatLabel();
+
+	$('.wpsc-credit-card-form-card-number').on('keypress change', function (e) {
+		var a = [];
+    	var k = e.which;
+
+    	for (i = 48; i < 58; i++)
+	        a.push(i);
+
+	    if (!(a.indexOf(k)>=0))
+        	e.preventDefault();
+
+    	$(this).val(function (index, value) {
+			return value.replace(/\W/gi, '').replace(/(.{4})/g, '$1 ');
+		});
+	});
 })(jQuery);
