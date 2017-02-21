50621f70a38eb10306acbea34c85a6b8186a745e
Liz Karaffa <liz@zao.is>
2017-02-21 15:00:18 -0800

    float credit card labels

--- wpsc-components/theme-engine-v2/theming/assets/js/checkout.js
+++ wpsc-components/theme-engine-v2/theming/assets/js/checkout.js
@@ -3,4 +3,5 @@
 	$( '.wpsc-field-select_region' ).FloatLabel();
 	$( '.wpsc-field-select_country' ).FloatLabel();
 	$( '.wpsc-field-password' ).FloatLabel();
+	$( '.wpsc-cc-field' ).FloatLabel();
 })(jQuery);
