b1d8308fdfb97993f9d3dba5ce50415a69a85479
Liz Karaffa <liz@zao.is>
2017-02-21 16:38:23 -0800

    allow only numbers and add appropriate characters to credit card fields

--- wpsc-components/theme-engine-v2/theming/assets/js/checkout.js
+++ wpsc-components/theme-engine-v2/theming/assets/js/checkout.js
@@ -19,4 +19,29 @@
 			return value.replace(/\W/gi, '').replace(/(.{4})/g, '$1 ');
 		});
 	});
+	$('.wpsc-credit-card-form-card-expiry').on('keypress change', function (e) {
+		var a = [];
+    	var k = e.which;
+
+    	for (i = 48; i < 58; i++)
+	        a.push(i);
+
+	    if (!(a.indexOf(k)>=0))
+        	e.preventDefault();
+
+    	$(this).val(function (index, value) {
+			return value.replace(/\W/gi, '').replace(/(.{2})/g, '$1\/');
+		});
+	});
+	$('.wpsc-credit-card-form-card-cvc').on('keypress change', function (e) {
+		var a = [];
+    	var k = e.which;
+
+    	for (i = 48; i < 58; i++)
+	        a.push(i);
+
+	    if (!(a.indexOf(k)>=0))
+        	e.preventDefault();
+	});
+
 })(jQuery);
